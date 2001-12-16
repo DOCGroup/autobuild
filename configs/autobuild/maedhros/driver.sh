3223742335c5ab17311e448b7dac8c25c110cbdf
Carlos O'Ryan <coryan@atdesk.com>
2001-12-16 14:05:39 +0000

    ChangeLogTag:Sun Dec 16 06:00:42 2001  Carlos O'Ryan  <coryan@uci.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@24 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/maedhros/driver.sh
+++ configs/autobuild/maedhros/driver.sh
@@ -5,8 +5,8 @@
 CVSROOT=:ext:ucibuilds@cvs.doc.wustl.edu:/project/cvs-repository
 export CVSROOT
 
-ACE_ROOT=/project/maedhros/bugzilla/Core/ACE_wrappers
-export ACE_ROOT
+CVS_RSH=/usr/bin/ssh
+export CVS_RSH
 
 PATH=/usr/local/redhat-7.1/gcc-3.0.2/bin:$PATH
 export PATH
@@ -20,12 +20,12 @@ cvs -r -z 3 -Q checkout -P autobuild
 
 perl -w ./autobuild/autobuild.pl autobuild/configs/autobuild/maedhros/Core.xml
 
-WEBLOG=$HOME/.www-docs/auto_compile_logs/maedhros_Core
-
-DATE=`date -u +%Y_%m_%d_%H_%M`
-/bin/cp build_log.txt $WEBLOG/$DATE.txt
-perl -w ./autobuild/make_pretty.pl -b -c makefile -i $WEBLOG/$DATE.txt \
-    -o $WEBLOG/${DATE}_brief.html
-perl -w ./autobuild/make_pretty.pl    -c makefile -i $WEBLOG/$DATE.txt \
-    -o $WEBLOG/${DATE}.html
+#WEBLOG=$HOME/.www-docs/auto_compile_logs/maedhros_Core
+#
+#DATE=`date -u +%Y_%m_%d_%H_%M`
+#/bin/cp build_log.txt $WEBLOG/$DATE.txt
+#perl -w ./autobuild/make_pretty.pl -b -c makefile -i $WEBLOG/$DATE.txt \
+#    -o $WEBLOG/${DATE}_brief.html
+#perl -w ./autobuild/make_pretty.pl    -c makefile -i $WEBLOG/$DATE.txt \
+#    -o $WEBLOG/${DATE}.html
     
