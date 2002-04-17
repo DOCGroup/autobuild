e8ae77bcfc9ed088131e339b63cd84f37d2bd6b7
Craig Rodrigues <crodrigu@bbn.com>
2002-04-17 01:10:33 +0000

    Module to check the compiler version.
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@54 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ command/check_compiler.pm
@@ -0,0 +1,68 @@
+#
+# $Id$
+#
+
+package check_compiler;
+
+use strict;
+use warnings;
+
+use Cwd;
+
+###############################################################################
+# Constructor
+
+sub new
+{
+    my $proto = shift;
+    my $class = ref ($proto) || $proto;
+    my $self = {};
+
+    bless ($self, $class);
+    return $self;
+}
+
+##############################################################################
+
+sub CheckRequirements ()
+{
+    my $self = shift;
+    my $root = main::GetVariable ('root');
+
+    if (!defined $root) {
+        print STDERR __FILE__, ": Requires \"root\" variable\n";
+        return 0;
+    }
+    
+    if (!-r $root || !-d $root) {
+        print STDERR __FILE__, ": Cannot access \"root\" directory: $root\n";
+        return 0;
+    }
+
+    return 1;
+}
+
+##############################################################################
+
+sub Run ($)
+{
+    my $self = shift;
+    my $compiler = shift;
+
+    main::PrintStatus ('Setup', "check compiler $compiler" );
+
+    print "================ Compiler version ================\n";
+
+    if(lc $compiler eq "gcc"){
+        system("gcc -v 2>&1");
+    }
+
+    if(lc $compiler eq "sun_cc"){
+        system("CC -V");
+    }
+    return 1;
+}
+
+##############################################################################
+
+main::RegisterCommand ("check_compiler", new check_compiler());
