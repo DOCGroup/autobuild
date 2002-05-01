eb6d710d95d8b82944d07a775d9c4094c9789f82
wustlbuilds <wustlbuilds>
2002-05-01 04:04:41 +0000

    ChangeLogTag: Wed May  1 04:02:01 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@71 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/ace/driver.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+PATH=/opt/SUNWspro_6.0/SUNWspro/bin:/pkg/perl-5.6.1/bin:/pkg/gnu/bin:$PATH
+PATH=.:$HOME/bin:/opt/SUNWspro_6.1/SUNWspro/bin:/pkg/perl-5.6.1/bin/usr/ccs/bin:/pkg/gnu2k1/bin:/pkg/gnu/bin:/usr/bin:/pkg/purify/purify-5.1-solaris2:$PATH
+export PATH
+
+perl -w ../../../autobuild.pl $@
+
