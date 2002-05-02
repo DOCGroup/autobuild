92ac4f7018f9bec57a98c91ebc5395fda13a922c
wustlbuilds <wustlbuilds>
2002-05-02 02:24:52 +0000

    ChangeLogTag: Thu May  2 02:22:15 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@76 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/ace/driver.sh
+++ configs/autobuild/ace/driver.sh
@@ -3,9 +3,8 @@
 # $Id$
 #
 
-PATH=/opt/SUNWspro_6.0/SUNWspro/bin:/pkg/perl-5.6.1/bin:/pkg/gnu/bin:$PATH
 PATH=.:$HOME/bin:/opt/SUNWspro_6.1/SUNWspro/bin:/pkg/perl-5.6.1/bin/usr/ccs/bin:/pkg/gnu2k1/bin:/pkg/gnu/bin:/usr/bin:/pkg/purify/purify-5.1-solaris2:$PATH
 export PATH
 
-perl -w ../../../autobuild.pl $@
-
+exec /pkg/perl-5.6.1/bin/perl $HOME/autobuild/autobuild.pl \
+          $HOME/autobuild/configs/autobuild/ace/SunOS_FORTE_UPDATE_1.xml
