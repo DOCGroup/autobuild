5a9ed22fa9e36adb1e792c9398e3f1503ab98587
Bala Natarajan <bala@dre.vanderbilt.edu>
2002-05-10 00:32:35 +0000

    ChangeLogTag: Thu May  9 19:23:45 2002  Balachandran Natarajan  <bala@cs.wustl.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@90 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/danzon/driver.sh
+++ configs/autobuild/danzon/driver.sh
@@ -7,5 +7,5 @@ PATH=/opt/SUNWspro_6.2/SUNWspro/bin:/pkg/perl-5.6.1/bin:/pkg/gnu/bin:$PATH
 export PATH
 
 exec /pkg/perl-5.6.1/bin/perl $HOME/autobuild/autobuild.pl \
-                             $HOME/autobuild/configs/autobuild/danzon/SunOS_SunCC51.xml 2>&1 
+                             $HOME/autobuild/configs/autobuild/danzon/SunOS_SunCC53.xml 2>&1 
 
