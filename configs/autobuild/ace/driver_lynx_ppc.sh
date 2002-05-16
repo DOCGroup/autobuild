5e07c3225cf0bb9fb380011606219bb449aba78f
Craig Rodrigues <crodrigu@bbn.com>
2002-05-16 18:11:22 +0000

    ChangeLogTag: Thu May 16 18:10:51 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@100 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/ace/driver_lynx_ppc.sh
+++ configs/autobuild/ace/driver_lynx_ppc.sh
@@ -3,7 +3,7 @@
 # $Id$
 #
 
-PATH=.:$HOME/bin:/pkg/perl-5.6.1/bin:/pkg/gnu2k1/bin:/pkg/gnu/bin:/usr/bin:$PATH
+PATH=.:$HOME/bin:/pkg/perl-5.6.1/bin:/usr/lynx/3.1.0/ppc/cdk/sunos-xcoff-ppc/bin:/usr/lynx/3.1.0/ppc/cdk/sunos-xcoff-ppc/usr/bin:/pkg/gnu2k1/bin:/pkg/gnu/bin:/usr/bin:$PATH
 export PATH
 
 exec /pkg/perl-5.6.1/bin/perl $HOME/autobuild/autobuild.pl \
