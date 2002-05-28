6a5c3a0d98ca8060f2e438a4da145ab5c9d7027f
Craig Rodrigues <crodrigu@bbn.com>
2002-05-28 21:32:32 +0000

    ChangeLogTag: Tue May 28 21:18:09 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@121 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/rumba/driver_gcc_304.sh
+++ configs/autobuild/rumba/driver_gcc_304.sh
@@ -4,4 +4,4 @@
 #
 
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
-                             $HOME/autobuild/configs/autobuild/rumba/Debian_WChar.xml
+                             $HOME/autobuild/configs/autobuild/rumba/Debian_GCC_3.0.4.xml
