c8f217ffc219c96f67ea9913d9f2a75079c3268a
Craig Rodrigues <crodrigu@bbn.com>
2002-06-20 14:29:22 +0000

    ChangeLogTag: Thu Jun 20 14:28:35 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@131 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/guajira/driver.sh
+++ configs/autobuild/guajira/driver.sh
@@ -3,7 +3,11 @@
 # $Id$
 #
 
-PATH=.:$HOME/bin:/usr/local/openssh/bin:/pkg/perl-5.6.1/bin:/pkg/local/bin:/pkg/gnu/bin:/sbin:$PATH
+ulimit -d 1048576
+ulimit -c 0
+umask 022
+
+PATH=.:$HOME/bin:/usr/local/openssh/bin:/pkg/gnu/bin:/pkg/local/bin:/sbin:$PATH
 export PATH
 
 exec perl  $HOME/autobuild/autobuild.pl \
