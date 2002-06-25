bfd5c0af886eaa05efaf68f57164a1a4272d31d2
wustlbuilds <wustlbuilds>
2002-06-25 20:26:50 +0000

    ChangeLogTag:Tue Jun 25 15:25:04 2002  Nanbor Wang  <nanbor@cs.wustl.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@138 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/nirvana/driver.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set "LIB=%LIB%;d:\bugzilla\openssl-0.9.6\out32dll"
+set "INCLUDE=%INCLUDE%;D:\bugzilla\openssl-0.9.6\inc32"
+call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
+cd ..\..\..\
+cvs up -P -d
+cd configs\autobuild\nirvana
+perl D:\bugzilla\autobuild\autobuild.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
