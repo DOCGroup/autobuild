11776f33b8dd3b6b67ead95ee86865be4a5a4c47
wustlbuilds <wustlbuilds>
2002-06-25 21:26:53 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@139 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/tao/driver.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set "LIB=%LIB%;F:\bugzilla\ssl\openssl-0.9.6\out32dll"
+set "INCLUDE=%INCLUDE%;F:\bugzilla\ssl\openssl-0.9.6\inc32"
+call "E:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
+cd ..\..\..\
+cvs up -P -d
+cd configs\autobuild\nirvana
+perl F:\bugzilla\autobuild\autobuild.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
