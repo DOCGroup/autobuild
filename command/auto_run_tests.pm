968ae9b5d52dd5f03e0aee28a76d1084990aedc4
wustlbuilds <wustlbuilds>
2002-06-27 17:03:35 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@148 59e07337-6b18-0410-a141-c5efe34a0da7

--- command/auto_run_tests.pm
+++ command/auto_run_tests.pm
@@ -34,7 +34,7 @@ sub CheckRequirements ()
         print STDERR __FILE__, ": Requires \"root\" variable\n";
         return 0;
     }
-    
+
     if (!-r $root || !-d $root) {
         print STDERR __FILE__, ": Cannot access \"root\" directory: $root\n";
         return 0;
@@ -59,6 +59,8 @@ sub Run ($)
         $root = $1;
     }
 
+    $options =~ s/\\x22/"/g;
+
     main::PrintStatus ('Test', 'auto_run_tests');
 
     my $current_dir = getcwd ();
@@ -71,7 +73,7 @@ sub Run ($)
     if (!defined $project_root) {
         $project_root = 'ACE_wrappers';
     }
-    
+
     if (!chdir $project_root) {
         print STDERR __FILE__, ": Cannot change to $project_root\n";
         return 0;
