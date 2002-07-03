65de425acfd8cc216bbcb05d035a665403fca484
wustlbuilds <wustlbuilds>
2002-07-03 15:12:34 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@152 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/nirvana/driver.bat
+++ configs/autobuild/nirvana/driver.bat
@@ -6,5 +6,6 @@ call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
 cd ..\..\..\
 cvs up -P -d
 cd configs\autobuild\nirvana
+if not exist Z: net use Z: \\TAO\bugzilla_www
 perl D:\bugzilla\autobuild\autobuild.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
