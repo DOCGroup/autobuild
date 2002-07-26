ccb11e03d7082dfb8689fa7f3fe8b2e56acf9621
Johnny Willemsen <jwillemsen@remedy.nl>
2002-07-26 11:28:46 +0000

    ChangeLogTag: Fri Jul 26 13:20:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@184 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/dobuildpc2.bat
@@ -0,0 +1,6 @@
+rem This batch file is used to start the real build file depending on the pc. This way I can easily 
+rem change the number of different builds done on a certain build pc by just editing the correct
+rem driverbuildpc# batch file. Because the driverbuildpc does a cvs update I can easily to this 
+rem remotely
+
+driverbuildpc2
---
+++ configs/autobuild/remedynl/driverbuildpc1.bat
@@ -0,0 +1,5 @@
+cd ..\..\..\
+cvs up -P -d
+cd configs\autobuild\remedynl
+perl D:\autobuild\autobuild.pl BCB6DynamicDebug.xml
+dobuildpc1
