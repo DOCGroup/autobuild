8937beabf754b28f814e1b4018c43740e80fcc90
Johnny Willemsen <jwillemsen@remedy.nl>
2002-08-02 09:59:14 +0000

    ChangeLogTag: Fri Aug 02 11:56:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@208 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc1.bat
+++ configs/autobuild/remedynl/driverbuildpc1.bat
@@ -1,8 +1,8 @@
 cd ..\..\..\
 cvs -z9 up -P -d
 cd configs\autobuild\remedynl
+perl D:\autobuild\autobuild.pl MingWGCC31.xml
 perl D:\autobuild\autobuild.pl BCB6DynamicRelease.xml
 perl D:\autobuild\autobuild.pl BCB6DynamicDebugWChar.xml
 perl D:\autobuild\autobuild.pl BCB6DynamicDebug.xml
-perl D:\autobuild\autobuild.pl MingWGCC31.xml
 dobuildpc1
