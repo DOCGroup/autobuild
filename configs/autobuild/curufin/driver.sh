9e742e41ca21884a67dac0e0b56fe647d2ac3ddb
Craig Rodrigues <crodrigu@bbn.com>
2002-09-03 22:08:47 +0000

    ChangeLogTag: Tue Sep  3 22:07:42 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@269 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/curufin/driver.sh
+++ configs/autobuild/curufin/driver.sh
@@ -2,14 +2,6 @@
 #
 # $Id$
 #
-CVSROOT=:ext:ucibuilds@cvs.doc.wustl.edu:/project/cvs-repository
-export CVSROOT
 
-CVS_RSH=/usr/bin/ssh
-export CVS_RSH
-
-cd /project/curufin/bugzilla/SingleThreaded
-cvs -r -z 3 -Q checkout -P autobuild
-
-exec perl -w ./autobuild/autobuild.pl autobuild/configs/autobuild/curufin/SingleThreaded.xml >/dev/null 2>&1 </dev/null
+exec perl -w $HOME/autobuild/autobuild.pl $HOME/autobuild/configs/autobuild/curufin/SingleThreaded.xml >/dev/null 2>&1 </dev/null
 
