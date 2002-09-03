b7c7b9be0862146ce187a229a37d7b860ed09ea0
Craig Rodrigues <crodrigu@bbn.com>
2002-09-03 22:12:50 +0000

    ChangeLogTag: Tue Sep  3 22:07:42 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@270 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/curufin/driver.sh
+++ configs/autobuild/curufin/driver.sh
@@ -3,5 +3,5 @@
 # $Id$
 #
 
-exec perl -w $HOME/autobuild/autobuild.pl $HOME/autobuild/configs/autobuild/curufin/SingleThreaded.xml >/dev/null 2>&1 </dev/null
+exec perl -w $HOME/autobuild/autobuild.pl $HOME/autobuild/configs/autobuild/curufin/SingleThreaded.xml 
 
