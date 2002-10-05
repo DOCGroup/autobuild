c2604789cc5ab0b285f624a9998f73a520398101
Craig Rodrigues <crodrigu@bbn.com>
2002-10-05 12:35:39 +0000

    ChangeLogTag: Sat Oct  5 12:31:21 UTC 2002  Craig Rodrigues  <crodrigu@bbn.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@304 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/doc.sh
+++ configs/scoreboard/doc.sh
@@ -11,4 +11,4 @@ export CVS_RSH
 cd $HOME/autobuild
 cvs up
 
-exec /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c autobuild/configs/scoreboard/acetao.xml 2>&1
+exec /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c $HOME/autobuild/configs/scoreboard/acetao.xml 2>&1
