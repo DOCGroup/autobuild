1c4c2e069d400df1620ac6e6d97881f4a96f764b
Johnny Willemsen <jwillemsen@remedy.nl>
2002-10-25 05:54:54 +0000

    ChangeLogTag: Fri Oct 24 05:54:12 UTC 2002 Johnny Willemsen <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@349 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc1.bat
+++ configs/autobuild/remedynl/driverbuildpc1.bat
@@ -1,15 +1,17 @@
 cd ..\..\..\
 start c:\putty\pageant.exe c:\putty\privatenokey
 sleep 4
-del D:\bcb6d\.disable
-del D:\BCB6ddwchar\.disable
-del D:\BCB6dr\.disable
+rem del D:\bcb6d\.disable
+rem del D:\BCB6ddwchar\.disable
+rem del D:\BCB6dr\.disable
+del D:\BCB4dd\.disable
 set CVS_RSH=plink
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 rem perl D:\autobuild\autobuild.pl MingWGCC31.xml
 rem perl D:\autobuild\autobuild.pl CygwinGCC2_95_3_5.xml 
 rem perl D:\autobuild\autobuild.pl BCB6DynamicRelease.xml 
-perl D:\autobuild\autobuild.pl BCB6DynamicDebugWChar.xml 
-perl D:\autobuild\autobuild.pl BCB6DynamicDebug.xml
+rem perl D:\autobuild\autobuild.pl BCB6DynamicDebugWChar.xml 
+rem perl D:\autobuild\autobuild.pl BCB6DynamicDebug.xml
+perl D:\autobuild\autobuild.pl BCB4BynamicDebug.xml
 dobuildpc1
