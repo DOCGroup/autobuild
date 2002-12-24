912fcf2098cc0f5800abea4435c5590a0284651b
Johnny Willemsen <jwillemsen@remedy.nl>
2002-12-24 09:41:37 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@454 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc2.bat
+++ configs/autobuild/remedynl/driverbuildpc2.bat
@@ -1,5 +1,7 @@
 cd ..\..\..\
 cvs -z9 up -P -d
+del D:\Cygwin\.disable
+del D:\.disable
 cd configs\autobuild\remedynl
 perl D:\autobuild\autobuild.pl CygwinGCC32.xml
 perl D:\autobuild\autobuild.pl BCB5StaticDebug.xml
