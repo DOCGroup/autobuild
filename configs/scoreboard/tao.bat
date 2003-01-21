913fafd1d2e32608622f406240599a7eabff8aa6
Nanbor Wang <nanbor@dre.vanderbilt.edu>
2003-01-21 23:29:02 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@499 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/tao.bat
+++ configs/scoreboard/tao.bat
@@ -3,6 +3,6 @@ setlocal
 
 cvs -r -Q checkout -P autobuild > run.log
 cd autobuild
-perl -w scoreboard.pl -o ../html > ../run.log
+perl -w scoreboard.pl -o ../html -c configs/scoreboard/acetao.xml > ../run.log
 cd ..
 move run.log html\log.txt
