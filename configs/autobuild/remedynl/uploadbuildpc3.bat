3329acb5fee0a130813759dce5cd527635d5e154
Johnny Willemsen <jwillemsen@remedy.nl>
2003-01-27 14:38:18 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@517 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/uploadbuildpc3.bat
+++ configs/autobuild/remedynl/uploadbuildpc3.bat
@@ -1,4 +1,7 @@
 rem This batch file is used to upload all logfiles manually on buildpc3 in case of a network failure
+start d:\putty\pageant.exe d:\putty\privatenokey
+sleep 4
+set CVS_RSH=plink
 pscp D:\ACE\BCB6dd\*.txt D:\ACE\BCB6dd\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_BCB6_Dynamic_Debug
 pscp D:\ACE\BCB6ddu\*.txt D:\ACE\BCB6ddu\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_BCB6_Dynamic_Debug_Unicode
 pscp D:\ACE\MingW\*.txt D:\ACE\MingW\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_MingW
