90adceea19971b4a265937f820cc52bd3fb7ec06
Johnny Willemsen <jwillemsen@remedy.nl>
2003-01-27 14:28:53 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@516 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/uploadbuildpc3.bat
@@ -0,0 +1,6 @@
+rem This batch file is used to upload all logfiles manually on buildpc3 in case of a network failure
+pscp D:\ACE\BCB6dd\*.txt D:\ACE\BCB6dd\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_BCB6_Dynamic_Debug
+pscp D:\ACE\BCB6ddu\*.txt D:\ACE\BCB6ddu\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_BCB6_Dynamic_Debug_Unicode
+pscp D:\ACE\MingW\*.txt D:\ACE\MingW\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_MingW
+pscp D:\ACE\BCB6dr\*.txt D:\ACE\BCB6dr\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_WinNT4_BCB6_Dynamic_Release
+pscp D:\ACE\BCB6dru\*.txt D:\ACE\BCB6dru\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_BCB6_Dynamic_Release_Unicode
