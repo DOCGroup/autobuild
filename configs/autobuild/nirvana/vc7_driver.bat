d59428ba4a153dbeb81e724b804914028602141a
Nanbor Wang <nanbor@dre.vanderbilt.edu>
2003-02-10 20:06:44 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@535 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/nirvana/vc7_driver.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+set "LIB=d:\ssl\openssl-0.9.6\out32dll"
+set "INCLUDE=D:\ssl\openssl-0.9.6\inc32"
+set "CVSROOT=:ext:wustlbuilds:/project/cvs-repository"
+C:\Program Files\Microsoft Visual Studio .NET\Vc7\bin\vcvars32.bat
+cd ..\..\..\
+cvs up -P -d
+cd configs\autobuild\nirvana
+if not exist Z: net use Z: \\TAO\bugzilla_www
+perl G:\bugzilla\autobuild\autobuild.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
