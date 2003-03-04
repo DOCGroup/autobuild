8d8de0f31204068cc8ee8fb27b2a326bcf95c235
Johnny Willemsen <jwillemsen@remedy.nl>
2003-03-04 10:55:23 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@566 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverpc4.sh
+++ configs/autobuild/remedynl/driverpc4.sh
@@ -2,6 +2,18 @@
 #
 # $Id$
 #
+CVSROOT=:ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
+export CVSROOT
+
+CVS_RSH=ssh
+export CVS_RSH
+
+cd $HOME/ACE/autobuild
+
+cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
                              $HOME/ACE/autobuild/configs/autobuild/remedynl/GCC32.xml
+
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/Kylix3.xml
