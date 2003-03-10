f5779b40d455210e1d1c29f7734a12761d9b1c94
Don Hinton <dhinton@dre.vanderbilt.edu>
2003-03-10 19:48:39 +0000

    ChangeLogTag:Mon Mar 10 19:41:10 UTC 2003  Don Hinton  <dhinton@ieee.org>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@579 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/dresystems/driver_local_scoreboard.sh
@@ -0,0 +1,4 @@
+
+cd /home/dhinton/development/ace/autobuild/
+
+/home/dhinton/development/ace/autobuild/scoreboard.pl -c /home/dhinton/development/ace/autobuild/configs/autobuild/dresystems/local_scoreboard.xml -o /home/svn/www/dresystems.com
---
+++ configs/autobuild/dresystems/driver_skoll_2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+PATH=/usr/local/bin:$PATH
+export PATH
+
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+
+exec /usr/bin/perl $HOME/development/ace/autobuild/autobuild.pl \
+                             $HOME/development/ace/autobuild/configs/autobuild/dresystems/skoll_2.xml 2>&1 
+
