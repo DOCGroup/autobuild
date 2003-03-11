12229d29d8bdf6b7133971f3e5bfe160a1e55384
Ossama Othman <othmano@ociweb.com>
2003-03-11 21:20:45 +0000

    ChangeLogTag:Tue Mar 11 13:20:37 2003  Ossama Othman  <ossama@uci.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@589 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/danzon/driver.sh
+++ configs/autobuild/danzon/driver.sh
@@ -6,6 +6,11 @@
 PATH=/opt/SUNWspro_6.2/SUNWspro/bin:/pkg/perl-5.6.1/bin:/pkg/gnu2k1/bin:/pkg/gnu/bin:$PATH
 export PATH
 
+# OpenSSL requires an explicit source of entropy on Solaris.  The
+# Entropy Gathering Daemon perl script fulfills that requirement.
+ENTROPY_FILE=/tmp/entropy
+test -f $ENTROPY_FILE || /project/doc/pkg/perl5/bin/egd.pl $ENTROPY_FILE
+
 exec /pkg/perl-5.6.1/bin/perl $HOME/autobuild/autobuild.pl \
                              $HOME/autobuild/configs/autobuild/danzon/SunOS_SunCC53.xml 2>&1 
 
