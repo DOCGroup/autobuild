e334d11c62849f2bd92b1d5b9da4b3dc997fb14a
Bala Natarajan <bala@dre.vanderbilt.edu>
2003-03-17 19:07:23 +0000

    ChangeLogTag: Mon Mar 17 13:06:57 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@618 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/tao.bat
+++ configs/scoreboard/tao.bat
@@ -5,11 +5,11 @@ cvs -r -Q checkout -P autobuild > run.log
 cd autobuild
 perl -w scoreboard.pl -o ../html -c configs/scoreboard/acetao.xml > ../run.log
 perl -w scoreboard.pl -d ../html -i configs/scoreboard/index.xml > ../run.log
-perl -w scoreboard.pl -d ../html -f configs/scoreboard/ace.xml -o ../html/ace.html >> ../run.log
-perl -w scoreboard.pl -d ../html -f configs/scoreboard/ace_future.xml -o ../html/ace_future.html >> ../run.log
-perl -w scoreboard.pl -d ../html -f configs/scoreboard/tao.xml -o ../html/tao.html >> ../runlog
-perl -w scoreboard.pl -d ../html -f configs/scoreboard/tao_future.xml -o ../html/tao_future.html >> ../run.log
-perl -w scoreboard.pl -d ../html -f configs/scoreboard/misc.xml -o ../html/misc.html >> ../run.log
+perl -w scoreboard.pl -d ../html -f configs/scoreboard/ace.xml -o ace.html >> ../run.log
+perl -w scoreboard.pl -d ../html -f configs/scoreboard/ace_future.xml -o ace_future.html >> ../run.log
+perl -w scoreboard.pl -d ../html -f configs/scoreboard/tao.xml -o tao.html >> ../runlog
+perl -w scoreboard.pl -d ../html -f configs/scoreboard/tao_future.xml -o tao_future.html >> ../run.log
+perl -w scoreboard.pl -d ../html -f configs/scoreboard/misc.xml -o misc.html >> ../run.log
 perl -w scoreboard.pl -d ../html -z >> ../run.log
 cd ..
 move run.log html\log.txt
