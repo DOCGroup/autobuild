f753af2f31010995fde0412f563091ef85fbce32
Bala Natarajan <bala@dre.vanderbilt.edu>
2003-03-17 01:16:24 +0000

    ChangeLogTag:Sun Mar 16 19:11:23 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@605 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/doc.sh
+++ configs/scoreboard/doc.sh
@@ -15,7 +15,7 @@ cvs up
 
 
 exec /usr/bin/perl ./scoreboard.pl -v -d /export/project/workarea01/Scoreboard/html -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
-exec /usr/bin/perl ./scoreboard.pl -v -d /export/project/workarea01/Scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace.xml -o /project/taotmp/bala/html/ace.html 2>&1 &
+exec /usr/bin/perl ./scoreboard.pl -v -d /export/project/workarea01/Scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace.xml -o /export/project/workarea01/Scoreboard/html/ace.html  2>&1 &
 exec /usr/bin/perl ./scoreboard.pl -d /export/project/workarea01/Scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace_future.xml -o /export/project/workarea01/Scoreboard/html/ace_future.html 2>&1 &
 exec /usr/bin/perl ./scoreboard.pl -d /export/project/workarea01/Scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao.xml -o /export/project/workarea01/Scoreboard/html/tao.html 2>&1  &
 exec /usr/bin/perl ./scoreboard.pl -d /export/project/workarea01/Scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao_future.xml -o /export/project/workarea01/Scoreboard/html/tao_future.html 2>&1 &
