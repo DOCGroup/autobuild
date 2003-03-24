f5afa95592ab8a6e19191762fcc628fc34c8ff98
Johnny Willemsen <jwillemsen@remedy.nl>
2003-03-24 06:50:35 +0000

    ChangeLogTag: Mon Mar 24 06:44:12 UTC 2002 Johnny Willemsen <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@637 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc2.bat
+++ configs/autobuild/remedynl/driverbuildpc2.bat
@@ -4,6 +4,5 @@ del D:\Cygwin\.disable
 del D:\.disable
 cd configs\autobuild\remedynl
 perl D:\autobuild\autobuild.pl CygwinGCC32.xml
-perl D:\autobuild\autobuild.pl BCB5StaticDebug.xml
 rem c:\cygwin\bin\sleep 1h
 dobuildpc2
