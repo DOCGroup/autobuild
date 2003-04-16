a769151af978dedec71d67e41e8e8e95a38aa950
Bala Natarajan <bala@dre.vanderbilt.edu>
2003-04-16 12:26:55 +0000

    ChangeLogTag:Wed Apr 16 07:11:05 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@666 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/tao_dre.sh
+++ configs/scoreboard/tao_dre.sh
@@ -14,7 +14,7 @@ cvs up
 #exec /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c $HOME/autobuild/configs/scoreboard/acetao.xml 2>&1 &
 
 # Generate the index page!
-exec /usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
+exec /usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/ -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
 # Generate other pages!
 exec /usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace.xml -o ace.html  2>&1 &
 exec /usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace_future.xml -o ace_future.html 2>&1 &
