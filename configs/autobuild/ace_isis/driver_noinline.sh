ad6cebdb716b121820ac7d9dbc727dc95b3550e2
isisbuilds <isisbuilds>
2003-04-17 17:21:30 +0000

    ChangeLogTag:Thu Apr 17 11:52:54 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@702 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/ace_isis/driver_noinline.sh
+++ configs/autobuild/ace_isis/driver_noinline.sh
@@ -1,11 +1,7 @@
-#!/pkg/gnu/bin/bash
 #
 # $Id$
 #
 
-PATH=:$HOME/bin:/pkg/gnu2k1/bin:/pkg/gnu/bin:$PATH
-export PATH
-
-exec /pkg/perl-5.6.1/bin/perl -w $HOME/autobuild/autobuild.pl \
-                     $HOME/autobuild/configs/autobuild/tango/SunOS_GCC_2_95.xml
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+                   $HOME/autobuild/configs/autobuild/ace_isis/RH8_NoInline.xml
 
--- configs/autobuild/ace_isis/driver_static.sh
+++ configs/autobuild/ace_isis/driver_static.sh
@@ -6,5 +6,5 @@ CVS_RSH=/usr/bin/ssh
 export CVS_RSH
 
 /usr/bin/perl $HOME/autobuild/autobuild.pl \
-	autobuild/configs/autobuild/ace_isis/RH8_Static_Core.xml >/dev/null 2>&1 </dev/null
+	$HOME/autobuild/configs/autobuild/ace_isis/RH8_Static_Core.xml
 
