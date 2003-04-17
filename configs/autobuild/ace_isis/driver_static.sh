bda0ac90e35b7fe82c1ff12b9e06b31434347709
Bala Natarajan <bala@dre.vanderbilt.edu>
2003-04-17 17:07:21 +0000

    ChangeLogTag:Thu Apr 17 11:52:54 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@701 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/ace_isis/driver_static.sh
+++ configs/autobuild/ace_isis/driver_static.sh
@@ -2,21 +2,9 @@
 #
 # $Id$
 #
-CVSROOT=:ext:ucibuilds@cvs.doc.wustl.edu:/project/cvs-repository
-export CVSROOT
-
 CVS_RSH=/usr/bin/ssh
 export CVS_RSH
 
-cd /project/maedhros/bugzilla/Core
-cvs -r -z 3 -Q checkout -P autobuild
-
-PATH=/usr/local/redhat-7.1/gcc-3.0.2/bin:$PATH
-export PATH
-
-LD_LIBRARY_PATH=/project/maedhros/bugzilla/Core/ACE_wrappers/ace:\
-/usr/local/redhat-7.1/gcc-3.0.2/lib
-export LD_LIBRARY_PATH
-
-exec perl -w ./autobuild/autobuild.pl autobuild/configs/autobuild/maedhros/Core.xml >/dev/null 2>&1 </dev/null
+/usr/bin/perl $HOME/autobuild/autobuild.pl \
+	autobuild/configs/autobuild/ace_isis/RH8_Static_Core.xml >/dev/null 2>&1 </dev/null
 
