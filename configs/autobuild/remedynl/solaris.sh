1ada54a7c4a870c4d5d9f070c5f6c2d966b365c3
Johnny Willemsen <jwillemsen@remedy.nl>
2003-05-27 11:19:07 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@768 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/solaris.sh
+++ configs/autobuild/remedynl/solaris.sh
@@ -3,5 +3,9 @@
 # $Id$
 #
 
+cd $HOME/ACE/autobuild
+
+cvs -z9 up -P -d
+
 exec /usr/local/bin/perl $HOME/ACE/autobuild/autobuild.pl \
                              $HOME/ACE/autobuild/configs/autobuild/remedynl/SunForte.xml
