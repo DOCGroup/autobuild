9879cb9b35d54343d32c1e70bb3e1feabc3ed754
Dante J. Cannarozzi <djc2@cs.wustl.edu>
2003-06-03 04:57:19 +0000

    javart stuff
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@804 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/scoreboard/javart.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# $Id $
+
+PERLLIB=autobuild
+export PERLLIB
+
+#CVS_RSH=ssh
+#export CVS_RSH
+
+#cd $HOME/autobuild
+#cvs up
+
+
+# Generate the index page!
+exec /usr/bin/perl ./scoreboard.pl -v -d /project/deuce/pces/javart/scoreboard -i $HOME/autobuild/configs/scoreboard/index-javart.xml  2>&1 &
+# Generate other pages!
+exec /usr/bin/perl ./scoreboard.pl -v -d /project/deuce/pces/javart/scoreboard -f $HOME/autobuild/configs/scoreboard/javart.xml -o javart.html  2>&1 &
+
+# Generate integrated pages!
+#exec /usr/bin/perl ./scoreboard.pl -d /export/project/workarea01/Scoreboard/html -z 2>&1;
+
