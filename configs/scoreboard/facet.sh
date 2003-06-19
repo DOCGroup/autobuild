28285daa10e2415c39daa45c895881da6e66f2dc
Dante J. Cannarozzi <djc2@cs.wustl.edu>
2003-06-19 21:20:21 +0000

    scoreboard updates for facet
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@852 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/facet.sh
+++ configs/scoreboard/facet.sh
@@ -2,6 +2,11 @@
 #
 # $Id$
 
+# Author: Dante Cannarozzi
+# Purpose: Generate the facet scoreboard results as well as the facet build
+#          using ACE+TAO from cvs
+
+
 PERLLIB=autobuild
 export PERLLIB
 
@@ -14,8 +19,10 @@ cd $HOME/autobuild
 
 # Generate the index page!
 exec /usr/bin/perl ./scoreboard.pl -v -d /project/deuce/pces/facet/scoreboard -i $HOME/autobuild/configs/scoreboard/index-facet.xml  2>&1 &
+#exec /usr/bin/perl ./scoreboard.pl -v -d /project/deuce/pces/facet/scoreboard -i $HOME/autobuild/configs/scoreboard/index-facet-cvs.xml  2>&1 &
 # Generate other pages!
 exec /usr/bin/perl ./scoreboard.pl -v -d /project/deuce/pces/facet/scoreboard -f $HOME/autobuild/configs/scoreboard/facet.xml -o facet.html  2>&1 &
+exec /usr/bin/perl ./scoreboard.pl -v -d /project/deuce/pces/facet/scoreboard -f $HOME/autobuild/configs/scoreboard/facet-cvs.xml -o facet-cvs.html  2>&1 &
 
 # Generate integrated pages!
 #exec /usr/bin/perl ./scoreboard.pl -d /export/project/workarea01/Scoreboard/html -z 2>&1;
