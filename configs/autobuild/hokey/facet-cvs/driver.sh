b58c35b774b2da11c149ec82bf793309911c7cc1
Dante J. Cannarozzi <djc2@cs.wustl.edu>
2003-06-24 22:45:33 +0000

    moved facet stuff into separate directories
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@874 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/hokey/facet-cvs/driver.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Author: Dante Cannarozzi
+# Purpose: autobuild driver for running the facet builds using ACE_TAO from cvs. 
+#          It first builds the external libraries that facet uses, then jACE and ACE_TAO.
+
+# the directory which contains all of the configuration files
+BASE = $HOME/autobuild/configs/autobuild/hokey/facet-cvs
+
+/usr/bin/perl $HOME/autobuild/autobuild.pl $BASE/facet.xml
+/usr/bin/perl $HOME/autobuild/autobuild.pl $BASE/jACE.xml
+/usr/bin/perl $HOME/autobuild/autobuild.pl $BASE/ACE_TAO.xml
+/usr/bin/perl $HOME/autobuild/autobuild.pl $BASE/tao-facet-adapter.xml
