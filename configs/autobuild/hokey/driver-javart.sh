61fdf80efc5de01135bdc92e6b4197c4096a625a
Dante J. Cannarozzi <djc2@cs.wustl.edu>
2003-06-27 21:43:48 +0000

    fixes
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@901 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/hokey/driver-javart.sh
+++ configs/autobuild/hokey/driver-javart.sh
@@ -2,9 +2,9 @@
 #
 # $Id$
 #
-exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+/usr/bin/perl $HOME/autobuild/autobuild.pl \
                              $HOME/autobuild/configs/autobuild/hokey/javart-jvm.xml
-exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+/usr/bin/perl $HOME/autobuild/autobuild.pl \
                              $HOME/autobuild/configs/autobuild/hokey/javart-rtsj.xml
-exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+/usr/bin/perl $HOME/autobuild/autobuild.pl \
                              $HOME/autobuild/configs/autobuild/hokey/javart-jRate.xml
