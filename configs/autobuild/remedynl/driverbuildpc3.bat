a3095f611ea00c329a10c1bc4369559703449fb1
Johnny Willemsen <jwillemsen@remedy.nl>
2003-07-11 10:56:10 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@921 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3.bat
+++ configs/autobuild/remedynl/driverbuildpc3.bat
@@ -4,10 +4,6 @@ sleep 4
 set CVS_RSH=plink
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl D:\ACE\autobuild\autobuild.pl BCB6DynamicDebugMbg.xml
-perl D:\ACE\autobuild\autobuild.pl BCB6DynamicDebugUnicodeMbg.xml
-perl D:\ACE\autobuild\autobuild.pl BCB6DynamicRelease.xml
-perl D:\ACE\autobuild\autobuild.pl BCB6DynamicReleaseUnicodeMbg.xml
 perl D:\ACE\autobuild\autobuild.pl BCB6StaticRelease.xml
 perl D:\ACE\autobuild\autobuild.pl Cygwin.xml
 perl D:\ACE\autobuild\autobuild.pl MingW.xml
--- configs/autobuild/remedynl/driverbuildpcreeuwijk.bat
+++ configs/autobuild/remedynl/driverbuildpcreeuwijk.bat
@@ -5,5 +5,7 @@ set CVS_RSH=plink
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl D:\ACE\autobuild\autobuild.pl BCB6DynamicDebugUnicodeMbg.xml
+perl D:\ACE\autobuild\autobuild.pl BCB6DynamicReleaseUnicodeMbg.xml
 perl D:\ACE\autobuild\autobuild.pl BCB6DynamicDebugMbg.xml
+perl D:\ACE\autobuild\autobuild.pl BCB6DynamicRelease.xml
 
