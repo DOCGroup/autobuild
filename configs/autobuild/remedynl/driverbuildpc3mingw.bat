286b0eb723759edcba6e2e84dbd0941c5e5e02e4
Johnny Willemsen <jwillemsen@remedy.nl>
2003-07-28 13:23:18 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@945 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3mingw.bat
+++ configs/autobuild/remedynl/driverbuildpc3mingw.bat
@@ -4,4 +4,6 @@ sleep 4
 set CVS_RSH=plink
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl D:\ACE\autobuild\autobuild.pl MingW.xml
+rem set MSYS_ROOT=d:\msys
+rem %MSYS_ROOT%\bin\sh --login -i -c "cd /d/ACE/autobuild/configs/autobuild/remedynl; perl D:/ACE/autobuild/autobuild.pl MingW.xml"
+d:\MSys\opt\perl\5.8.0\bin\MSWin32-x86-multi-thread\perl D:/ACE/autobuild/autobuild.pl MingW.xml
