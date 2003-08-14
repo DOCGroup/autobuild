9b5beea619fd1de3a9f125e6498c7aaf63bac5bc
Johnny Willemsen <jwillemsen@remedy.nl>
2003-08-14 14:59:06 +0000

    ChangeLogTag: Thu Aug 14 14:57:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1018 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverpc4.sh
+++ configs/autobuild/remedynl/driverpc4.sh
@@ -12,7 +12,7 @@ cd $HOME/ACE/autobuild
 
 cvs -z9 up -P -d
 
-$HOME/ACE/autobuild/configs/autobuild/remedynl/driverpc4kylixace.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/driverpc4a.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/driverpc4kylix.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/driverpc4gccace.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/driverpc4a.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/driverpc4kylixace.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/driverpc4kylix.sh
