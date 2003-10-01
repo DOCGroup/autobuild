7390d1dba5fd3704faf53fa73156528b229c1b14
Johnny Willemsen <jwillemsen@remedy.nl>
2003-10-01 10:33:05 +0000

    ChangeLogTag: Wed Oct 01 10:23:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1084 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3.bat
+++ configs/autobuild/remedynl/driverbuildpc3.bat
@@ -1,7 +1,5 @@
 cd ..\..\..\
-start d:\putty\pageant.exe d:\putty\privatenokey
-sleep 4
-set CVS_RSH=plink
+set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl D:\ACE\autobuild\autobuild.pl Cygwin.xml
