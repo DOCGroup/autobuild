050b38c1e0ca963abcb6d34d87d5474a424d3ae8
Johnny Willemsen <jwillemsen@remedy.nl>
2003-10-06 05:53:07 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1091 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc2.bat
+++ configs/autobuild/remedynl/driverbuildpc2.bat
@@ -1,8 +1,9 @@
+set CVS_RSH=ssh
+set CVSROOT=:ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
 cd ..\..\..\
 cvs -z9 up -P -d
 del D:\Cygwin\.disable
 del D:\.disable
 cd configs\autobuild\remedynl
 perl D:\autobuild\autobuild.pl CygwinGCC32.xml
-rem c:\cygwin\bin\sleep 1h
 dobuildpc2
