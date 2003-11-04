13f5b4e063b5414f33fdb1b80f92b5f37f9b835e
Johnny Willemsen <jwillemsen@remedy.nl>
2003-11-04 12:16:45 +0000

    ChangeLogTag: Wed Nov  4 12:16:12 UTC 2003  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1152 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/scp_GCC.sh
+++
@@ -1,5 +0,0 @@
-cd $HOME/ACE/GCC
-
-ssh bugzilla@charanga.cs.wustl.edu rm .www-docs/auto_compile_logs/remedy.nl_SuSE81_GCC/* -f
-
-scp -C *.txt *.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_SuSE81_GCC
