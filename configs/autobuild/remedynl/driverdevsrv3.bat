e66a855f396b9cea73499f89e03ef18d986d058d
Johnny Willemsen <jwillemsen@remedy.nl>
2003-11-12 08:31:31 +0000

    ChangeLogTag: Wed Nov 12 08:22:13 UTC 2003  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1172 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/driverdevsrv3.bat
@@ -0,0 +1,6 @@
+cd ..\..\..\
+set CVS_RSH=ssh
+cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\remedynl
+perl D:\ACE\autobuild\autobuild.pl msvcdebug.xml
+	
