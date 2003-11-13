2b0fcef0cfb53aa20d47dc8f5bb2a9e72b75b5ed
Don Hinton <dhinton@dre.vanderbilt.edu>
2003-11-13 22:29:42 +0000

    ChangeLogTag:Thu Nov 13 22:41:16 UTC 2003  Don Hinton  <dhinton@dresystems.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1183 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3.bat
+++ configs/autobuild/remedynl/driverbuildpc3.bat
@@ -4,7 +4,7 @@ cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl D:\ACE\autobuild\autobuild.pl Cygwin.xml
 perl D:\ACE\autobuild\autobuild.pl MingW.xml
-perl D:\ACE\autobuild\autobuild.pl VC7.xml
+perl D:\ACE\autobuild\autobuild.pl vc7.xml
 perl D:\ACE\autobuild\autobuild.pl BCB6StaticRelease.xml
 perl D:\ACE\autobuild\autobuild.pl cbxpreview.xml
 perl D:\ACE\autobuild\autobuild.pl cbxdd.xml
