ea9d1696e5ccea357d229f5e094f829adeb9c1eb
Johnny Willemsen <jwillemsen@remedy.nl>
2003-11-24 10:37:58 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1192 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3.bat
+++ configs/autobuild/remedynl/driverbuildpc3.bat
@@ -2,8 +2,8 @@ cd ..\..\..\
 set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl D:\ACE\autobuild\autobuild.pl Cygwin.xml
-perl D:\ACE\autobuild\autobuild.pl MingW.xml
+REM perl D:\ACE\autobuild\autobuild.pl Cygwin.xml
+REM perl D:\ACE\autobuild\autobuild.pl MingW.xml
 perl D:\ACE\autobuild\autobuild.pl vc7.xml
 perl D:\ACE\autobuild\autobuild.pl BCB6StaticRelease.xml
 perl D:\ACE\autobuild\autobuild.pl cbxpreview.xml
