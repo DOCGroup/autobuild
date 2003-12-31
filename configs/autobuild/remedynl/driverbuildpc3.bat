82dea5c6109d63b545c528224c35539b8a98fce3
Johnny Willemsen <jwillemsen@remedy.nl>
2003-12-31 10:56:30 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1263 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3.bat
+++ configs/autobuild/remedynl/driverbuildpc3.bat
@@ -2,10 +2,10 @@ cd ..\..\..\
 set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-REM perl D:\ACE\autobuild\autobuild.pl Cygwin.xml
-REM perl D:\ACE\autobuild\autobuild.pl MingW.xml
-perl D:\ACE\autobuild\autobuild.pl vc7.xml
-perl D:\ACE\autobuild\autobuild.pl BCB6StaticRelease.xml
-REM perl D:\ACE\autobuild\autobuild.pl cbxpreview.xml
-REM perl D:\ACE\autobuild\autobuild.pl cbxdd.xml
+perl C:\ACE\autobuild\autobuild.pl Cygwin.xml
+REM perl C:\ACE\autobuild\autobuild.pl MingW.xml
+perl C:\ACE\autobuild\autobuild.pl vc7.xml
+perl C:\ACE\autobuild\autobuild.pl BCB6StaticRelease.xml
+perl C:\ACE\autobuild\autobuild.pl cbxpreview.xml
+perl C:\ACE\autobuild\autobuild.pl cbxdd.xml
 	
--- configs/autobuild/remedynl/driverbuildpc3mingw.bat
+++ configs/autobuild/remedynl/driverbuildpc3mingw.bat
@@ -4,4 +4,4 @@ cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 rem set MSYS_ROOT=d:\msys
 rem %MSYS_ROOT%\bin\sh --login -i -c "cd /d/ACE/autobuild/configs/autobuild/remedynl; perl D:/ACE/autobuild/autobuild.pl MingW.xml"
-d:\MSys\opt\perl\5.8.0\bin\MSWin32-x86-multi-thread\perl D:/ACE/autobuild/autobuild.pl MingW.xml
+perl c:/ACE/autobuild/autobuild.pl MingW.xml
