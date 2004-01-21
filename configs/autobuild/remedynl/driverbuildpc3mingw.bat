46508ab33994c799d3f570c62ffd042a45c3c09c
Johnny Willemsen <jwillemsen@remedy.nl>
2004-01-21 08:53:58 +0000

    ChangeLogTag: Wed Jan 21 08:52:13 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1335 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3mingw.bat
+++ configs/autobuild/remedynl/driverbuildpc3mingw.bat
@@ -2,6 +2,4 @@ cd ..\..\..\
 set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-rem set MSYS_ROOT=d:\msys
-rem %MSYS_ROOT%\bin\sh --login -i -c "cd /d/ACE/autobuild/configs/autobuild/remedynl; perl D:/ACE/autobuild/autobuild.pl MingW.xml"
 perl c:/ACE/autobuild/autobuild.pl MingW.xml
