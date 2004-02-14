ec8f398fbf3c55d8312d186bdca7ae196aa97815
Bala Natarajan <bala@dre.vanderbilt.edu>
2004-02-14 21:23:35 +0000

    ChangeLogTag:Sat Feb 14 15:22:41 2004  Balachandran Natarajan  <bala@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1370 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/nirvana/new_driver.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+set "LIB=%LIB%;d:\ssl\openssl-0.9.6\out32dll"
+set "INCLUDE=%INCLUDE%;D:\ssl\openssl-0.9.6\inc32"
+set "CVSROOT=:ext:wustlbuilds:/project/cvs-repository"
+call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
+cd ..\..\..\
+cvs up -P -d
+cd configs\autobuild\nirvana
+if not exist Z: net use Z: \\TAO\bugzilla_www
+perl G:\bugzilla\autobuild\autobuild.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
