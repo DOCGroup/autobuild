738f99a18e55da4ac0865194a2a7919187b1ab5c
Bala Natarajan <bala@dre.vanderbilt.edu>
2004-02-14 22:26:31 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1371 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/nirvana/new_driver.bat
+++ configs/autobuild/nirvana/new_driver.bat
@@ -2,7 +2,7 @@
 setlocal
 set "LIB=%LIB%;d:\ssl\openssl-0.9.6\out32dll"
 set "INCLUDE=%INCLUDE%;D:\ssl\openssl-0.9.6\inc32"
-set "CVSROOT=:ext:wustlbuilds:/project/cvs-repository"
+set "CVSROOT=:ext:wustlbuilds@cvs:/project/cvs-repository"
 call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
 cd ..\..\..\
 cvs up -P -d
