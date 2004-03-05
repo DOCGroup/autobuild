480c11a7082bdc14404a04c0eabcc5553d0c5d20
Johnny Willemsen <jwillemsen@remedy.nl>
2004-03-05 11:04:49 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1412 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverdevsrv4.bat
+++ configs/autobuild/remedynl/driverdevsrv4.bat
@@ -3,4 +3,5 @@ set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl c:\Develop\autobuild\autobuild.pl MinGWTAO.xml
+dodevsrv4
 
