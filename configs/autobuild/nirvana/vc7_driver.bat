c11c699bc756212d2ecbe4e48467e744199fdeda
Bala Natarajan <bala@dre.vanderbilt.edu>
2004-04-11 04:29:28 +0000

    ChangeLogTag:Sat Apr 10 23:28:38 2004  Balachandran Natarajan  <bala@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1506 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/nirvana/vc7_driver.bat
+++ configs/autobuild/nirvana/vc7_driver.bat
@@ -1,8 +1,9 @@
 @echo off
 setlocal
-set "LIB=d:\ssl\openssl-0.9.6\out32dll"
-set "INCLUDE=D:\ssl\openssl-0.9.6\inc32"
-set "CVSROOT=:ext:wustlbuilds:/project/cvs-repository"
+set "LIB=%LIB%;d:\ssl\openssl-0.9.6\out32dll"
+set "INCLUDE=%INCLUDE%;D:\ssl\openssl-0.9.6\inc32"
+set "CVSROOT=:ext:wustlbuilds@cvs:/project/cvs-repository"
+set "CVS_RSH=plink"
 call "C:\Program Files\Microsoft Visual Studio .NET\Vc7\bin\vcvars32.bat"
 cd ..\..\..\
 cvs up -P -d
@@ -10,3 +11,6 @@ cd configs\autobuild\nirvana
 if not exist Z: net use Z: \\TAO\bugzilla_www
 perl G:\bugzilla\autobuild\autobuild.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
+
+
+
