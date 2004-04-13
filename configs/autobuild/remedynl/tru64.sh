41dde88288bf37e1975f611aa19767f1bc1da467
Johnny Willemsen <jwillemsen@remedy.nl>
2004-04-13 14:29:21 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1526 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/tru64.sh
+++ configs/autobuild/remedynl/tru64.sh
@@ -14,4 +14,4 @@ export PATH
 cvs -z9 up -P -d
 
 exec /usr//bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/Tru64.xml
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/tru64.xml
