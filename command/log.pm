62b870458038b75375f0d78c60796bc40feb8a98
Chad Elliott <elliott_c@ociweb.com>
2004-05-07 15:25:14 +0000

    ChangeLogTag: Fri May  7 10:23:06 2004  Chad Elliott  <elliott_c@ociweb.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1645 59e07337-6b18-0410-a141-c5efe34a0da7

--- command/log.pm
+++ command/log.pm
@@ -11,6 +11,7 @@ use Cwd;
 use FileHandle;
 use Time::Local;
 use File::Path;
+use File::Spec;
 
 ###############################################################################
 # Constructor
@@ -19,7 +20,8 @@ sub new
 {
     my $proto = shift;
     my $class = ref ($proto) || $proto;
-    my $self = {};
+    my $self = {'paused' => 0,
+               };
 
     bless ($self, $class);
     return $self;
@@ -52,25 +54,27 @@ sub Run ($)
 {
     my $self = shift;
     my $options = shift;
-    my $root = main::GetVariable ('root');
-    my $logfile = main::GetVariable ('log_file');
 
     # replace all '\x22' with '"'
     $options =~ s/\\x22/"/g;
 
-    if (!-r $root || !-d $root) {
-        mkpath($root);
-    }
-
-    # chop off trailing slash
-    if ($root =~ m/^(.*)\/$/) {
-        $root = $1;
-    }
-
-    # Force file paths to always be relative....
-    my $logpath = $root . '/' . $logfile;
-
     if (uc $options eq "ON") {
+        my $root = main::GetVariable ('root');
+
+        if (!-r $root || !-d $root) {
+            mkpath($root);
+        }
+
+        # chop off trailing slash
+        if ($root =~ m/^(.*)\/$/) {
+            $root = $1;
+        }
+
+        my $logfile = main::GetVariable ('log_file');
+
+        # Force file paths to always be relative....
+        my $logpath = $root . '/' . $logfile;
+
         # Make copies of current handles
         # Type "perlfunc -f open" to see an example of redirecting STDOUT
         open (OLDOUT, ">&STDOUT");
@@ -132,6 +136,61 @@ sub Run ($)
 
         if( $main::verbose == 1) {
             main::PrintStatus ('Setup', 'Logging: stdout/stderr restored');
+        }
+    }
+    elsif (uc $options eq "PAUSE") {
+        if ($self->{'paused'}) {
+            ## If we're paused and we get another pause, then we
+            ## restore logging.
+
+            # Restore the old handles
+
+            if (!open (STDERR, ">&PAUSEERR")) {
+                print OLDERR __FILE__, ": Error restoring ",
+                                       "stderr during pause: $!\n";
+                return 0;
+            }
+            if (!open (STDOUT, ">&PAUSEOUT")) {
+                print OLDERR __FILE__, ": Error restoring ",
+                                       "stdout during pause: $!\n";
+                return 0;
+            }
+
+            # Close the duplicate handles
+            if (!close (PAUSEOUT)) {
+                print STDERR __FILE__, ": Error closing PAUSEOUT: $!\n";
+                return 0;
+            }
+            if (!close (PAUSEERR)) {
+                print STDERR __FILE__, ": Error closing PAUSEERR: $!\n";
+                return 0;
+            }
+
+            ## The redirection was successful, now the
+            ## logs are no longer paused.
+            $self->{'pause'} = 0;
+        }
+        else {
+            open (PAUSEOUT, ">&STDOUT");
+            open (PAUSEERR, ">&STDERR");
+
+            # Redirect to the null device
+
+            if (!open (STDOUT, '>', File::Spec->devnull())) {
+                print STDERR __FILE__, ": Can't redirect stdout ",
+                                       "to null device: $!\n";
+                return 0;
+            }
+            if (!open (STDERR, ">&STDOUT")) {
+                print STDERR __FILE__, ": Can't dup stdout ",
+                                       "duing pause: $!\n";
+                return 0;
+            }
+            select(STDERR); $| = 1;    # make unbuffered
+            select(STDOUT); $| = 1;    # make unbuffered
+
+            ## The redirection was successful, now the logs are paused.
+            $self->{'paused'} = 1;
         }
     }
 
