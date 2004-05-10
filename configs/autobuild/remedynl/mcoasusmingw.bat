b5068b4ae13a88880da387505db6a98cf2c67263
Johnny Willemsen <jwillemsen@remedy.nl>
2004-05-10 08:42:01 +0000

    ChangeLogTag: Mon May 10 08:41:21 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1675 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/mcoasusmingw.bat
+++ configs/autobuild/remedynl/mcoasusmingw.bat
@@ -4,3 +4,5 @@ cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl c:/ACE/autobuild/autobuild.pl VC8.xml
 perl c:/ACE/autobuild/autobuild.pl MingWTAO.xml
+perl c:/ACE/autobuild/autobuild.pl MingW.xml
+perl c:/ACE/autobuild/autobuild.pl mingws.xml
