58787dea6363529ae7b46776ee07f4da2733af31
Frank Hunleth <fhunleth@cs.wustl.edu>
2004-05-17 15:34:30 +0000

    ChangeLogTag: Mon May 17 11:30:07 2004  Frank Hunleth  <frank@hunleth.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1708 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/slytherin/driver.sh
+++ configs/autobuild/slytherin/driver.sh
@@ -3,8 +3,15 @@
 # $Id$
 #
 
+cd $HOME/autobuild
 PATH=$HOME/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+CVSROOT=cvs.doc.wustl.edu:/project/cvs-repository
+CVS_RSH=ssh
+export CVSROOT
+export CVS_RSH
 export PATH
+
+cvs -z9 up -P -d
 
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
                    $HOME/autobuild/configs/autobuild/slytherin/OpenBSD_Core.xml 2>&1
