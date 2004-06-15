b1f71b694710e3f539a2f55c0a696e797790ac13
Johnny Willemsen <jwillemsen@remedy.nl>
2004-06-15 11:33:43 +0000

    ChangeLogTag: Tue Jun 15 08:50:31 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1764 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/tribeard.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd /build/ACE
+PATH=/usr/local/bin:$PATH
+CVSROOT=jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
+CVS_RSH=ssh
+export CVSROOT
+export CVS_RSH
+export PATH
+
+cvs -z9 up -P -d
+
+exec /opt/perl/bin/perl /build/ACE/autobuild/autobuild.pl \
+                             /build/ACE/autobuild/configs/autobuild/remedynl/hpux.xml
