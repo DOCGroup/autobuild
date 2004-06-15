427047080db345614c57d08de663bb9c03287a12
Johnny Willemsen <jwillemsen@remedy.nl>
2004-06-15 08:04:20 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1753 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/gandalf_vc71.bat
+++ configs/autobuild/remedynl/gandalf_vc71.bat
@@ -2,5 +2,4 @@ cd ..\..\..\
 set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl BCB6CodeGuard.xml
-
+perl C:\ACE\autobuild\autobuild.pl vc71.xml
--- configs/autobuild/remedynl/uploadbuildpc3.bat
+++
@@ -1,3 +0,0 @@
-rem This batch file is used to upload all logfiles manually on buildpc3 in case of a network failure
-set CVS_RSH=ssh
-scp -C D:\ACE\MingW\*.txt D:\ACE\MingW\*.html bugzilla@charanga.cs.wustl.edu:.www-docs/auto_compile_logs/remedy.nl_Win2K_MingW
