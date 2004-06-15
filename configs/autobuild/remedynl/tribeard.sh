d9423a24498becdef433aa1a324b682667020d5a
Johnny Willemsen <jwillemsen@remedy.nl>
2004-06-15 11:37:46 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1766 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/tribeard.sh
+++ configs/autobuild/remedynl/tribeard.sh
@@ -5,7 +5,7 @@
 
 cd /home/build/ACE
 PATH=/usr/local/bin:$PATH
-CVSROOT=jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
+CVSROOT=:ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
 CVS_RSH=ssh
 export CVSROOT
 export CVS_RSH
