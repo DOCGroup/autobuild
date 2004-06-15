e2482bf725976e8f05eeced5020fb5738accca95
Johnny Willemsen <jwillemsen@remedy.nl>
2004-06-15 09:40:22 +0000

    ChangeLogTag: Tue Jun 15 08:50:31 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1760 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/gandalf_cbxddu.bat
+++ configs/autobuild/remedynl/gandalf_cbxddu.bat
@@ -2,9 +2,4 @@ cd ..\..\..\
 set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl Cygwin.xml
 perl C:\ACE\autobuild\autobuild.pl cbxddu.xml
-perl C:\ACE\autobuild\autobuild.pl cbxdd.xml
-perl C:\ACE\autobuild\autobuild.pl cbxsr.xml
-perl C:\ACE\autobuild\autobuild.pl vc71.xml
-
