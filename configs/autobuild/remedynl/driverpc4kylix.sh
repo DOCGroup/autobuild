c11e0cd33ea89950af15a23693c4f91b628f3e2c
Johnny Willemsen <jwillemsen@remedy.nl>
2004-06-25 14:58:05 +0000

    ChangeLogTag: Fri Jun 25 14:56:31 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1816 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverpc4kylix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-
-exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/Kylix3.xml
--- configs/autobuild/remedynl/drivervxworks.bat
+++
@@ -1,7 +0,0 @@
-del c:\ACE\VxWorks\.disable
-cd ..\..\..\
-set CVS_RSH=ssh
-cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
-cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl VxWorks.xml
-
