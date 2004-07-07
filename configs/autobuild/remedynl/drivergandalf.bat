47e785b1b1f8f737b843ec01888caccbd19af598
Johnny Willemsen <jwillemsen@remedy.nl>
2004-07-07 05:32:30 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1852 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/drivergandalf.bat
+++ configs/autobuild/remedynl/drivergandalf.bat
@@ -7,4 +7,5 @@ perl C:\ACE\autobuild\autobuild.pl cbxddu.xml
 perl C:\ACE\autobuild\autobuild.pl cbxdd.xml
 perl C:\ACE\autobuild\autobuild.pl cbxsr.xml
 perl C:\ACE\autobuild\autobuild.pl vc71.xml
-
+perl C:\ACE\autobuild\autobuild.pl gandalfvc71dr.xml
+perl C:\ACE\autobuild\autobuild.pl gandalfvc71sd.xml
