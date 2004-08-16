57870357aeb3ce180422422ad361488f0dd21e47
Johnny Willemsen <jwillemsen@remedy.nl>
2004-08-16 12:58:27 +0000

    ChangeLogTag: Mon Aug 16 06:27:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1970 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/scarab_driver.bat
@@ -0,0 +1,5 @@
+cd ..\..\..\
+set CVS_RSH=ssh
+cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\remedynl
+rem perl C:\ACE\autobuild\autobuild.pl Cygwin.xml
