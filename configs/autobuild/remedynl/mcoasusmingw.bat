f2a72737ef40edbb6e6c6f2f8ca39026668b3661
Johnny Willemsen <jwillemsen@remedy.nl>
2004-08-18 14:10:18 +0000

    ChangeLogTag:  Wed Aug 18 14:10:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1978 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/mcoasusmingw.bat
+++
@@ -1,7 +0,0 @@
-cd ..\..\..\
-set CVS_RSH=ssh
-cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
-cd configs\autobuild\remedynl
-perl c:/ACE/autobuild/autobuild.pl MingWTAO.xml
-perl c:/ACE/autobuild/autobuild.pl MingW.xml
-perl c:/ACE/autobuild/autobuild.pl mingws.xml
