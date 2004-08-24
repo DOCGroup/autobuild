23d94e94fdf1c02ccf1d33bcf26f3e65287190e4
Trevor Fields <fields_t@ociweb.com>
2004-08-24 19:33:14 +0000

    ChangeLogTag:  Trevor Fields  <fields_t@ociweb.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1990 59e07337-6b18-0410-a141-c5efe34a0da7

--- testmatrix/update_scoreboard.sh
+++ testmatrix/update_scoreboard.sh
@@ -24,33 +24,27 @@ function update_local_scoreboard()
   # list of builds that was updated by the make
   BUILD_LIST=$TEST_MATRIX_DIR/ace-list
 
-  # full name of the simple test matrix html page
-  SIMPLE_TEST_MATRIX=ace_simple_matrix.html
-
-  # beginning of the name of the fancy test matrix
+  # beginning of the name of the test matrix
   # the name will have ".matrix.html" appended to it
-  COMPLEX_TEST_MATRIX=ace_detailed
+  TEST_MATRIX=ace_detailed
 
   # generate the matrices
-  ./buildMatrix $BUILD_LIST $SIMPLE_TEST_MATRIX $COMPLEX_TEST_MATRIX
+  ./buildMatrix $BUILD_LIST $TEST_MATRIX
 
 
   BUILD_LIST=$TEST_MATRIX_DIR/ace_future-list
-  SIMPLE_TEST_MATRIX=ace_future_simple_matrix.html
-  COMPLEX_TEST_MATRIX=ace_future_detailed
-  ./buildMatrix $BUILD_LIST $SIMPLE_TEST_MATRIX $COMPLEX_TEST_MATRIX
+  TEST_MATRIX=ace_future_detailed
+  ./buildMatrix $BUILD_LIST $TEST_MATRIX
 
 
   BUILD_LIST=$TEST_MATRIX_DIR/tao-list
-  SIMPLE_TEST_MATRIX=tao_simple_matrix.html
-  COMPLEX_TEST_MATRIX=tao_detailed
-  ./buildMatrix $BUILD_LIST $SIMPLE_TEST_MATRIX $COMPLEX_TEST_MATRIX
+  TEST_MATRIX=tao_detailed
+  ./buildMatrix $BUILD_LIST $TEST_MATRIX
 
 
   BUILD_LIST=$TEST_MATRIX_DIR/tao_future-list
-  SIMPLE_TEST_MATRIX=tao_future_simple_matrix.html
-  COMPLEX_TEST_MATRIX=tao_future_detailed
-  ./buildMatrix $BUILD_LIST $SIMPLE_TEST_MATRIX $COMPLEX_TEST_MATRIX
+  TEST_MATRIX=tao_future_detailed
+  ./buildMatrix $BUILD_LIST $TEST_MATRIX
 
 
   cp -f matrix.css $LOG_DIR/matrix.css
