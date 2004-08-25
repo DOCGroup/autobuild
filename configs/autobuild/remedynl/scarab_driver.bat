c043388451a9806ef2add7b3f45a5544deb7f163
Johnny Willemsen <jwillemsen@remedy.nl>
2004-08-25 06:40:22 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@1994 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/scarab_driver.bat
+++ configs/autobuild/remedynl/scarab_driver.bat
@@ -1,3 +1,7 @@
+if exist c:\ACE\.disable goto _Exit
+
+echo Yes > C:\ACE\.disable
+
 cd ..\..\..\
 set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
@@ -7,3 +11,11 @@ perl C:\ACE\autobuild\autobuild.pl scarab_bcb6ddu.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_bcb6dr.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_bcb6dd.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_vc8.xml
+perl C:\ACE\autobuild\autobuild.pl scarab_MingW.xml
+perl C:\ACE\autobuild\autobuild.pl scarab_MingWTAO.xml
+perl C:\ACE\autobuild\autobuild.pl scarab_MingWs.xml
+
+del /f/q C:\ACE\.disable
+
+:_Exit
+
