c1cdf3695b1ff5e7585b24aa140845be061b499b
Johnny Willemsen <jwillemsen@remedy.nl>
2004-09-17 07:45:48 +0000

    ChangeLogTag:  Fri Sep 17 07:44:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2022 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/driverbuildpc3.bat
+++
@@ -1,6 +0,0 @@
-cd ..\..\..\
-set CVS_RSH=ssh
-cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
-cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl BCB6CodeGuard.xml
-
--- configs/autobuild/remedynl/driverdevsrv4.bat
+++
@@ -1,7 +0,0 @@
-cd \develop\autobuild
-set CVS_RSH=ssh
-cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
-cd configs\autobuild\remedynl
-perl c:\Develop\autobuild\autobuild.pl MingWTAO.xml
-dodevsrv4
-
