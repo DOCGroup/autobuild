e08baaae8933d3ea1827714d7721aefd1684f02d
Johnny Willemsen <jwillemsen@remedy.nl>
2004-09-28 13:55:50 +0000

    ChangeLogTag: Tue Sep 28 11:28:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2078 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/drivergandalf.bat
+++ configs/autobuild/remedynl/drivergandalf.bat
@@ -2,10 +2,10 @@ cd ..\..\..\
 set CVS_RSH=ssh
 cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl Cygwin.xml
-perl C:\ACE\autobuild\autobuild.pl cbxddu.xml
-perl C:\ACE\autobuild\autobuild.pl cbxdd.xml
-perl C:\ACE\autobuild\autobuild.pl cbxsr.xml
+perl C:\ACE\autobuild\autobuild.pl gandalf_cygwin.xml
+perl C:\ACE\autobuild\autobuild.pl gandalf_cbxddu.xml
+perl C:\ACE\autobuild\autobuild.pl gandalf_cbxdd.xml
+perl C:\ACE\autobuild\autobuild.pl gandalf_cbxsr.xml
 perl C:\ACE\autobuild\autobuild.pl gandalf_vc71.xml
 perl C:\ACE\autobuild\autobuild.pl gandalfvc71dr.xml
 perl C:\ACE\autobuild\autobuild.pl gandalfvc71sd.xml
