c3847c07caafd0684fce433de0ae03b8869bcbd8
Johnny Willemsen <jwillemsen@remedy.nl>
2004-11-17 10:57:15 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2206 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/legolas_cross_host_vc71.bat
+++ configs/autobuild/remedynl/legolas_cross_host_vc71.bat
@@ -2,4 +2,4 @@ cd ..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
 c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl cross_host_vc71.xml
+perl C:\ACE\autobuild\autobuild.pl legolas_cross_host_vc71.xml
--- configs/autobuild/remedynl/legolas_vxworks-ppc85xx-gnu.bat
+++ configs/autobuild/remedynl/legolas_vxworks-ppc85xx-gnu.bat
@@ -2,4 +2,4 @@ cd ..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
 c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl vxworks-ppc85xx-gnu.xml
+perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc85xx-gnu.xml
