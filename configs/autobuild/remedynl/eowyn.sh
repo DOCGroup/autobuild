a03291ad1a839402c0156b41b5a6d7a5dfabe13b
Johnny Willemsen <jwillemsen@remedy.nl>
2004-11-17 15:04:20 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2213 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/eowyn.sh
+++ configs/autobuild/remedynl/eowyn.sh
@@ -15,3 +15,4 @@ cvs -z9 up -P -d
 $HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_gcc.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_pgi.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_poarefactor.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_gccwarn.sh
