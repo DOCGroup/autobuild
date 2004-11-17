b8873f7bee62a0d3c346601a809464761dc1a000
Martin Corino <mcorino@remedy.nl>
2004-11-17 10:24:34 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2203 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/legolas_cross_host_vc71.bat
+++ configs/autobuild/remedynl/legolas_cross_host_vc71.bat
@@ -1,5 +1,5 @@
 cd ..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
-cvs -d :ext:mcorino@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+c:\cygwin\bin\cvs -d :ext:mcorino@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl cross_host_vc71.xml
