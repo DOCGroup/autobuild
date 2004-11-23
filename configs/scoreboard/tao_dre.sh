4f7af550da08239841eca2e525f931c76fd5988f
Carlos O'Ryan <coryan@atdesk.com>
2004-11-23 01:08:01 +0000

    Mon Nov 22 20:05:03 2004  Carlos O'Ryan  <coryan@atdesk.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2258 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/tao_dre.sh
+++ configs/scoreboard/tao_dre.sh
@@ -11,22 +11,29 @@ export CVS_RSH
 cd $HOME/autobuild
 cvs up
 
-#exec /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c $HOME/autobuild/configs/scoreboard/acetao.xml 2>&1 &
+# /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c $HOME/autobuild/configs/scoreboard/acetao.xml 2>&1 &
 
 # Generate the index page!
-exec /usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
+/usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
 # Generate other pages!
-exec /usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace.xml -o ace.html  2>&1 &
-exec /usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace_future.xml -o ace_future.html 2>&1 &
-exec /usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao.xml -o tao.html 2>&1  &
-exec /usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao_future.xml -o tao_future.html 2>&1 &
-exec /usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/misc.xml -o misc.html 2>&1 &
+/usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace.xml -o ace.html  2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace_future.xml -o ace_future.html 2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao.xml -o tao.html 2>&1  &
+/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao_future.xml -o tao_future.html 2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/misc.xml -o misc.html 2>&1 &
 
 # Generate the test matrices!
-exec testmatrix/update_scoreboard.sh 2>&1 &
+testmatrix/update_scoreboard.sh 2>&1 &
 
 # Generate integrated pages!
-exec /usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -z 2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -z 2>&1 &
 
 #Generate build matrix
-exec /usr/bin/perl buildmatrix/buildmatrix.pl $HOME/autobuild/configs/scoreboard/ace.xml /project/taotmp/scoreboard/html > /project/taotmp/scoreboard/buildmatrix/output.html 2> /tmp/build.out;
+/usr/bin/perl buildmatrix/buildmatrix.pl $HOME/autobuild/configs/scoreboard/ace.xml /project/taotmp/scoreboard/html > /project/taotmp/scoreboard/buildmatrix/output.html 2> /tmp/build.out
+
+/usr/bin/perl buildmatrix/buildmatrix.pl $HOME/autobuild/configs/scoreboard/tao.xml /project/taotmp/scoreboard/html > /project/taotmp/scoreboard/buildmatrix/tao.html 2> /tmp/build.out
+
+# 
+wait
+
+exit 0
