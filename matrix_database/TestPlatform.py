dd9850e462775ddcba7665d4b7b29cb1a463388c
Trevor Fields <fields_t@ociweb.com>
2004-12-23 19:28:20 +0000

    handling testnames with parameters properly
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2327 59e07337-6b18-0410-a141-c5efe34a0da7

--- matrix_database/TestPlatform.py
+++ matrix_database/TestPlatform.py
@@ -196,17 +196,9 @@ class TestPlatform:
 			if len(tokens) == 3:
 				ttime = "" 
 				tresult = "" 
-			elif len(tokens) == 4:
-				ttime = tokens[2]
-				tresult = tokens[3]
-			elif len(tokens) == 5:
-				ttime = tokens[3]
-				tresult = tokens[4]
 			else:
 				ttime = tokens[len(tokens) - 2]
 				tresult = tokens[len(tokens) - 1]
-				print "ERROR: What is happening here?"
-#				sys.exit(-1)
 			if ttime != "":
 				e,f=ttime.split(":")
 				time=int(f.replace('s',''))
