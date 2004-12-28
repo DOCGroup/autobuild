9e5baa5693dba027e7bae553e71a682d23a633c9
Johnny Willemsen <jwillemsen@remedy.nl>
2004-12-28 08:56:43 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2333 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/legolas_vxworks-pentium-gnu.bat
@@ -0,0 +1,5 @@
+cd ..\..\..\
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\remedynl
+perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-pentium-gnu.xml
