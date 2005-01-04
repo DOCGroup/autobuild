d795e731a5bd0c4037eac84d26916bdb9b4b8cae
Bala Natarajan <bala@dre.vanderbilt.edu>
2005-01-04 04:53:13 +0000

    ChangeLogTag:Mon Jan  3 22:51:46 2005  Balachandran Natarajan  <bala@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2357 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/corona/driver_studio.sh
@@ -0,0 +1,14 @@
+#!/pkg/gnu/bin/bash
+#
+# $Id$
+#
+
+PATH=/opt/csw/bin:/opt/SUNWspro:$PATH
+export PATH
+
+# OpenSSL requires an explicit source of entropy on Solaris.  The
+# Entropy Gathering Daemon perl script fulfills that requirement.
+
+exec $HOME/autobuild/autobuild.pl \
+     $HOME/autobuild/configs/autobuild/carona/SunOS9_Sun_Studio9.xml 2>&1 
+
