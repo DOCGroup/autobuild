3239ac925127adcd9dba3b4764ba70bf9bbee60c
Johnny Willemsen <jwillemsen@remedy.nl>
2005-01-16 18:29:02 +0000

    ChangeLogTag: Sat Jan 15 22:22:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2375 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/aragon_driver.bat
@@ -0,0 +1,10 @@
+cd \ace\autobuild
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\remedynl
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvcdebug.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvclibdebug.xml
+perl c:\ACE\autobuild\autobuild.pl aragon_msvclibrelease.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvcunicodedebug.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvcrelease.xml
+
