0a0253f042083e0f0609f28d75e96bae0105b9d6
Johnny Willemsen <jwillemsen@remedy.nl>
2005-01-17 17:48:25 +0000

    ChangeLogTag: Mon Jan 17 11:04:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2377 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/aragon_msvcunicodedebug.bat
+++ configs/autobuild/remedynl/aragon_msvcunicodedebug.bat
@@ -2,9 +2,5 @@ cd \ace\autobuild
 set CVS_RSH=c:\cygwin\bin\ssh
 c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl c:\ACE\autobuild\autobuild.pl aragon_msvcdebug.xml
-perl c:\ACE\autobuild\autobuild.pl aragon_msvclibdebug.xml
-perl c:\ACE\autobuild\autobuild.pl aragon_msvclibrelease.xml
 perl c:\ACE\autobuild\autobuild.pl aragon_msvcunicodedebug.xml
-perl c:\ACE\autobuild\autobuild.pl aragon_msvcrelease.xml
 
