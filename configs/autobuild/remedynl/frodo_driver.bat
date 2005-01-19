a717d30f4087efa666afd493591f7f4af5ad21f7
Johnny Willemsen <jwillemsen@remedy.nl>
2005-01-19 11:53:45 +0000

    ChangeLogTag: Tue Jan 18 19:59:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2383 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/frodo_driver.bat
+++ configs/autobuild/remedynl/frodo_driver.bat
@@ -2,9 +2,9 @@ cd \ace\autobuild
 set CVS_RSH=c:\cygwin\bin\ssh
 c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl c:\ACE\autobuild\autobuild.pl frodo_msvcdebug.xml
-perl c:\ACE\autobuild\autobuild.pl frodo_msvclibdebug.xml
-perl c:\ACE\autobuild\autobuild.pl frodo_msvclibrelease.xml
-perl c:\ACE\autobuild\autobuild.pl frodo_msvcunicodedebug.xml
-perl c:\ACE\autobuild\autobuild.pl frodo_msvcrelease.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvcdebug.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvclibdebug.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvclibrelease.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvcunicodedebug.xml
+rem perl c:\ACE\autobuild\autobuild.pl frodo_msvcrelease.xml
 
