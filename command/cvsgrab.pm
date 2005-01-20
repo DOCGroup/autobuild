98795396bfbbf12ac568067d3719c2455ae90a03
Bala Natarajan <bala@dre.vanderbilt.edu>
2005-01-20 12:41:43 +0000

    ChangeLogTag:Thu Jan 20 06:40:55 2005  Balachandran Natarajan  <bala@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2389 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ command/cvsgrab.pm
@@ -0,0 +1,87 @@
+#
+# $Id$
+#
+
+package CVSGrab;
+
+use strict;
+use warnings;
+
+use Cwd;
+use FileHandle;
+use File::Path;
+
+###############################################################################
+# Constructor
+
+sub new
+{
+    my $proto = shift;
+    my $class = ref ($proto) || $proto;
+    my $self = {};
+
+    bless ($self, $class);
+    return $self;
+}
+
+##############################################################################
+
+sub CheckRequirements ()
+{
+    my $self = shift;
+    my $root = main::GetVariable ('root');
+
+    if (!defined $root) {
+        print STDERR __FILE__, ": Requires \"root\" variable\n";
+        return 0;
+    }
+
+    return 1;
+}
+
+##############################################################################
+
+sub Run ($)
+{
+    my $self = shift;
+    my $options = shift;
+    my $root = main::GetVariable ('root');
+
+    # replace all '\x22' with '"'
+    $options =~ s/\\x22/"/g;
+
+    if (!-r $root || !-d $root) {
+        mkpath($root);
+    }
+
+    # chop off trailing slash
+    if ($root =~ m/^(.*)\/$/) {
+        $root = $1;
+    }
+
+    main::PrintStatus ('Setup', 'CVSGrab');
+
+    my $current_dir = getcwd ();
+
+    if (!chdir $root) {
+        print STDERR __FILE__, ": Cannot change to $root\n";
+        return 0;
+    }
+
+    my $cvs_program = main::GetVariable ('cvs_program');
+    if (! defined $cvs_program) {
+        # The "cvs_program" variable was not defined in the
+        # XML config file.  Default to using a program called "cvs".
+        $cvs_program = "cvsgrab"
+    }
+
+    system ("$cvs_program $options");
+
+    chdir $current_dir;
+
+    return 1;
+}
+
+##############################################################################
+
+main::RegisterCommand ("cvsgrab", new CVS ());
