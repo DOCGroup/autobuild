ba9fbfd0ecde069e1a8b1be8689028b7be90ed91
isisbuilds <isisbuilds>
2005-03-02 18:34:08 +0000

    Fixed the autobuild scripts
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2471 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/tao_dre.sh
+++ configs/scoreboard/tao_dre.sh
@@ -14,24 +14,24 @@ cvs up
 # /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c $HOME/autobuild/configs/scoreboard/acetao.xml 2>&1 &
 
 # Generate the index page!
-/usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
+/usr/bin/perl ./scoreboard.pl -v -d /export/web/www/scoreboard -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
 # Generate other pages!
-/usr/bin/perl ./scoreboard.pl -v -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace.xml -o ace.html  2>&1 &
-/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/ace_future.xml -o ace_future.html 2>&1 &
-/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao.xml -o tao.html 2>&1  &
-/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/tao_future.xml -o tao_future.html 2>&1 &
-/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -f $HOME/autobuild/configs/scoreboard/misc.xml -o misc.html 2>&1 &
+/usr/bin/perl ./scoreboard.pl -v -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ace.xml -o ace.html  2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ace_future.xml -o ace_future.html 2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/tao.xml -o tao.html 2>&1  &
+/usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/tao_future.xml -o tao_future.html 2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/misc.xml -o misc.html 2>&1 &
 
 # Generate the test matrices!
 testmatrix/update_scoreboard.sh 2>&1 &
 
 # Generate integrated pages!
-/usr/bin/perl ./scoreboard.pl -d /project/taotmp/scoreboard/html -z 2>&1 &
+/usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -z 2>&1 &
 
 #Generate build matrix
-#/usr/bin/perl buildmatrix/buildmatrix.pl $HOME/autobuild/configs/scoreboard/ace.xml /project/taotmp/scoreboard/html 1 > /project/taotmp/scoreboard/buildmatrix/output.html 2> /tmp/build.out
+#/usr/bin/perl buildmatrix/buildmatrix.pl $HOME/autobuild/configs/scoreboard/ace.xml /export/web/www/scoreboard 1 > /project/taotmp/scoreboard/buildmatrix/output.html 2> /tmp/build.out
 
-#/usr/bin/perl buildmatrix/buildmatrix.pl $HOME/autobuild/configs/scoreboard/tao.xml /project/taotmp/scoreboard/html 1 > /project/taotmp/scoreboard/buildmatrix/tao.html 2> /tmp/build.out
+#/usr/bin/perl buildmatrix/buildmatrix.pl $HOME/autobuild/configs/scoreboard/tao.xml /export/web/www/scoreboard 1 > /project/taotmp/scoreboard/buildmatrix/tao.html 2> /tmp/build.out
 
 #Remove the obsolete db files and give the list of available db files.
 matrix_database/RemoveAndListCompilationDbFiles.sh
