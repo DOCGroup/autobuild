c19f31519e3150a26aedb0ef2267876d4cab9caa
Johnny Willemsen <jwillemsen@remedy.nl>
2005-04-23 14:58:07 +0000

    ChangeLogTag: Fri Apr 22 20:32:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2578 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/aix51vac.sh
+++ configs/autobuild/remedynl/aix51vac.sh
@@ -6,5 +6,5 @@
 cd $HOME/ACE/autobuild
 
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_icc.xml
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/aix51vac.xml
 
