98ea96721a2abb148bb12f7fabc3774c467ab6ec
Ossama Othman <othmano@ociweb.com>
2005-05-04 02:51:19 +0000

    ChangeLogTag:Tue May  3 19:47:43 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2608 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/pantera_isis/gcc-4.0.sh
+++ configs/autobuild/pantera_isis/gcc-4.0.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #
 # $Id$
-#
 
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
-  $HOME/autobuild/configs/autobuild/flamenco/gcc-4.0.xml
-
+  $HOME/autobuild/configs/autobuild/pantera_isis/gcc-4.0.xml
