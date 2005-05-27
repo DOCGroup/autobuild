6dc30e2d6a1b49fd4d00d006c06e967d72a33ecc
Emre Turkay <turkaye@dre.vanderbilt.edu>
2005-05-27 01:20:54 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2660 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/merengue/merengue_driver.bat
+++ configs/autobuild/merengue/merengue_driver.bat
@@ -1,4 +1,10 @@
 rem $Id$
 rem perl C:\ACE\autobuild\autobuild.pl merengue_vc71.xml
+
+cd ..\..\..\
+set CVS_RSH=plink
+cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\merengue
+
 call merengue_vc71.bat
 
--- configs/autobuild/merengue/merengue_vc71.bat
+++ configs/autobuild/merengue/merengue_vc71.bat
@@ -1,5 +1,3 @@
-cd ..\..\..\
-set CVS_RSH=plink
-cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
-cd configs\autobuild\merengue
+rem $Id$
+
 perl C:\ACE\autobuild\autobuild.pl merengue_vc71.xml
