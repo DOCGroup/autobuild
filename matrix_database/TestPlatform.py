97c31ba55e1b0a086a5414178ef27901a99899e3
Chad Elliott <elliott_c@ociweb.com>
2005-06-08 14:53:47 +0000

    ChangeLogTag: Wed Jun  8 09:51:22 2005  Chad Elliott  <elliott_c@ociweb.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2676 59e07337-6b18-0410-a141-c5efe34a0da7

--- matrix_database/TestPlatform.py
+++ matrix_database/TestPlatform.py
@@ -12,7 +12,7 @@ from DBConfig import *
 
 #patterns for log file
 compile_error = re.compile(r'error',re.IGNORECASE)
-test_error = re.compile(r'Error|ERROR|fatal|EXCEPTION|ACE_ASSERT|Assertion|Mismatched free|are definitely lost in loss record|Invalid write of size|Invalid read of size|pure virtual ')
+test_error = re.compile(r'Error|ERROR|fatal|EXCEPTION|ACE_ASSERT|Assertion|Mismatched free|are definitely lost in loss record|: parse error|Invalid write of size|Invalid read of size|pure virtual ')
 ace_test_start = re.compile (r'Running')
 build_begin = re.compile(r'#################### Begin');
 build_end   = re.compile(r'#################### End');
