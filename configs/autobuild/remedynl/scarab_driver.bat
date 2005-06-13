27a6db60ccd8335aaab13c51740aba158bbc32e5
Johnny Willemsen <jwillemsen@remedy.nl>
2005-06-13 19:43:55 +0000

    ChangeLogTag: Mon Jun 13 18:51:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2690 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/scarab_driver.bat
+++ configs/autobuild/remedynl/scarab_driver.bat
@@ -1,6 +1,4 @@
-perl C:\ACE\autobuild\autobuild.pl scarab_bcb6dru.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_bcb6ddu.xml
-perl C:\ACE\autobuild\autobuild.pl scarab_bcb6dr.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_bcb6dd.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_vc8.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_MingWTAO.xml
