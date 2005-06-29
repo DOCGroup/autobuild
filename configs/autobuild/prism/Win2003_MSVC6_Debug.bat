9e097ae7fca3719c11e84cdb9c7078a83908d50d
Simon Massey <simon.massey@prismtech.com>
2005-06-29 13:41:34 +0000

    First draft.
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2712 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/prism/Win2003_MSVC6_Debug.bat
@@ -0,0 +1,9 @@
+call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
+f:
+cd \Overnight_Builds\DOC_Group\autobuild
+set CVSROOT=:ext:sma@cvs.doc.wustl.edu:/project/cvs-repository
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -z9 up -P -d
+cd configs\autobuild\prism
+perl f:\Overnight_Builds\DOC_Group\autobuild\autobuild.pl Win2003_MSVC6_Debug.xml
+
