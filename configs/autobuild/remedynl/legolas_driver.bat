e927eba9cdd467991faafcfa6bc9476d1f0aeb34
Johnny Willemsen <jwillemsen@remedy.nl>
2005-07-27 08:43:10 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2734 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/legolas_driver.bat
+++ configs/autobuild/remedynl/legolas_driver.bat
@@ -3,8 +3,9 @@ set CVS_RSH=c:\cygwin\bin\ssh
 c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl legolas_cross_host_vc71.xml
-perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc85xx-gnu.xml
-perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc604-gnu.xml
-perl C:\ACE\autobuild\autobuild.pl legolas_cross_host_vc71.xml
-perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-pentium-gnu.xml
-perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-pentium-pthread-gnu.xml
+REM perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc85xx-gnu.xml
+REM perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc603-gnu.xml
+call legolas_vxworks-ppc603-gnu_run.bat
+REM perl C:\ACE\autobuild\autobuild.pl legolas_cross_host_vc71.xml
+REM perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-pentium-gnu.xml
+REM perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-pentium-pthread-gnu.xml
