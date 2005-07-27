7f6b686cd850ce22c44ca0314ebd2ea9c0a161b8
Martin Corino <mcorino@remedy.nl>
2005-07-27 09:55:47 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2736 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/theoden_vxworks-simnt-gnu.bat
@@ -0,0 +1,5 @@
+cd ..\..\..\
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:mcorino@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\remedynl
+perl C:\ACE\autobuild\autobuild.pl theoden_vxworks-simnt-gnu.xml
