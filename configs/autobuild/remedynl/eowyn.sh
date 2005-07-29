c5893abe2db363226dc70ba94c1a135c14f47ef4
Johnny Willemsen <jwillemsen@remedy.nl>
2005-07-29 11:09:21 +0000

    ChangeLogTag: Fri Jul 29 11:02:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2741 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/eowyn.sh
+++ configs/autobuild/remedynl/eowyn.sh
@@ -12,6 +12,7 @@ cd $HOME/ACE/autobuild
 
 cvs -z9 up -P -d
 
+$HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_any.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_gcc.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_gccwarn.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/eowyn_ipv6wchar.sh
