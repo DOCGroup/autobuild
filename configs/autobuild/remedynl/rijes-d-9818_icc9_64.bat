086090a366f41639fb3d8593cb2cc1bcfb5e7767
Johnny Willemsen <jwillemsen@remedy.nl>
2005-08-29 09:55:34 +0000

    ChangeLogTag: Mon Aug 29 09:31:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2787 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/rijes-d-9818_icc9_64.bat
+++ configs/autobuild/remedynl/rijes-d-9818_icc9_64.bat
@@ -2,4 +2,4 @@ cd ..\..\..\
 set CVS_RSH=d:\cygwin\bin\ssh
 d:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl rijes-d-9818_icc9_64.xml
+perl D:\ACE\autobuild\autobuild.pl rijes-d-9818_icc9_64.xml
