bfe17b8a352e22d6ed7f10f949cfa8612f9264e5
Johnny Willemsen <jwillemsen@remedy.nl>
2005-09-28 07:23:06 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2862 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/legolas_driver.bat
+++ configs/autobuild/remedynl/legolas_driver.bat
@@ -3,8 +3,7 @@ set CVS_RSH=c:\cygwin\bin\ssh
 c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl legolas_cross_host_vc71.xml
-REM perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc85xx-gnu.xml
-REM perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc603-gnu.xml
+perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc603-gnu-sharedlib.xml
 call legolas_vxworks-ppc603-gnu_run.bat
 REM perl C:\ACE\autobuild\autobuild.pl legolas_cross_host_vc71.xml
 REM perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-pentium-gnu.xml
---
+++ configs/autobuild/remedynl/legolas_vxworks-ppc603-gnu_test.bat
@@ -0,0 +1,10 @@
+@setlocal
+
+set PATH=%PATH%;c:\tools\pstools;c:\tornado2.2\host\x86-win32\bin
+pskill -t tgtsvr.exe
+start tgtsvr.exe 10.2.128.61 -n RLM -V -m 61685760 -B wdbrpc -R C:\ -RW -redirectIO -C
+perl C:\ACE\autobuild\autobuild.pl legolas_vxworks-ppc603-gnu_test.xml
+pskill -t tgtsvr.exe
+REM cmd /k
+
+@endlocal
