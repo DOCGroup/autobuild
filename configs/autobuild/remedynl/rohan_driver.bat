af715f36f2f0082ac60584b051d728583fd94c9a
Martin Corino <mcorino@remedy.nl>
2005-10-04 11:57:30 +0000

    new files
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2875 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/rohan_driver.bat
@@ -0,0 +1,8 @@
+cd ..\..\..\
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+
+cd configs\autobuild\remedynl
+
+perl C:\ACE\autobuild\autobuild.pl rohan_icc9_64.xml
+perl C:\ACE\autobuild\autobuild.pl rohan_icc9_32.xml
