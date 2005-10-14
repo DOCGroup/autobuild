a704b2addb8221e559d750033e5a31e7746e81c1
Johnny Willemsen <jwillemsen@remedy.nl>
2005-10-14 08:02:45 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2894 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/elrond.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# $Id$
+#
+CVSROOT=:ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
+export CVSROOT
+
+CVS_RSH=ssh
+export CVS_RSH
+
+cd $HOME/ACE/autobuild
+
+cvs -z9 up -P -d
+
+$HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_icc64.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_icc32.sh
