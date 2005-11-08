8b503b34049afda709ca0da8c0a3ad6d3325c033
Ossama Othman <othmano@ociweb.com>
2005-11-08 06:15:50 +0000

    ChangeLogTag:Mon Nov  7 22:12:49 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2984 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade12/ace_for_tao.sh
+++ configs/autobuild/isislab/blade12/ace_for_tao.sh
@@ -3,4 +3,4 @@
 # $Id$
 
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
-  $HOME/autobuild/configs/autobuild/pantera_isis/gcc-4.0.xml
+  $HOME/autobuild/configs/autobuild/isislab/blade12/ace_for_tao.xml
