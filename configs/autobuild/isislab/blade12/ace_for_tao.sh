6f0affaec2ae095f024e72ba3b2efc4b07c69cc4
Ossama Othman <othmano@ociweb.com>
2005-11-08 06:21:54 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@2986 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade12/ace_for_tao.sh
+++ configs/autobuild/isislab/blade12/ace_for_tao.sh
@@ -2,5 +2,10 @@
 #
 # $Id$
 
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+cvs up
+
+# Run the build.
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
   $HOME/autobuild/configs/autobuild/isislab/blade12/ace_for_tao.xml
