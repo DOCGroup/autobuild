6eb05a236d50977959bbea46b6af806082fd43ea
Johnny Willemsen <jwillemsen@remedy.nl>
2005-11-15 07:49:49 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3004 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/elrond.sh
+++ configs/autobuild/remedynl/elrond.sh
@@ -14,3 +14,4 @@ cvs -z9 up -P -d
 
 $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_icc64.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_icc32.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_sequence.sh
