b899966550a58f96ba362063ca4bd156a375f05f
isisbuilds <isisbuilds>
2005-11-29 17:12:54 +0000

    ChangeLogTag:Tue Nov 29 12:01:33 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3042 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/isislab/blade12/versioned_namespace.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# $Id$
+
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+cvs up
+
+# Run the build.
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/isislab/blade12/versioned_namespace.xml
+
