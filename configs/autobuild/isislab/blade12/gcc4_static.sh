d88c8f9a25d9dde12301e7207730de8d91143fb3
Ossama Othman <othmano@ociweb.com>
2005-12-02 08:55:54 +0000

    ChangeLogTag:Fri Dec  2 00:53:57 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3050 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/isislab/blade12/gcc4_static.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# $Id$
+
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+cvs up
+
+# Run the build.
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/isislab/blade12/gcc4_static.xml
