de4692cb1779d68d0a2f4a37b75ebc287d4ee9d2
Ossama Othman <othmano@ociweb.com>
2005-12-08 20:09:13 +0000

    ChangeLogTag:Thu Dec  8 07:07:36 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3072 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/isislab/blade11/versioned_namespace.bat
@@ -0,0 +1,9 @@
+REM $Id$
+
+call "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.bat"
+
+cd "C:\Documents and Settings\bczar\autobuild"
+
+cvs -z3 -q up -d -P
+
+perl autobuild.pl configs\autobuild\isislab\blade11\versioned_namespace.xml
