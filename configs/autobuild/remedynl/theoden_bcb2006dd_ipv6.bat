acdef74929334eb9eb45486b64783b0241552158
Martin Corino <mcorino@remedy.nl>
2005-12-15 07:41:30 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3097 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/theoden_bcb2006dd_ipv6.bat
+++ configs/autobuild/remedynl/theoden_bcb2006dd_ipv6.bat
@@ -2,4 +2,4 @@ cd ..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
 c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl theoden_cb6sr.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dd_ipv6.xml
---
+++ configs/autobuild/remedynl/theoden_driver.bat
@@ -0,0 +1,15 @@
+cd ..\..\..\
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\remedynl
+
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dd_sequence.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dr.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006sr.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dd.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006sd.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006ddu.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dd_ipv6.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_cb6sr.xml
+
+rem perl C:\ACE\autobuild\autobuild.pl theoden_vxworks-simnt-gnu.xml
