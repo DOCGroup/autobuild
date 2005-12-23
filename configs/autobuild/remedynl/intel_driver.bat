035c81c9eb81d52856998485f71ce945f136a28c
Johnny Willemsen <jwillemsen@remedy.nl>
2005-12-23 18:57:58 +0000

    ChangeLogTag: Fri Dec 23 14:15:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3148 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/intel_driver.bat
+++ configs/autobuild/remedynl/intel_driver.bat
@@ -1,3 +1,6 @@
 perl C:\ACE\autobuild\autobuild.pl intel_vc8p.xml
 perl C:\ACE\autobuild\autobuild.pl intel_vc8.xml
 perl C:\ACE\autobuild\autobuild.pl intel_icc91w.xml
+perl C:\ACE\autobuild\autobuild.pl intel_vc8p.xml
+perl C:\ACE\autobuild\autobuild.pl intel_vc8.xml
+perl C:\ACE\autobuild\autobuild.pl intel_icc91w.xml
