9f40667e59d3df6ed373943ca239f8e4ad52ca19
Johnny Willemsen <jwillemsen@remedy.nl>
2006-01-09 15:19:20 +0000

    ChangeLogTag: Mon Jan  9 14:38:12 UTC UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3166 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/theoden_cygwinautoconf.bat
+++ configs/autobuild/remedynl/theoden_cygwinautoconf.bat
@@ -1,5 +1,5 @@
 cd ..\..\..\
-set CVS_RSH=ssh
-cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
-perl C:\ACE\autobuild\autobuild.pl configs\autobuild\remedynl\theoden_cygwinautoconf.xml 
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+perl C:\ACE\autobuild\autobuild.pl configs\autobuild\remedynl\theoden_cygwinautoconf.xml
 cd configs\autobuild\remedynl
