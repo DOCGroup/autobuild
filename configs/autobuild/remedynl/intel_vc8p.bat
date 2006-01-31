aa131e14a8f081c0a0ea75cccbbf376f8965b9af
Johnny Willemsen <jwillemsen@remedy.nl>
2006-01-31 11:10:29 +0000

    ChangeLogTag: Tue Jan 31 09:44:10 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3199 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/intel_vc8p.bat
+++ configs/autobuild/remedynl/intel_vc8p.bat
@@ -1,5 +1,3 @@
 cd ..\..\..\
-set CVS_RSH=ssh
-cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl intel_vc8p.xml
