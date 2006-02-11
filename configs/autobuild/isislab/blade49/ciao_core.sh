2bf55a52a1eb894b1b9b21b59f12f119630a8063
isisbuilds <isisbuilds>
2006-02-11 06:17:19 +0000

    ChangeLogTag:Sat Feb 11 06:14:50 UTC 2006  xiong,ming  <ming.xiong@vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3239 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/isislab/blade49/ciao_core.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# $Id$
+
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+CVS_RSH=ssh cvs up
+
+# Run the build.
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/isislab/blade49/ciao_core.xml
