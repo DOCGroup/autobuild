6f9b3f19e7193662f29fa4dfe2495a6438668514
bugzilla <bugzilla>
2006-02-17 22:44:44 +0000

    ChangeLogTag:Fri Feb 17 22:43:12 UTC 2006  xiong,ming  <ming.xiong@vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3299 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade46/ciaocore_vc8.bat
+++ configs/autobuild/isislab/blade46/ciaocore_vc8.bat
@@ -1,9 +1,9 @@
 rem $Id$
-rem perl C:\ACE\autobuild\autobuild.pl merengue_vc71.xml
+rem perl C:\ACE\autobuild\autobuild.pl ciaocore_vc8.xml
 
 cd C:\ACE\autobuild
 set CVS_RSH=plink
-cvs -q -d :ext:mxiong@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 
 
 perl autobuild.pl configs\autobuild\isislab\blade46\ciaocore_vc8.xml
