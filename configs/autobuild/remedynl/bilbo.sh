42a23d4abc78ede7bcc601dae46056adaf80ab09
Johnny Willemsen <jwillemsen@remedy.nl>
2006-02-20 10:32:10 +0000

    ChangelogTag: Mon Feb 20 07:27:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3315 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/bilbo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd $HOME/ACE/autobuild
+PATH=/usr/local/bin:/etc:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:$PATH
+CVSROOT=:ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
+CVS_RSH=ssh
+export CVSROOT
+export CVS_RSH
+export PATH
+
+cvs -z9 up -P -d
+
+exec /usr/local/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/bilbo_suncc.xml
