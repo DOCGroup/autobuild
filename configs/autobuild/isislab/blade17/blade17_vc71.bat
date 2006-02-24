1126158317df3e582c1522cabb0f2fb3d94b61d1
isisbuilds <isisbuilds>
2006-02-24 23:08:28 +0000

    ChangeLogTag:Fri Feb 24 23:07:02 UTC 2006  xiong,ming  <ming.xiong@vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3354 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade17/blade17_vc71.bat
+++ configs/autobuild/isislab/blade17/blade17_vc71.bat
@@ -3,6 +3,6 @@ rem perl C:\ACE\autobuild\autobuild.pl blade17_vc71.xml
 
 cd C:\ACE\autobuild
 set CVS_RSH=plink
-cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 
 perl autobuild.pl  configs\autobuild\isislab\blade17\blade17_vc71.xml
