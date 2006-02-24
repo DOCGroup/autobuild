5317fc4f65093595b71b2f9a3348ab56dfa668ce
isisbuilds <isisbuilds>
2006-02-24 23:38:58 +0000

    ChangeLogTag:Fri Feb 24 23:37:51 UTC 2006  xiong,ming  <ming.xiong@vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3355 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade46/ciaocore_vc8.bat
+++ configs/autobuild/isislab/blade46/ciaocore_vc8.bat
@@ -3,7 +3,7 @@ rem perl C:\ACE\autobuild\autobuild.pl ciaocore_vc8.xml
 
 cd C:\ACE\autobuild
 set CVS_RSH=plink
-cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 
 
 perl autobuild.pl configs\autobuild\isislab\blade46\ciaocore_vc8.xml
