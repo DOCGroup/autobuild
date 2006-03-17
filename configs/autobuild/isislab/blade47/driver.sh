51749de27d46537454932e8ce037d491796ce7f0
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-17 10:07:06 +0000

    ChangeLogTag: Fri Mar 17 09:49:13 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3440 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade47/driver.sh
+++ configs/autobuild/isislab/blade47/driver.sh
@@ -4,7 +4,8 @@
 
 # Make sure autobuild directory is up to date.
 cd $HOME/autobuild
-CVS_RSH=ssh cvs up
+CVS_RSH=ssh
+cvs up
 
 # Run the build.
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
--- configs/autobuild/isislab/blade56/ACE_TRACE.sh
+++ configs/autobuild/isislab/blade56/ACE_TRACE.sh
@@ -2,6 +2,11 @@
 #
 # $Id$
 
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+CVS_RSH=ssh
+cvs up
+
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
   $HOME/autobuild/configs/autobuild/isislab/blade56/ACE_TRACE.xml
 
