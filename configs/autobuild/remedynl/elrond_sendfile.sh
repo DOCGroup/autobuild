60211bde8dbe2f4ca78d11a7aa773b3945a67732
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-21 10:24:01 +0000

    ChangeLogTag: Tue Mar 21 06:49:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3447 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/elrond_sendfile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd $HOME/ACE/autobuild
+
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_sendfile.xml
+
