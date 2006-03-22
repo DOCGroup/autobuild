60c5dd61ce531c557d306e1551b0eb0472aafa49
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-22 15:49:11 +0000

    ChangeLogTag: Wed Mar 22 15:32:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3473 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/dresystems/driver_local_scoreboard.sh
+++
@@ -1,4 +0,0 @@
-
-cd /home/dhinton/development/ace/autobuild/
-
-/home/dhinton/development/ace/autobuild/scoreboard.pl -c /home/dhinton/development/ace/autobuild/configs/autobuild/dresystems/local_scoreboard.xml -o /home/svn/www/dresystems.com 2>&1
