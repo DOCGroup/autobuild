33f3bf0b324e17e2c477ad71fcfb76a6e595f50e
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-22 08:26:48 +0000

    ChangeLogTag: Tue Mar 21 13:00:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>Tue Mar 21 13:01:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3451 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade27/blade27_vc71_minimum_corba.bat
+++ configs/autobuild/isislab/blade27/blade27_vc71_minimum_corba.bat
@@ -3,6 +3,6 @@ rem perl C:\ACE\autobuild\autobuild.pl blade27_vc71_minimum_corba.xml
 
 cd C:\ACE\autobuild
 set CVS_RSH=c:\cygwin\bin\ssh
-c:\cygwin\bin\cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 
 perl autobuild.pl  configs\autobuild\isislab\blade27\blade27_vc71_minimum_corba.xml
