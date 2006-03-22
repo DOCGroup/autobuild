0f9c1c02c4be57f043769d3446b3f3b096dbb111
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-22 11:48:00 +0000

    ChangeLogTag: Wed Mar 22 11:22:13 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3461 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/simbagcc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-
-cd /home/build/ACE/autobuild
-
-exec /opt/perl/bin/perl /home/build/ACE/autobuild/autobuild.pl \
-                             /home/build/ACE/autobuild/configs/autobuild/remedynl/simbagcc.xml
