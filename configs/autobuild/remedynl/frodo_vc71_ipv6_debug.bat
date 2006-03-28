c9208f63b4db19a0e015158d7dfb23caa353a8ac
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-28 12:58:34 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3495 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/frodo_vc71_ipv6_debug.bat
@@ -0,0 +1,5 @@
+cd ..\..\..\
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cd configs\autobuild\remedynl
+perl C:\ACE\autobuild\autobuild.pl frodo_vc71_ipv6_debug.xml
