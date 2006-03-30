fbc7b29b7d08e5124065aaec136df1c942e181e0
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-30 08:24:02 +0000

    ChangeLogTag: Thu Mar 30 08:02:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3514 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade50/blade50_driver.bat
+++ configs/autobuild/isislab/blade50/blade50_driver.bat
@@ -1,7 +1,7 @@
 
 cd ..\..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
-cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\isislab\blade50
 
 call blade50_vc8.bat
--- configs/autobuild/tao_win2003/tao_driver.bat
+++ configs/autobuild/tao_win2003/tao_driver.bat
@@ -2,7 +2,7 @@ rem $Id$
 
 cd ..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
-cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\tao_win2003
 
 call tao_vc8.bat
