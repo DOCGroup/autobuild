28c17006e55238ef8d1b57ca73249c9d4467bae1
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-31 09:21:58 +0000

    ChangeLogTag: Thu Mar 30 13:14:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3537 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade49/blade49.bat
+++ configs/autobuild/isislab/blade49/blade49.bat
@@ -1,4 +1,3 @@
 cd C:\bin
-start pageant keys\bczar-private.ppk
 cd C:\ACE\autobuild\configs\autobuild\isislab\blade49
 call blade49_driver.bat
--- configs/autobuild/isislab/blade49/blade49_driver.bat
+++ configs/autobuild/isislab/blade49/blade49_driver.bat
@@ -2,7 +2,7 @@
 rem perl C:\ACE\autobuild\autobuild.pl blade49_vc71.xml
 
 cd ..\..\..\..\
-set CVS_RSH=plink
+set CVS_RSH=c:\cygwin\bin\ssh
 cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\isislab\blade49
 
