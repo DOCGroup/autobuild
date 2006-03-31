72d8c747984f47aa2dd80e60137bbaf717da5cc5
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-31 09:39:14 +0000

    ChangeLogTag: Thu Mar 30 13:14:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3539 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade49/blade49_driver.bat
+++ configs/autobuild/isislab/blade49/blade49_driver.bat
@@ -1,6 +1,4 @@
 
-rem perl C:\ACE\autobuild\autobuild.pl blade49_vc71.xml
-
 cd ..\..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
 cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
