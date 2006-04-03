4bc262de982a86947471fed7eef229d62f8a6d9b
Johnny Willemsen <jwillemsen@remedy.nl>
2006-04-03 10:15:20 +0000

    Sun Apr  2 18:56:25 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3543 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade11/versioned_namespace.bat
+++ configs/autobuild/isislab/blade11/versioned_namespace.bat
@@ -4,6 +4,7 @@ call "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.
 
 cd "C:\Documents and Settings\bczar\autobuild"
 
-cvs -z3 -q up -d -P
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 
 perl autobuild.pl configs\autobuild\isislab\blade11\versioned_namespace.xml
