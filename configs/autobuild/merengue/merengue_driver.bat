2392dcbd46016aaa7b2ef0fbe5ba9154479a4749
Johnny Willemsen <jwillemsen@remedy.nl>
2006-04-04 11:37:52 +0000

    ChangeLogTag: Tue Apr  4 08:02:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>Tue Apr  4 09:22:12 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3557 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/merengue/merengue_driver.bat
+++ configs/autobuild/merengue/merengue_driver.bat
@@ -2,8 +2,8 @@ rem $Id$
 rem perl C:\ACE\autobuild\autobuild.pl merengue_vc71.xml
 
 cd ..\..\..\
-set CVS_RSH=plink
-cvs -q -d :ext:bugzilla@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+set CVS_RSH=c:\cygwin\bin\ssh
+cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 cd configs\autobuild\merengue
 
 call cidlc.bat
