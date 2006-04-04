7c7e9c466e9501d9feb6533b9e8cfc5f5da7ae30
Johnny Willemsen <jwillemsen@remedy.nl>
2006-04-04 08:27:14 +0000

    ChangeLogTag: Tue Apr  4 08:02:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3554 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade27/blade27_driver.bat
+++ configs/autobuild/isislab/blade27/blade27_driver.bat
@@ -2,6 +2,6 @@
 cd ..\..\..\..\
 set CVS_RSH=c:\cygwin\bin\ssh
 cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
-cd configs\autobuild\isislab\blade11
+cd configs\autobuild\isislab\blade27
 
 call blade27_vc71_minimum_corba.bat
