33b6459ac172fc282214ed86f42863f19ddf13c7
Johnny Willemsen <jwillemsen@remedy.nl>
2006-04-26 10:11:13 +0000

    ChangeLogTag: Wed Apr 26 10:05:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3623 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade17/blade17_vc71_w4.bat
+++ configs/autobuild/isislab/blade17/blade17_vc71_w4.bat
@@ -2,7 +2,7 @@ rem $Id$
 rem perl C:\ACE\autobuild\autobuild.pl blade17_vc71.xml
 
 cd C:\ACE\autobuild
-set CVS_RSH=plink
-cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+set CVS_RSH=c:\cygwin\bin\ssh
+c:\cygwin\bin\cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
 
 perl autobuild.pl  configs\autobuild\isislab\blade17\blade17_vc71_w4.xml
