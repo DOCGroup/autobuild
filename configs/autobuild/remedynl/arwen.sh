e55567fbe36c7a5cbfab4148c7c13daffcdaeb55
Johnny Willemsen <jwillemsen@remedy.nl>
2006-05-12 09:05:27 +0000

    ChangeLogTag: Fri May 12 09:00:12 2006 UTC  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3680 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/arwen.sh
+++ configs/autobuild/remedynl/arwen.sh
@@ -12,5 +12,6 @@ cd $HOME/ACE/autobuild
 
 cvs -z9 up -P -d
 
+$HOME/ACE/autobuild/configs/autobuild/remedynl/arwen_icc91.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/arwen_icc.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/arwen_gcc.sh
