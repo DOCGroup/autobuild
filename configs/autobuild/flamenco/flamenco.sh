9573f523cfad8d63da5eab07a548727d3acc8265
Johnny Willemsen <jwillemsen@remedy.nl>
2006-05-18 14:11:03 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3699 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/flamenco/flamenco.sh
+++ configs/autobuild/flamenco/flamenco.sh
@@ -4,4 +4,14 @@
 #
 
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
-                        $HOME/autobuild/configs/autobuild/flamenco/Static.xml
+  $HOME/autobuild/configs/autobuild/flamenco/Static.xml
+
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/flamenco/compiletime_metrics.xml
+
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/flamenco/footprint_metrics.xml
+
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/flamenco/Static.xml
+
