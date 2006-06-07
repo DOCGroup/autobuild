bea3b775373428d1d6130cd7a66a88c71662f924
William R. Otte <wotte@dre.vanderbilt.edu>
2006-06-07 07:07:32 +0000

    Wed Jun  7 07:04:45 UTC 2006  William R. Otte  <wotte@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3733 59e07337-6b18-0410-a141-c5efe34a0da7

--- common/parse_compiler_output.pm
+++ common/parse_compiler_output.pm
@@ -112,7 +112,7 @@ sub handle_compiler_output_line($) {
     return;
   }
   
-  if ($s =~ m/This project will not be built due to the following missing/) {
+  if ($s =~ m/not be built due to the following missing/) {
     # Indicates that something is not properly configured - ie,
     # incorrect depenancies in the mpc files, default.featres setting,
     # etc.
