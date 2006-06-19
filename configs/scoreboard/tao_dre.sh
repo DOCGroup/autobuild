76152ca72c079f7e7e36b7fd89a2c82777a5d569
Johnny Willemsen <jwillemsen@remedy.nl>
2006-06-19 10:32:36 +0000

    ChangeLogTag: Mon Jun 19 10:08:17 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3751 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/tao_dre.sh
+++ configs/scoreboard/tao_dre.sh
@@ -21,6 +21,7 @@ cvs up
 /usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/tao.xml -o tao.html -r tao.rss 2>&1  &
 /usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/tao_future.xml -o tao_future.html 2>&1 &
 /usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ciao.xml -o ciao.html -r ciao.rss 2>&1  &
+/usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/dds.xml -o dds.html -r dds.rss 2>&1  &
 /usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/misc.xml -o misc.html 2>&1 &
 /usr/bin/perl ./scoreboard.pl -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/cosmic.xml -o cosmic.html 2>&1 &
 
