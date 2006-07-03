ed58aecd8e963875a8e457b1a868f26190def8ee
isisbuilds <isisbuilds>
2006-07-03 12:32:42 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3788 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/isislab/blade47/NoIIOP.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# $Id$
+
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+CVS_RSH=ssh
+export CVS_RSH
+cvs up -P -d
+
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+                             $HOME/autobuild/configs/autobuild/isislab/blade47/NoInt_NoEx.xml
+
