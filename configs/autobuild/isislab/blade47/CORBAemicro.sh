55023e667539b0aaec14019bd514a0f2212104c8
isisbuilds <isisbuilds>
2006-07-03 12:27:27 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3785 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/isislab/blade47/CORBAemicro.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# $Id$
+
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+CVS_RSH=ssh
+export CVS_RSH
+cvs up -P -d
+
+# Run the build.
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/isislab/blade47/NoInt_NoEx.xml
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/isislab/blade47/CORBAemicro.xml
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+  $HOME/autobuild/configs/autobuild/isislab/blade47/CORBAecompact.xml
