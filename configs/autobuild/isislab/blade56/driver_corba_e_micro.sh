adc6aa6e8e3762a3fe02eb58f167a6e97989e5ab
Johnny Willemsen <jwillemsen@remedy.nl>
2006-07-07 12:43:31 +0000

    no message
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3799 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade56/driver_corba_e_micro.sh
+++ configs/autobuild/isislab/blade56/driver_corba_e_micro.sh
@@ -3,5 +3,11 @@
 # $Id$
 #
 
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+CVS_RSH=ssh
+export CVS_RSH
+cvs up -P -d
+
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
   $HOME/autobuild/configs/autobuild/isislab/blade56/footprint_corba_e_micro.xml
