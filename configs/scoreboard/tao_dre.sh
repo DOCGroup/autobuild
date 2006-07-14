83efa00096339464419129eedd70fc95ea3c277b
William R. Otte <wotte@dre.vanderbilt.edu>
2006-07-14 01:38:14 +0000

    Fri Jul 14 01:37:51 UTC 2006  William R. Otte  <wotte@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3812 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/tao_dre.sh
+++ configs/scoreboard/tao_dre.sh
@@ -5,11 +5,8 @@
 PERLLIB=autobuild
 export PERLLIB
 
-CVS_RSH=ssh
-export CVS_RSH
-
 cd $HOME/autobuild
-cvs up
+svn up
 
 # /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c $HOME/autobuild/configs/scoreboard/acetao.xml 2>&1 &
 
