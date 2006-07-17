59e15d71b6ab1627f4db4a702995ec4d8b459dd1
Johnny Willemsen <jwillemsen@remedy.nl>
2006-07-17 08:48:34 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3826 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/arwen.sh
+++ configs/autobuild/remedynl/arwen.sh
@@ -2,15 +2,9 @@
 #
 # $Id$
 #
-CVSROOT=:ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository
-export CVSROOT
-
-CVS_RSH=ssh
-export CVS_RSH
-
 cd $HOME/ACE/autobuild
 
-cvs -z9 up -P -d
+svn up
 
 $HOME/ACE/autobuild/configs/autobuild/remedynl/arwen_icc91.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/arwen_icc.sh
