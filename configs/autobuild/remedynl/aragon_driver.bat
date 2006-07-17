bfa248fbfe145d569d301754a836a75e3032fcfc
Johnny Willemsen <jwillemsen@remedy.nl>
2006-07-17 08:15:47 +0000

    updated to svn
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3821 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/aragon_driver.bat
+++ configs/autobuild/remedynl/aragon_driver.bat
@@ -1,6 +1,5 @@
 cd \ace\autobuild
-set CVS_RSH=c:\cygwin\bin\ssh
-c:\cygwin\bin\cvs -d :ext:jwillemsen@cvs.doc.wustl.edu:/project/cvs-repository -z9 up -P -d
+svn up
 cd configs\autobuild\remedynl
 perl c:\ACE\autobuild\autobuild.pl aragon_cb6sr.xml
 call aragon_vxworks-pcPentiumXSharedLib-gnu-run.bat
