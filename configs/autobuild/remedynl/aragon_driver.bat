3764bbde729121393aa11ea03c1a53a6aee97031
Johnny Willemsen <jwillemsen@remedy.nl>
2006-07-17 08:17:38 +0000

    updated to svn
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3822 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/aragon_driver.bat
+++ configs/autobuild/remedynl/aragon_driver.bat
@@ -1,5 +1,5 @@
 cd \ace\autobuild
-svn up
+c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
 perl c:\ACE\autobuild\autobuild.pl aragon_cb6sr.xml
 call aragon_vxworks-pcPentiumXSharedLib-gnu-run.bat
