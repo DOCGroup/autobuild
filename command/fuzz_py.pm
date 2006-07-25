a10ad45229590e068e2ec96398ed92731128f197
William R. Otte <wotte@dre.vanderbilt.edu>
2006-07-25 20:38:06 +0000

    Tue Jul 25 20:37:09 UTC 2006  William R. Otte  <wotte@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3901 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ command/fuzz_py.pm
@@ -0,0 +1,96 @@
+#
+# $Id: fuzz.pm 991 2003-08-07 18:57:37Z oci $
+#
+
+package Fuzz_py;
+
+use strict;
+use warnings;
+
+use Cwd;
+use File::Path;
+
+###############################################################################
+# Constructor
+
+sub new
+{
+    my $proto = shift;
+    my $class = ref ($proto) || $proto;
+    my $self = {};
+
+    bless ($self, $class);
+    return $self;
+}
+
+##############################################################################
+
+sub CheckRequirements ()
+{
+    my $self = shift;
+    my $root = main::GetVariable ('root');
+
+    if (!defined $root) {
+        print STDERR __FILE__, ": Requires \"root\" variable\n";
+        return 0;
+    }
+    
+    return 1;
+}
+
+##############################################################################
+
+sub Run ($)
+{
+    my $self = shift;
+    my $options = shift;
+    my $root = main::GetVariable ('root');
+    my $project_root = main::GetVariable ('project_root');
+
+    # replace all '\x22' with '"'
+    $options =~ s/\\x22/"/g;
+
+    if (!-r $root || !-d $root) {
+        mkpath($root);
+    }
+
+    # chop off trailing slash
+    if ($root =~ m/^(.*)\/$/) {
+        $root = $1;
+    }
+
+    main::PrintStatus ('Compile', 'fuzz');
+
+    my $current_dir = getcwd ();
+
+    if (!chdir $root) {
+        print STDERR __FILE__, ": Cannot change to $root\n";
+        return 0;
+    }
+
+    if (!defined $project_root) {
+        $project_root = 'ACE_wrappers';
+    }
+    
+    if (!-r $project_root || !-d $project_root) {
+        mkpath($project_root);
+    }
+
+    if (!chdir $project_root) {
+        print STDERR __FILE__, ": Cannot change to $project_root\n";
+        return 0;
+    }
+
+    my $command = "python bin/fuzz.py $options";
+
+    print "Running: $command\n";
+    system ($command);
+
+    chdir $current_dir;
+    
+    return 1;
+}
+
+##############################################################################
+
+main::RegisterCommand ("fuzz_py", new Fuzz_py ());
---
+++ configs/autobuild/tao_isis/driver_fuzz_py.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/bin/perl $HOME/autobuild/autobuild.pl $HOME/autobuild/configs/autobuild/tao_isis/Fuzz_py.xml 
+
