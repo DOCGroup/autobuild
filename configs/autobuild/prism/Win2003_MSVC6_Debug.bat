a069b66b2c83bc37ca6b24db72f56862765f17af
Simon Massey <simon.massey@prismtech.com>
2006-07-27 12:02:37 +0000

    Thu Jul 27 12:05:00 UTC 2006  Simon Massey  <sma@prismtech.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3921 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/prism/Win2003_MSVC6_Debug.bat
+++ configs/autobuild/prism/Win2003_MSVC6_Debug.bat
@@ -1,7 +1,9 @@
+rem $Id$
 call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
 f:
 cd \Overnight_Builds\DOC_Group\autobuild
 svn up .
+svn status .
 cd configs\autobuild\prism
 perl f:\Overnight_Builds\DOC_Group\autobuild\autobuild.pl Win2003_MSVC6_Debug.xml
 c:\cygwin\bin\chmod o+r /cygdrive/T/doc-scoreboard/windows/MSVC6_Debug/*
