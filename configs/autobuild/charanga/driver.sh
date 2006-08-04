85a4b0db71d68c9383603668057f0856169be2d2
Johnny Willemsen <jwillemsen@remedy.nl>
2006-08-04 09:11:14 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3952 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/charanga/driver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-
-exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
-                             $HOME/autobuild/configs/autobuild/charanga/Debian_Minimum_Static.xml 2>&1
-
