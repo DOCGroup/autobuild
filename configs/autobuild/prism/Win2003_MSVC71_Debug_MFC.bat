f84fc377939147a4515d57ea5dd3abfa4a0256cf
Simon Massey <simon.massey@prismtech.com>
2006-08-11 11:54:26 +0000

    Fri Aug 11 12:55:00 UTC 2006  Simon Massey  <sma@prismtech.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@3965 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/prism/Win2003_MSVC71_Debug_MFC.bat
+++ configs/autobuild/prism/Win2003_MSVC71_Debug_MFC.bat
@@ -1,8 +1,17 @@
 @echo off
 rem $Id: Win2003_MSVC6_Debug.bat 3921 2006-07-27 12:02:37Z sma $
+set THIS_BUILD=C:\Overnight_Builds\DOC_Group\vc71\debugMFC\
 echo ==============================================================================
 echo                          DOC_Group MSVC71 Debug MFC
 echo ==============================================================================
+if not exist %THIS_BUILD%.disable goto doBuild
+  echo.
+  echo A previous build is either still in progress or failed abnormally as the
+  echo "%THIS_BUILD%.disable" file exists.
+  echo.
+  pause
+  if exist %THIS_BUILD%.disable goto :eof
+:doBuild
 call "C:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\bin\VCVARS32.BAT"
 mkdir C:\temp > nul: 2>&1
 mkdir C:\temp\DOC_Group > nul: 2>&1
@@ -18,8 +27,8 @@ echo.
 echo status autobuild
 svn status autobuild
 echo.
-echo For status, notepad C:\Overnight_Builds\DOC_Group\vc71\debugMFC\log.txt
-cd \Overnight_Builds\DOC_Group\vc71\debugMFC
+echo For status, notepad %THIS_BUILD%build.txt
+cd %THIS_BUILD%
 perl C:\Overnight_Builds\DOC_Group\autobuild\autobuild.pl C:\Overnight_Builds\DOC_Group\autobuild\configs\autobuild\prism\Win2003_MSVC71_Debug_MFC.xml > nul: 2>&1
 cd \Overnight_Builds\DOC_Group\autobuild\configs\autobuild\prism
 echo Finished !!
