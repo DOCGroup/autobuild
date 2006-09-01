6cd9d7b9bb7e71f4a743af7978f4762619f731c9
Johnny Willemsen <jwillemsen@remedy.nl>
2006-09-01 08:01:45 +0000

    just one script that executes all builds
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4018 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/balar_isis/driver.sh
+++ configs/autobuild/balar_isis/driver.sh
@@ -6,3 +6,15 @@
 exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
                             $HOME/autobuild/configs/autobuild/balar_isis/ImplicitTemplates.xml
 
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+                             $HOME/autobuild/configs/autobuild/balar_isis/DynamicHash.xml
+
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+                        $HOME/autobuild/configs/autobuild/balar_isis/IPV6.xml
+
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+                        $HOME/autobuild/configs/autobuild/balar_isis/Static.xml
+
+exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
+                            $HOME/autobuild/configs/autobuild/balar_isis/wchar.xml
+
