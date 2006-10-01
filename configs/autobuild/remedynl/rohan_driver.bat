3d6f7c55a2e27fd34811dcd0f6414277a58f4db3
Johnny Willemsen <jwillemsen@remedy.nl>
2006-10-01 11:37:10 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4063 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/rohan_driver.bat
+++ configs/autobuild/remedynl/rohan_driver.bat
@@ -2,5 +2,6 @@ cd ..\..\..\
 svn up
 cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl rohan_icc9_64.xml
+perl C:\ACE\autobuild\autobuild.pl rohan_icc91_64.xml
 perl C:\ACE\autobuild\autobuild.pl rohan_icc9_32.xml
 perl C:\ACE\autobuild\autobuild.pl rohan_icc9_32_r.xml
