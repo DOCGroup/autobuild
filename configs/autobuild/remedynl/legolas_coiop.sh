44aaa0ad0c3066497bad70654c121e97f7a7e5e3
Johnny Willemsen <jwillemsen@remedy.nl>
2006-10-12 14:28:01 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4084 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/legolas_coiop.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# $Id$
+
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+svn up
+
+# Run the build.
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_coiop.xml
