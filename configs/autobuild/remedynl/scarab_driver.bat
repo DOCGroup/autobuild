bbb0899592dc2ae191fdd0eda44358f56b4c6181
Johnny Willemsen <jwillemsen@remedy.nl>
2006-10-17 14:54:53 +0000

    removed mingw build
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4114 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/scarab_driver.bat
+++ configs/autobuild/remedynl/scarab_driver.bat
@@ -3,5 +3,4 @@ perl C:\ACE\autobuild\autobuild.pl scarab_vc8.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_bcb6ddu.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_bcb6dd.xml
 perl C:\ACE\autobuild\autobuild.pl scarab_bcb6dr.xml
-perl C:\ACE\autobuild\autobuild.pl scarab_MingWs.xml
 call scarab.bat
