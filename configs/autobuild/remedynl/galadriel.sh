d8c480d95dc3d2c3fc7000b1eebf9093460d962d
Johnny Willemsen <jwillemsen@remedy.nl>
2006-11-04 12:17:17 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4151 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/galadriel.sh
+++ configs/autobuild/remedynl/galadriel.sh
@@ -13,3 +13,4 @@ $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_galadriel.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_footprint.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_autoconf.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63r.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63ripv6.sh
