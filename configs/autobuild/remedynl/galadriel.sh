7ad5f5f29befa2038b6c9dee6eed2b5242d980f3
Martin Corino <mcorino@remedy.nl>
2006-11-08 10:21:04 +0000

    added check to prevent nested runs
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4163 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/galadriel.sh
+++ configs/autobuild/remedynl/galadriel.sh
@@ -2,15 +2,24 @@
 #
 # $Id$
 #
-cd $HOME/ACE/autobuild
 
-svn up
+RUNCHECK=/home/build/.galadriel_autobuild_running
 
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_host.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61_ppc603.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_galadriel.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_footprint.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_autoconf.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63r.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63ripv6.sh
+if !(test -e ${RUNCHECK}); then
+    echo RUNNING>${RUNCHECK}
+
+    cd $HOME/ACE/autobuild
+
+    svn up
+
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_host.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61_ppc603.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_galadriel.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_footprint.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_autoconf.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63r.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63ripv6.sh
+
+    rm -f ${RUNCHECK}
+fi
