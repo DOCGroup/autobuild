57e34f451825715b6dec57c39c3fce4b1b97bf8e
Martin Corino <mcorino@remedy.nl>
2006-11-11 15:58:10 +0000

    split galadriels builds in 2 parallel batches
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4177 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/galadriel.sh
+++ configs/autobuild/remedynl/galadriel.sh
@@ -15,11 +15,10 @@ if !(test -e ${RUNCHECK}); then
     $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_host.sh
     $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61.sh
     $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61_ppc603.sh
-    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_galadriel.sh
-    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_footprint.sh
-    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_autoconf.sh
-    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63r.sh
-    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63ripv6.sh
+#    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_footprint.sh
+#    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_autoconf.sh
+#    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63r.sh
+#    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63ripv6.sh
 
     rm -f ${RUNCHECK}
 fi
---
+++ configs/autobuild/remedynl/galadriel_2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+RUNCHECK=/home/build/.galadriel_2_autobuild_running
+
+if !(test -e ${RUNCHECK}); then
+    echo RUNNING>${RUNCHECK}
+
+    cd $HOME/ACE/autobuild
+
+    svn up
+
+#    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_host.sh
+#    $$HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61.sh
+#    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks61_ppc603.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_footprint.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_autoconf.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63r.sh
+    $HOME/ACE/autobuild/configs/autobuild/remedynl/galadriel_vxworks63ripv6.sh
+
+    rm -f ${RUNCHECK}
+fi
