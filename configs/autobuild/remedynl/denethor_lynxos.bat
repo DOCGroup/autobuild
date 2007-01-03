2135b7b92142fa1559387b0a5480b67118ebe7ca
Johnny Willemsen <jwillemsen@remedy.nl>
2007-01-03 09:00:30 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4289 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/denethor_lynxos.bat
+++ configs/autobuild/remedynl/denethor_lynxos.bat
@@ -4,5 +4,5 @@ cd ..\..\..\
 "c:\program files\subversion\bin\svn" up
 cd configs\autobuild\remedynl
 set PATH=%PATH%;C:\LynuxWorks\4p2p0\bin;
-c:\cygwin\bin\perl c:\ACE\autobuild\autobuild.pl denethor_lynxos.xml
+c:\perl\bin\perl c:\ACE\autobuild\autobuild.pl denethor_lynxos.xml
 @endlocal
