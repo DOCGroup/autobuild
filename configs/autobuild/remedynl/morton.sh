d9abae7ff5be4ce87d4ec7facd44d37d1fd14a67
Johnny Willemsen <jwillemsen@remedy.nl>
2007-01-05 08:33:59 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4310 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/morton.sh
+++ configs/autobuild/remedynl/morton.sh
@@ -6,5 +6,7 @@ cd $HOME/ACE/autobuild
 
 svn up
 
+$HOME/ACE/autobuild/configs/autobuild/remedynl/morton_autoconfgcc.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/morton_acc.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/morton_gcc.sh
+
---
+++ configs/autobuild/remedynl/morton_autoconfgcc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd $HOME/ACE/autobuild
+
+exec /opt/perl/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/morton_autoconfgcc.xml
+
