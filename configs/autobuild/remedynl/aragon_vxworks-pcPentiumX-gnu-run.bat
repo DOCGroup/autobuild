543e8db1ddd38eb41362458df69f40679abfdded
Johnny Willemsen <jwillemsen@remedy.nl>
2007-01-22 07:25:43 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4405 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/aragon_vxworks-pcPentiumX-gnu-run.bat
+++ configs/autobuild/remedynl/aragon_vxworks-pcPentiumX-gnu-run.bat
@@ -1,9 +1,9 @@
 @setlocal
 
 set PATH=%PATH%;c:\tools\pstools;c:\tornado2.2\host\x86-win32\bin
-pskill -t tgtsvr.exe
-start tgtsvr.exe 10.2.128.65 -n P4 -V -m 61685760 -B wdbrpc -R C:\ -RW -redirectIO -C
+rem pskill -t tgtsvr.exe
+rem start tgtsvr.exe 10.2.128.65 -n P4 -V -m 61685760 -B wdbrpc -R C:\ -RW -redirectIO -C
 perl c:\ACE\autobuild\autobuild.pl aragon_vxworks-pcPentiumX-gnu.xml
-pskill -t tgtsvr.exe
+rem pskill -t tgtsvr.exe
 
 @endlocal
