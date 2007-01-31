f895899ffe07589fdd3b0844a6a16ad08c29c5ab
Johnny Willemsen <jwillemsen@remedy.nl>
2007-01-31 19:56:28 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4438 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/legolas.sh
+++ configs/autobuild/remedynl/legolas.sh
@@ -13,3 +13,4 @@ $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems_nonet.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems_nonets.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_acefortao.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_qnx.sh
