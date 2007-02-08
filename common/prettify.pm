552c1eabd58f1be60f5cdbb0e6972dd8594d1df9
Johnny Willemsen <jwillemsen@remedy.nl>
2007-02-08 18:05:23 +0000

    Added Checksum mismatch for as error
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4450 59e07337-6b18-0410-a141-c5efe34a0da7

--- common/prettify.pm
+++ common/prettify.pm
@@ -807,6 +807,7 @@ sub Setup_Handler ($)
         $s =~ m/is not a working copy/ ||
         $s =~ m/svn help cleanup/ ||
         $s =~ m/t connect to host/ ||
+        $s =~ m/Checksum mismatch for/ ||
         $s =~ m/Failed to add file/ ||
         $s =~ m/Failed to add directory/ ||
         $s =~ m/^ERROR/ ||
