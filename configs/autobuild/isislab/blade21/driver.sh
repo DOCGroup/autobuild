7646df1bd421eb9f4d6bbfe3a015f3cbe650d177
William R. Otte <wotte@dre.vanderbilt.edu>
2007-03-01 03:27:33 +0000

    ChangeLogTag:Thu Mar  1 03:25:04 UTC 2007  William R. Otte  <wotte@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4497 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/isislab/blade21/driver.sh
@@ -0,0 +1,5 @@
+$HOME/autobuild/configs/autobuild/isislab/blade21/driver_DynamicHash.sh
+$HOME/autobuild/configs/autobuild/isislab/blade21/driver_ipv6.sh
+$HOME/autobuild/configs/autobuild/isislab/blade21/driver_static.sh
+$HOME/autobuild/configs/autobuild/isislab/blade21/driver_wchar.sh
+$HOME/autobuild/configs/autobuild/isislab/blade21/driver_implicittemplates.sh
