2b0f24d74328022742ac91f412b8cebc4badb49e
Johnny Willemsen <jwillemsen@remedy.nl>
2007-03-07 10:12:13 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4521 59e07337-6b18-0410-a141-c5efe34a0da7

--- command/check_compiler.pm
+++ command/check_compiler.pm
@@ -42,7 +42,7 @@ sub Run ($)
 
     print "================ Compiler version ================\n";
 
-    if($compiler =~ m/^(gcc|g\+\+|g\+\+-?[0-9]|ccsimpc|ccpentium|c\+\+ppc)/){
+    if($compiler =~ m/^(gcc|g\+\+|g\+\+-?[0-9]|ccsimpc|ccpentium|c\+\+ppc|c\+\+pentium)/){
         system($compiler." -v 2>&1");
       if($compiler =~ m/^(gcc|g\+\+|g\+\+-?[0-9])/){
           my $linker = `$compiler -print-prog-name=ld`;
