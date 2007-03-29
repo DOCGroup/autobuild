bae8c431f9a701a6375e6c26b4e4a48ba15909bb
Krishnakumar Balasubramanian <kitty@dre.vanderbilt.edu>
2007-03-29 23:03:11 +0000

    ChangeLogTag: Thu Mar 29 23:01:28 UTC 2007  Krishnakumar B  <kitty@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4614 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/equus/equus_driver.bat
+++ configs/autobuild/equus/equus_driver.bat
@@ -1,8 +1,6 @@
 rem $Id$
 
 cd C:\bczar\autobuild
-set CVS_RSH=plink.exe
-cvs -q -d :ext:isisbuilds@cvs.doc.wustl.edu:/project/cvs-repository -z3 up -P -d
+svn up
 cd configs\autobuild\equus
-
 call equus_vc71.bat
