ddd307e2747f36fa811dd6aa886417041d0acd6b
Johnny Willemsen <jwillemsen@remedy.nl>
2007-06-20 09:59:32 +0000

    disabled rtems builds
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4722 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/legolas.sh
+++ configs/autobuild/remedynl/legolas.sh
@@ -8,9 +8,9 @@ svn up
 
 $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_coiop.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_linux_nonet.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_host.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems_nonet.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems_nonets.sh
+#$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_host.sh
+#$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems.sh
+#$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems_nonet.sh
+#$HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_rtems_nonets.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_acefortao.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/legolas_qnx.sh
