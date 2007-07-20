c18ccef2c6714c03334295464cd0b080814eb05a
Johnny Willemsen <jwillemsen@remedy.nl>
2007-07-20 13:36:44 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4777 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/aragon_icc10_32.bat
+++ configs/autobuild/remedynl/aragon_icc10_32.bat
@@ -1,5 +1,5 @@
 cd ..\..\..\
-svn up
+c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
 call "C:\Program Files\Intel\Compiler\C++\10.0.026\IA32\Bin\ICLVars.bat"
 perl C:\ACE\autobuild\autobuild.pl aragon_icc10_32.xml
