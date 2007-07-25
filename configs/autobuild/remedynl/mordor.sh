676953ae0dc4b46fbad4c0b4c8c5a51f34227986
Johnny Willemsen <jwillemsen@remedy.nl>
2007-07-25 14:55:08 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4803 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/mordor.sh
+++ configs/autobuild/remedynl/mordor.sh
@@ -6,7 +6,6 @@ cd $HOME/ACE/autobuild
 
 /usr/local/bin/svn up
 
-$HOME/ACE/autobuild/configs/autobuild/remedynl/mordor_autoconf.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/mordor_acc.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/mordor_gcc.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/mordor_accwchar.sh
