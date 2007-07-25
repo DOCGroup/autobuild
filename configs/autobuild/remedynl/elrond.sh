b2df96e492f88c8453e9f7e4984c2707fcb42a33
Johnny Willemsen <jwillemsen@remedy.nl>
2007-07-25 14:34:49 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4795 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/elrond.sh
+++ configs/autobuild/remedynl/elrond.sh
@@ -9,6 +9,5 @@ svn up
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_cidlc.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_icc64.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_icc32.sh
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_icc10_32.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_gcc.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/elrond_autoconf.sh
