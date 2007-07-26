0213ad07f705b8cc81288c4b27b4f9004ba9b773
Johnny Willemsen <jwillemsen@remedy.nl>
2007-07-26 13:38:23 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4824 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/aragon.bat
@@ -0,0 +1,4 @@
+cd ..\..\..\
+c:\cygwin\bin\svn up
+cd configs\autobuild\remedynl
+call aragon_driver.bat
--- configs/autobuild/remedynl/aragon_driver.bat
+++ configs/autobuild/remedynl/aragon_driver.bat
@@ -5,3 +5,4 @@ perl c:\ACE\autobuild\autobuild.pl aragon_cb6sr.xml
 perl C:\ACE\autobuild\autobuild.pl aragon_vxworks64.xml
 perl C:\ACE\autobuild\autobuild.pl aragon_vxworks65.xml
 perl C:\ACE\autobuild\autobuild.pl aragon_vxworks65k.xml
+call aragon.bat
