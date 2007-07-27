1dde0fc17246592bb3fd7673f41122fe459fe62f
Johnny Willemsen <jwillemsen@remedy.nl>
2007-07-27 10:14:00 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4838 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/gimli_driver.bat
+++ configs/autobuild/remedynl/gimli_driver.bat
@@ -1,7 +1,7 @@
 cd ..\..\..\
-svn up
+c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl gimli_icc10_64.xml
-perl C:\ACE\autobuild\autobuild.pl gimli_icc10_32.xml
-perl C:\ACE\autobuild\autobuild.pl gimli_icc10_32_r.xml
-perl C:\ACE\autobuild\autobuild.pl gimli_icc91_64.xml
+perl D:\ACE\autobuild\autobuild.pl gimli_icc10_64.xml
+perl D:\ACE\autobuild\autobuild.pl gimli_icc10_32.xml
+perl D:\ACE\autobuild\autobuild.pl gimli_icc10_32_r.xml
+perl D:\ACE\autobuild\autobuild.pl gimli_icc91_64.xml
--- configs/autobuild/remedynl/gimli_icc91_64.bat
+++ configs/autobuild/remedynl/gimli_icc91_64.bat
@@ -1,4 +1,4 @@
 cd ..\..\..\
-svn up
+c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl gimli_icc91_64.xml
+perl D:\ACE\autobuild\autobuild.pl gimli_icc91_64.xml
