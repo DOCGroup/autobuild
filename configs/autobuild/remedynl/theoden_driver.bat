c8d5e7067e2a229475ae7b33d7bc23f723195f1c
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-01 14:58:42 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4880 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/theoden_driver.bat
+++ configs/autobuild/remedynl/theoden_driver.bat
@@ -10,4 +10,8 @@ perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007ddu.xml
 perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dds.xml
 perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dd_ipv6.xml
 perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007dd.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_vc71.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_vc71_ipv6_debug.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_vc71dr.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_vc71sd.xml
 call theoden.bat
