0123c0ae95b19a8fca09294b44cca95aaa47fdbd
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-06 19:44:23 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4910 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/theoden_vc71_ipv6_debug.bat
+++ configs/autobuild/remedynl/theoden_vc71_ipv6_debug.bat
@@ -1,4 +1,4 @@
 cd ..\..\..\
 c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl gandalf_vc71_ipv6_debug.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_vc71_ipv6_debug.xml
--- configs/autobuild/remedynl/theoden_vc71sd.bat
+++ configs/autobuild/remedynl/theoden_vc71sd.bat
@@ -1,4 +1,4 @@
 cd ..\..\..\
 svn up
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl gandalf_vc71sd.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_vc71sd.xml
