3415e1632d05a318bd415dfbfb324017379573fc
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-08 08:40:10 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4922 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/blade56/driver.sh
+++ configs/autobuild/isislab/blade56/driver.sh
@@ -3,8 +3,8 @@
 # $Id$
 #
 
-$HOME/autobuild/configs/autobuild/isislab/blade56/driver_corba_e_compact.sh
 $HOME/autobuild/configs/autobuild/isislab/blade56/driver_corba_e_micro.sh
+$HOME/autobuild/configs/autobuild/isislab/blade56/driver_corba_e_compact.sh
 $HOME/autobuild/configs/autobuild/isislab/blade56/driver_minimum_corba.sh
 $HOME/autobuild/configs/autobuild/isislab/blade56/driver_regular.sh
 $HOME/autobuild/configs/autobuild/isislab/blade56/driver_autoconf.sh
