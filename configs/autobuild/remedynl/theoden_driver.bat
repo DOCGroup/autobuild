f3441992aec6b0f103d43f7a7b449a06abdf4980
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-09 14:56:49 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4931 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/theoden_driver.bat
+++ configs/autobuild/remedynl/theoden_driver.bat
@@ -3,12 +3,11 @@ c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
 
 perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007dr.xml
-perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006sr.xml
-perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dd.xml
-perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006sd.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007sr.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007sd.xml
 perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007ddu.xml
-perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dds.xml
-perl C:\ACE\autobuild\autobuild.pl theoden_bcb2006dd_ipv6.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007dds.xml
+perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007dd_ipv6.xml
 perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007dd.xml
 perl C:\ACE\autobuild\autobuild.pl theoden_vc71.xml
 perl C:\ACE\autobuild\autobuild.pl theoden_vc71_ipv6_debug.xml
