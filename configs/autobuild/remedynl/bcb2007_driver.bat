6bedd0d4880ba76092275c3154d02c34f7d26a89
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-13 18:31:53 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4952 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/bcb2007_driver.bat
+++ configs/autobuild/remedynl/bcb2007_driver.bat
@@ -4,7 +4,6 @@ cd configs\autobuild\remedynl
 
 perl C:\ACE\autobuild\autobuild.pl bcb2007dr.xml
 perl C:\ACE\autobuild\autobuild.pl bcb2007ddu.xml
-perl C:\ACE\autobuild\autobuild.pl bcb2007dds.xml
 perl C:\ACE\autobuild\autobuild.pl bcb2007dd_ipv6.xml
 perl C:\ACE\autobuild\autobuild.pl bcb2007dd.xml
 
