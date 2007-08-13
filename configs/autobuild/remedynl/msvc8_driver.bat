e0b5ca75648069708723324ef837d484a16dbbb8
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-13 13:14:52 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4950 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc8_driver.bat
+++ configs/autobuild/remedynl/msvc8_driver.bat
@@ -2,6 +2,6 @@ cd ..\..\..\
 svn up
 cd configs\autobuild\remedynl
 
-perl C:\ACE\autobuild\autobuild.pl msvc_cidlcstatic.xml
+perl C:\ACE\autobuild\autobuild.pl msvc8_cidlcstatic.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_version.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_d.xml
