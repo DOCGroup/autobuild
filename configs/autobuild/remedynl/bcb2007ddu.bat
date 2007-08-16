4869169a719b3459e6707465a718b3d93aa92978
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-16 13:01:31 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4958 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/bcb2007ddu.bat
+++ configs/autobuild/remedynl/bcb2007ddu.bat
@@ -1,4 +1,4 @@
 cd ..\..\..\
-c:\cygwin\bin\svn up
+svn up
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl theoden_bcb2007ddu.xml
+perl C:\ACE\autobuild\autobuild.pl bcb2007ddu.xml
