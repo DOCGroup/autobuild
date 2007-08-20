bf811e8e2674871ce2ad4bc01ce9edb39ed5b6fb
Johnny Willemsen <jwillemsen@remedy.nl>
2007-08-20 08:24:42 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@4982 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc71_driver.bat
+++ configs/autobuild/remedynl/msvc71_driver.bat
@@ -5,5 +5,4 @@ cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl msvc71_d.xml
 perl C:\ACE\autobuild\autobuild.pl msvc71_ipv6_debug.xml
 perl C:\ACE\autobuild\autobuild.pl msvc71_vc71dr.xml
-perl C:\ACE\autobuild\autobuild.pl msvc71_sd.xml
 perl C:\ACE\autobuild\autobuild.pl msvc71_cidlc.xml
--- configs/autobuild/remedynl/msvc71_sd.bat
+++
@@ -1,4 +0,0 @@
-cd ..\..\..\
-svn up
-cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl theoden_vc71sd.xml
