db32f50dc484bdd22b72b1761d052147165cd98e
Simon Massey <simon.massey@prismtech.com>
2007-10-04 13:04:33 +0000

    ChangeLogTag: Tue Oct  4 13:05:00 UTC 2007  Simon Massey  <sma@prismtech.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5120 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard.sh
+++ configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard.sh
@@ -10,10 +10,11 @@ cd /home/tao/doc_autobuild
 svn update autobuild
 cd /home/tao/rad1/doc_scoreboard/lynxos42_ppc_gcc322/standard
 #
-# build ACE and TAO on rhel4 with LynxOS42 PPC gcc 3.2.2 cross compiler
-# Sets ENV_PREFIX=/usr/local/lynx/4.0.0/ppc-322
 echo [`/bin/date`]: Building ACE and TAO on rhel4 with LynxOS4.2 PPC gcc 3.2.2 cross compiler
 /usr/bin/perl /home/tao/doc_autobuild/autobuild/autobuild.pl /home/tao/doc_autobuild/autobuild/configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard.xml
+#
+echo [`/bin/date`]: Package up the build for the target
+/bin/tar -chf cross_tests.tar ACE_wrappers --format=oldgnu --exclude "*.o" --exclude "*.obj" --exclude "*.c" --exclude "*.cpp" --exclude "*.h" --exclude "*.hpp" --exclude "*.i" --exclude "*.inl" --exclude "*.idl" --exclude "*.mpc" --exclude "*.mwc" --exclude "*akefile*" --exclude "*.sln" --exclude "*.vcproj" --exclude CVS --exclude .cvsignore
 #
 echo [`/bin/date`]: Triggering rad1 to run tests
 /usr/bin/nohup /usr/bin/ssh rad1 /bin/nohup /usr/users/tao/doc_autobuild/autobuild/configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard_Tests_Trigger.sh
