e576e5ee03694a41771bd7ee37636922183cc6e3
Simon Massey <simon.massey@prismtech.com>
2007-10-11 13:58:43 +0000

    ChangeLogTag: Thu Oct 11 13:50:00 UTC 2007  Simon Massey  <sma@prismtech.com>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5131 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard_Tests_Trigger.sh
+++ configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard_Tests_Trigger.sh
@@ -4,6 +4,6 @@
 if /bin/test -f /home/tao/rad1/doc_scoreboard/lynxos42_ppc_gcc322/standard/.disable; then echo [`/bin/date`]: .disable exists, previous tests still running; exit; fi
 #
 echo [`/bin/date`]: Triggering detached LynxOS4_PPC_GCC322_Standard_Tests on rad1
-/bin/nohup /home/tao/doc_autobuild/autobuild/configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard_Tests.sh > /home/tao/rad1/log/LynxOS42_PPC_GCC322_Standard_Tests.log 2>&1 < /dev/null &
+/bin/nohup /home/tao/doc_autobuild/autobuild/configs/autobuild/prism/LynxOS42_PPC_GCC322_Standard_Tests.sh > /home/tao/rad1/logs/LynxOS42_PPC_GCC322_Standard_Tests.log 2>&1 < /dev/null &
 echo [`/bin/date`]: Ok, bye for now.
 exit
