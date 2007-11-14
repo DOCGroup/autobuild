be178adec5b4984cf1843dd769734ba2a37219ec
Johnny Willemsen <jwillemsen@remedy.nl>
2007-11-14 10:44:18 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5185 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/shelob.sh
+++ configs/autobuild/remedynl/shelob.sh
@@ -7,6 +7,7 @@ cd $HOME/ACE/autobuild
 svn up
 
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/shelob_gcc.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/shelob_icc101_32.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/shelob_icc10_32.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/shelob_icc10_64.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/shelob_icc91_32.sh
