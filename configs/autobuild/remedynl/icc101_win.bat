1b8b44ab36e81b05e9b68b3bd04e1fa740d2018d
Johnny Willemsen <jwillemsen@remedy.nl>
2007-12-11 19:41:46 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5257 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/icc101_win.bat
+++ configs/autobuild/remedynl/icc101_win.bat
@@ -1,5 +1,5 @@
 cd ..\..\..\
 c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
-perl D:\ACE\autobuild\autobuild.pl icc101_win_64.xml
-perl D:\ACE\autobuild\autobuild.pl icc101_win_32.xml
+perl C:\ACE\autobuild\autobuild.pl icc101_win_64.xml
+perl C:\ACE\autobuild\autobuild.pl icc101_win_32.xml
