dd82320af316ef2c7343174ec67f3eaf0a518c24
Johnny Willemsen <jwillemsen@remedy.nl>
2007-12-19 19:27:50 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5283 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/cygwin.bat
+++ configs/autobuild/remedynl/cygwin.bat
@@ -1,4 +1,4 @@
 cd ..\..\..\
 c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
-c:\cygwin\bin\sh cygwin.sh
+c:\cygwin\bin\bash --login /cygdrive/c/ACE/autobuild/configs/autobuild/remedynl/cygwin.sh
