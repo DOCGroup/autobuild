8e6927fe9c326a1b9c0e0656fc615458da680d39
Johnny Willemsen <jwillemsen@remedy.nl>
2007-12-31 10:46:33 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5308 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/solaris.sh
+++ configs/autobuild/remedynl/solaris.sh
@@ -8,3 +8,5 @@ cd /home/build/ACE/autobuild
 exec /usr/local/bin/perl /home/build/ACE/autobuild/autobuild.pl \
                              /home/build/ACE/autobuild/configs/autobuild/remedynl/solaris_host.xml
 
+exec /usr/local/bin/perl /home/build/ACE/autobuild/autobuild.pl \
+                             /home/build/ACE/autobuild/configs/autobuild/remedynl/solaris_vxworks.xml
