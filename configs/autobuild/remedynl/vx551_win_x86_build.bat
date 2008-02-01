94b612964fc7371b1e35b7ebaeeb0bdea0c1313d
Johnny Willemsen <jwillemsen@remedy.nl>
2008-02-01 12:43:50 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5372 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/vx551_win_x86_build.bat
+++ configs/autobuild/remedynl/vx551_win_x86_build.bat
@@ -3,7 +3,7 @@
 set PATH=%PATH%;c:\tools\pstools;c:\tornado2.2\host\x86-win32\bin
 rem pskill -t tgtsvr.exe
 rem start tgtsvr.exe 10.2.128.65 -n P4 -V -m 61685760 -B wdbrpc -R C:\ -RW -redirectIO -C
-c:\perl\bin\perl c:\ACE\autobuild\autobuild.pl vxworks551_win_x86_build.xml
+c:\perl\bin\perl c:\ACE\autobuild\autobuild.pl vx551_win_x86_build.xml
 rem pskill -t tgtsvr.exe
 
 @endlocal
