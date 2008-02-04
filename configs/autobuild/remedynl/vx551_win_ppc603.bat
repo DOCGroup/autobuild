75c6252769db3e281f4d8d34351e52fe2fab0899
Johnny Willemsen <jwillemsen@remedy.nl>
2008-02-04 09:16:43 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5381 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/vx551_win_ppc603.bat
@@ -0,0 +1,4 @@
+cd ..\..\..\
+c:\cygwin\bin\svn up
+cd configs\autobuild\remedynl
+call vx551_win_ppc603_driver.bat
---
+++ configs/autobuild/remedynl/vx551_win_ppc603_driver.bat
@@ -0,0 +1,5 @@
+cd \ace\autobuild
+c:\cygwin\bin\svn up
+cd configs\autobuild\remedynl
+c:\perl\bin\perl c:\ACE\autobuild\autobuild.pl vx551_win_ppc603_host.xml
+c:\perl\bin\perl C:\ACE\autobuild\autobuild.pl vx551_win_ppc603_build.xml
