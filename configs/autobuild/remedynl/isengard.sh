9419b0ea0da537c028607236c7578f2eea5c8969
Johnny Willemsen <jwillemsen@remedy.nl>
2008-03-07 10:52:01 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5476 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/isengard.sh
+++ configs/autobuild/remedynl/isengard.sh
@@ -5,7 +5,9 @@
 
 cd $HOME/ACE/autobuild
 PATH=/usr/local/bin:/etc:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:$PATH
+LD_LIBRRAY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 export PATH
+export LD_LIBRRAY_PATH
 
 svn up
 
