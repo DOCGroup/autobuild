668c0c22bfd8b17508c0abb2a6a2806bf8bd13f9
Johnny Willemsen <jwillemsen@remedy.nl>
2008-03-11 16:13:08 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5502 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/isengard_host.sh
+++ configs/autobuild/remedynl/isengard_host.sh
@@ -5,6 +5,6 @@
 
 cd /export/home/build/ACE/autobuild
 
-exec /usr/local/bin/perl /export/home/build/ACE/autobuild/autobuild.pl \
+exec /usr/bin/perl /export/home/build/ACE/autobuild/autobuild.pl \
                              /export/home/build/ACE/autobuild/configs/autobuild/remedynl/isengard_host.xml
 
