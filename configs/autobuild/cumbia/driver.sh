53a95791659d8f6d0f9497bdbb29426f2ccceb84
Johnny Willemsen <jwillemsen@remedy.nl>
2008-04-15 19:38:01 +0000

    Tue Apr 15 19:36:24 UTC 2008  William R. Otte  <wotte@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5587 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/cumbia/driver.sh
+++ configs/autobuild/cumbia/driver.sh
@@ -7,7 +7,7 @@ cd $HOME/autobuild
 svn up
 
 # Run the build.
-sh $HOME/autobuild/configs/autobuild/cumbia/NoInt.sh
 sh $HOME/autobuild/configs/autobuild/cumbia/NoIIOP.sh
 sh $HOME/autobuild/configs/autobuild/cumbia/CORBAemicro.sh
 sh $HOME/autobuild/configs/autobuild/cumbia/CORBAecompact.sh
+sh $HOME/autobuild/configs/autobuild/cumbia/NoInt.sh
