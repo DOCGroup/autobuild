74abc4c3899b5be82035276cffa769917b585885
Johnny Willemsen <jwillemsen@remedy.nl>
2008-05-06 11:38:38 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5626 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/anduin.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd $HOME/ACE/autobuild
+PATH=/usr/local/bin:/etc:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:/opt/csw/bin:$PATH
+LD_LIBRRAY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export PATH
+export LD_LIBRRAY_PATH
+
+svn up
+
+sh configs/autobuild/remedynl/anduin_suncc.sh
