f33d04250f0a56dcb8a7a3f43b81d4fb282d1ae8
Johnny Willemsen <jwillemsen@remedy.nl>
2008-07-31 08:02:15 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5753 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/suse110_icc91.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd $HOME/ACE/autobuild
+
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/suse110_icc91_32.xml
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/suse110_icc91_64.xml
+
