f821e67055f0406aea80d5acaaf10e98eed62160
Johnny Willemsen <jwillemsen@remedy.nl>
2008-08-04 09:25:43 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5766 59e07337-6b18-0410-a141-c5efe34a0da7

--- common/prettify.rb
+++ common/prettify.rb
@@ -2,30 +2,32 @@
 
 # This is the ruby code that determines whether a test has failed or not.
 # The integrate scoreboard doesn't parse compile errors, that is only
-# done by the regular scoreboard. If you make changes to this file
-# send an email to Johnny. We will work on making this a seperate
-# function that is used by the scoreboard
+# done by the regular scoreboard.
+
+# Return true if the test succeeds, false if it fails!
 
 def parse_test_line (line)
-  if (line =~ /gethostbyname: getaddrinfo returned ERROR/)
-    return true
+        if (line =~ /gethostbyname: getaddrinfo returned ERROR/)
+                return true
+        end
 
-  if (line =~ /Error/ || line =~ /ERROR/ ||
-      line =~ /fatal/ || line =~ /FAILED/ ||
-      line =~ /EXCEPTION/ || line =~ /ACE_ASSERT/ ||
-        line =~ /Assertion/ || line =~ /Mismatched free/ ||
-        line =~ /are definitely lost in loss record/ ||
-        line =~ /Invalid write of size/ ||
-        line =~ /Invalid read of size/ ||
-        line =~ /aborted due to compilation errors/ ||
-        line =~ /memPartFree: invalid block/ ||
-        line =~ /memPartAlloc: block too big/ ||
-        line =~ /ld error: error loading file/ ||
-        line =~ /C interp: unable to open/ ||
-        line =~ /unknown symbol name/ ||
-        line =~ /pure virtual /i )
-    return true
-  else
-    return false
+        if (line =~ /Error/ || line =~ /ERROR/ ||
+            line =~ /fatal/ || line =~ /FAILED/ ||
+            line =~ /EXCEPTION/ || line =~ /ACE_ASSERT/ ||
+            line =~ /Assertion/ || line =~ /Mismatched free/ ||
+            line =~ /are definitely lost in loss record/ ||
+            line =~ /Invalid write of size/ ||
+            line =~ /Invalid read of size/ ||
+            line =~ /aborted due to compilation errors/ ||
+            line =~ /memPartFree: invalid block/ ||
+            line =~ /memPartAlloc: block too big/ ||
+            line =~ /ld error: error loading file/ ||
+            line =~ /C interp: unable to open/ ||
+            line =~ /unknown symbol name/ ||
+            line =~ /pure virtual /i )
+                return false
+        end
+
+        return true
 end
 
