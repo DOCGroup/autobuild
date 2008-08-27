d5d31a90b4a7e73c6d8e910852de5674560fe850
William R. Otte <wotte@dre.vanderbilt.edu>
2008-08-27 15:02:16 +0000

    Wed Aug 27 15:00:48 UTC 2008  William R. Otte  <wotte@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5786 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/trian/versioned_namespace.bat
@@ -0,0 +1,9 @@
+REM $Id$
+
+call "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.bat"
+
+cd "C:\ACE\autobuild"
+
+svn up
+
+perl autobuild.pl configs\autobuild\isislab\blade11\versioned_namespace.xml
