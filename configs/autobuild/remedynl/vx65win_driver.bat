30e40f335edfab422a68fbc391a8ffab6d699ba1
Johnny Willemsen <jwillemsen@remedy.nl>
2008-09-15 15:00:22 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5812 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/vx65win_driver.bat
+++ configs/autobuild/remedynl/vx65win_driver.bat
@@ -1,6 +1,6 @@
 cd \ace\autobuild
 svn up
 cd configs\autobuild\remedynl
-perl c:\ACE\autobuild\autobuild.pl vx65win_cb6sr.xml
+perl c:\ACE\autobuild\autobuild.pl vx65win_host.xml
 perl C:\ACE\autobuild\autobuild.pl vx65win_vxworks65.xml
 perl C:\ACE\autobuild\autobuild.pl vx65win_vxworks65k.xml
