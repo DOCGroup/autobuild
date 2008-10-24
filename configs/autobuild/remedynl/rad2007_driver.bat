c03caf307effbcdbd54d28aef9ea5a60442d196f
Johnny Willemsen <jwillemsen@remedy.nl>
2008-10-24 18:17:24 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5896 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/rad2007_driver.bat
+++ configs/autobuild/remedynl/rad2007_driver.bat
@@ -5,4 +5,5 @@ cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl rad2007dr.xml
 perl C:\ACE\autobuild\autobuild.pl rad2007ddu.xml
 perl C:\ACE\autobuild\autobuild.pl rad2007dd.xml
+perl C:\ACE\autobuild\autobuild.pl rad2007dd_dds.xml
 
