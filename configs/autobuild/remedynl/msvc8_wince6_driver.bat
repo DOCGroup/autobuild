51b6104017c6c5b662509ac36bb2ebb3349705dd
Johnny Willemsen <jwillemsen@remedy.nl>
2008-11-21 08:39:44 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@5976 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc8_wince6_driver.bat
+++ configs/autobuild/remedynl/msvc8_wince6_driver.bat
@@ -6,3 +6,4 @@ perl C:\ACE\autobuild\autobuild.pl msvc8_wince6_host.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_wince6_d.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_wince6_test.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_wince6_r.xml
+perl C:\ACE\autobuild\autobuild.pl msvc8_wince6_pro_r.xml
