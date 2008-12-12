f22dcf565a9f76c1d2d1ea00c203804ec093e4c6
Johnny Willemsen <jwillemsen@remedy.nl>
2008-12-12 21:45:06 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6058 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/emulab/build22.sh
+++ configs/autobuild/isislab/emulab/build22.sh
@@ -17,7 +17,7 @@
 /users/bczar/runbuild.sh IPV6
 /users/bczar/runbuild.sh linux_no_iiop
 /users/bczar/runbuild.sh linux_versioned_namespace
-/users/bczar/runbuild.sh NoIIOP
+/users/bczar/runbuild.sh FC6_NoIIOP
 /users/bczar/runbuild.sh NoInt
 /users/bczar/runbuild.sh prism_integration
 /users/bczar/runbuild.sh SingleThreaded
