4c227b352fbe10edf69a2145169d0aeb72993ee0
Johnny Willemsen <jwillemsen@remedy.nl>
2008-12-13 04:24:03 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6077 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/tango_isis/driver_c.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-
-exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
-               $HOME/autobuild/configs/autobuild/tango_isis/Full_Reactors.xml 2>&1 
-
--- configs/autobuild/tango_isis/driver_st.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-
-exec /usr/bin/perl -w $HOME/autobuild/autobuild.pl $HOME/autobuild/configs/autobuild/tango_isis/SingleThreaded.xml 
-
