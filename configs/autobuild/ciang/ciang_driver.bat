5bb17a1f38cfa3b19456b35323f57b798de91d9b
Johnny Willemsen <jwillemsen@remedy.nl>
2008-12-17 15:23:14 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6114 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/ciang/ciang_driver.bat
+++ configs/autobuild/ciang/ciang_driver.bat
@@ -3,6 +3,6 @@ cd \ace\autobuild
 cd configs\autobuild\ciang
 perl c:\ACE\autobuild\autobuild.pl ciang_vc9.xml
 perl C:\ACE\autobuild\autobuild.pl ciao_refactor.xml
-perl C:\ACE\autobuild\autobuild.pl ciaocore_vc8.xml
+perl C:\ACE\autobuild\autobuild.pl ciaocore_vc9.xml
 perl C:\ACE\autobuild\autobuild.pl vc9_w4.xml
 perl C:\ACE\autobuild\autobuild.pl vc9_static.xml
