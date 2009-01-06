c4bf242e8b22448d8be612b213c18ec12f5d4eb2
Johnny Willemsen <jwillemsen@remedy.nl>
2009-01-06 18:21:55 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6145 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/trian/trian_driver.bat
+++ configs/autobuild/trian/trian_driver.bat
@@ -3,5 +3,4 @@ svn up
 cd configs\autobuild\trian
 perl c:\ACE\autobuild\autobuild.pl ace_for_tao.xml
 perl C:\ACE\autobuild\autobuild.pl versioned_namespace.xml
-perl C:\ACE\autobuild\autobuild.pl trian_cosmic_release.xml
 perl C:\ACE\autobuild\autobuild.pl vc8_minimum_corba.xml
