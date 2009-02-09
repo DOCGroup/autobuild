dec058d59bf3c4f6ac0cb728fb796093736de77e
Johnny Willemsen <jwillemsen@remedy.nl>
2009-02-09 09:34:10 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6319 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/saruman.sh
+++ configs/autobuild/remedynl/saruman.sh
@@ -10,3 +10,4 @@ sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_icc110.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_cidlc.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_gcc.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_icc101.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_icc111.sh
