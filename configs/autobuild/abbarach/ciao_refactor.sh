c0b48d1945a9defe4e8fd06468b8b658662f5898
Johnny Willemsen <jwillemsen@remedy.nl>
2009-03-02 08:02:23 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6441 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/abbarach/ciao_refactor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-export PATH=/opt/local/bin:$PATH
-exec /usr/bin/perl $HOME/autobuild/autobuild.pl \
-                             $HOME/autobuild/configs/autobuild/abbarach/ciao_refactor.xml
