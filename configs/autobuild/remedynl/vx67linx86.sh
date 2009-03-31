d93d697306cd8e24b57e39d7b594da5d90a9b200
Johnny Willemsen <jwillemsen@remedy.nl>
2009-03-31 12:24:41 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6502 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/vx67linx86.sh
+++ configs/autobuild/remedynl/vx67linx86.sh
@@ -12,3 +12,9 @@ $HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linx86_vxworks67k.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linppc603_vxworks67k.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linx86_vxworks67.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linx86_vxworks67p.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linother_vxworks67r.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linother_vxworks67ripv6.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linother_vxworks67rk.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linother_vxworks67rkp.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/vx67linother_vxworks67sh.sh
+
