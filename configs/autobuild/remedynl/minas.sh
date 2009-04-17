50cfdb216aef87fc71ca534c8818f426b8937502
Johnny Willemsen <jwillemsen@remedy.nl>
2009-04-17 13:13:43 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6520 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/minas.sh
+++ configs/autobuild/remedynl/minas.sh
@@ -4,7 +4,7 @@
 #
 
 cd $HOME/ACE/autobuild
-PATH=/usr/local/bin:/etc:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:$PATH
+PATH=/usr/local/bin:/etc:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:/opt/csw/bin:$PATH
 LD_LIBRRAY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 export PATH
 export LD_LIBRRAY_PATH
