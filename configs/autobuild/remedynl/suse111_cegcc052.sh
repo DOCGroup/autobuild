6d7ac6c54770a98ff78788e3db9987c000c2d35e
Marcel Smit <msmit@remedy.nl>
2009-05-11 08:01:40 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6591 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/suse111_cegcc052.sh
+++ configs/autobuild/remedynl/suse111_cegcc052.sh
@@ -2,5 +2,4 @@
 #
 # $Id$
 #
-exec /usr/bin/perl /home/build/ACE/autobuild/autobuild.pl /home/build/ACE/autobuild/configs/autobuild/remedynl/suse111_cegcc052_host.xml
 exec /usr/bin/perl /home/build/ACE/autobuild/autobuild.pl /home/build/ACE/autobuild/configs/autobuild/remedynl/suse111_cegcc052.xml
