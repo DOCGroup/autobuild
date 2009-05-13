32736f425cc0b7c870eea88d93c4902afc9e728f
Marcel Smit <msmit@remedy.nl>
2009-05-13 07:48:59 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6605 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/cegcc_cygwin.bat
+++ configs/autobuild/remedynl/cegcc_cygwin.bat
@@ -1,6 +1,4 @@
-#
-#
-# $Id$
-#
-cd \cygwin\home\build\ACE\autobuild\configs\autobuild\remedy
+cd ..\..\..\
+c:\cygwin\bin\svn up
+cd configs\autobuild\remedynl
 c:\cygwin\bin\bash --login /home/build/ACE/autobuild/configs/autobuild/remedynl/cegcc_cygwin.sh
