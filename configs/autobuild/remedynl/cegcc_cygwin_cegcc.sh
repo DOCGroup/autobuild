c0c9bf4f2c3258343ea87e0e920a169d7e92d409
Marcel Smit <msmit@remedy.nl>
2009-05-14 05:40:45 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6609 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/cegcc_cygwin_cegcc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# $Id$
+#
+cd /opt
+rm -rf *
+cd /home/build/cegcc/
+svn up
+cd /home/build/cegcc/src/scripts/x86
+rm -rf *
+../build-x86.sh
+#copy all dll to lib-dir
+cd /home/build/opt/ x86mingw32ce/
+find . -name *.dll -type f -print0 |xargs -0 -i cp {} /home/build/ACE/cegcc/ACE_wrappers/lib
