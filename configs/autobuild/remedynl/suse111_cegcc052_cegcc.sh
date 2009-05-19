aa2eaec2a209cca9ed77ed17b90502cfa9e499c1
Marcel Smit <msmit@remedy.nl>
2009-05-19 06:48:32 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6641 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/suse111_cegcc052_cegcc.sh
+++ configs/autobuild/remedynl/suse111_cegcc052_cegcc.sh
@@ -7,6 +7,10 @@ svn up
 cd /home/build/cegcc/src/scripts/x86
 rm -rf *
 ../build-x86.sh
+# rename libcoredll6.a to libcoredll.a to avoid
+# linker errors
+cd /opt/x86mingw32ce/i386-mingw32ce/lib/
+mv libcoredll6.a libcoredll.a -f
 #copy all dll to lib-dir
 cd /home/build/opt/ x86mingw32ce/
 find . -name *.dll -type f -print0 |xargs -0 -i cp {} /home/build/ACE/cegcc/ACE_wrappers/lib
