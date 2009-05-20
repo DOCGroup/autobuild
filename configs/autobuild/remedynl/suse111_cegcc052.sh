d53345cfcac669f91f0200409eb107f28f0b0564
Johnny Willemsen <jwillemsen@remedy.nl>
2009-05-20 12:50:33 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6652 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/suse111_cegcc052.sh
+++ configs/autobuild/remedynl/suse111_cegcc052.sh
@@ -2,4 +2,5 @@
 #
 # $Id$
 #
+exec /usr/bin/perl /home/build/ACE/autobuild/autobuild.pl /home/build/ACE/autobuild/configs/autobuild/remedynl/suse111_cegcc052_host.xml
 exec /usr/bin/perl /home/build/ACE/autobuild/autobuild.pl /home/build/ACE/autobuild/configs/autobuild/remedynl/suse111_cegcc052.xml
