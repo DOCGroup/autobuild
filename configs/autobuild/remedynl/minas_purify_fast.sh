7c1522da1f6866186db8adf2d10d43fbdd7b84c8
Johnny Willemsen <jwillemsen@remedy.nl>
2009-06-04 07:00:57 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6702 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/minas_purify_fast.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd /export/home/build/ACE/autobuild
+
+exec /opt/csw/bin/perl /export/home/build/ACE/autobuild/autobuild.pl \
+                             /export/home/build/ACE/autobuild/configs/autobuild/remedynl/minas_purify_fast.xml
+
