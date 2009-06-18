ec9482bbd79677b078ccee4b28cbae66326efe10
Johnny Willemsen <jwillemsen@remedy.nl>
2009-06-18 08:14:33 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6740 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc8_wince6_x86.bat
+++ configs/autobuild/remedynl/msvc8_wince6_x86.bat
@@ -3,5 +3,6 @@ cd ..\..\..\
 cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl msvc8_wince6_x86_host.xml
 perl C:\ACE\autobuild\autobuild.pl cegcc_mingw_idlstubs.xml
+c:\cygwin\bin\bash --login /cygdrive/c/ACE/autobuild/configs/autobuild/remedynl/cegcc_cygwin_cegcc.sh
 c:\cygwin\bin\bash --login /cygdrive/c/ACE/autobuild/configs/autobuild/remedynl/cegcc_cygwin.sh
 rem perl C:\ACE\autobuild\autobuild.pl msvc8_wince6_x86_d.xml
