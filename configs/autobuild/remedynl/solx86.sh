613b5f512224dfb7b049d3b3ffaa3d747516c44d
Johnny Willemsen <jwillemsen@remedy.nl>
2009-06-28 17:19:32 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6750 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/solx86.sh
+++ configs/autobuild/remedynl/solx86.sh
@@ -4,7 +4,7 @@
 #
 
 cd /export/home/build/ACE/autobuild
-PATH=/usr/local/bin:/etc:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:/opt/csw/bin:$PATH
+PATH=/usr/local/bin:/etc:/opt/sunstudio12.1/bin:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:/opt/csw/bin:$PATH
 LD_LIBRRAY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 export PATH
 export LD_LIBRRAY_PATH
