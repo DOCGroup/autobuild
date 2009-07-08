7367bfe879d116787c75305d5d06cf30804ac547
Johnny Willemsen <jwillemsen@remedy.nl>
2009-07-08 13:04:56 +0000

    removed cidlc builds
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6768 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc8_driver.bat
+++ configs/autobuild/remedynl/msvc8_driver.bat
@@ -2,7 +2,6 @@ cd ..\..\..\
 svn up
 cd configs\autobuild\remedynl
 
-perl C:\ACE\autobuild\autobuild.pl msvc8_cidlc.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_version.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_d.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_r.xml
--- configs/autobuild/remedynl/saruman.sh
+++ configs/autobuild/remedynl/saruman.sh
@@ -7,7 +7,6 @@ cd $HOME/ACE/autobuild
 svn up
 
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_icc110.sh
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_cidlc.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_gcc.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_icc101.sh
 sh $HOME/ACE/autobuild/configs/autobuild/remedynl/saruman_icc111.sh
