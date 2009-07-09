e610e6e63607d9bc46dd344dadf73c2bef74567d
Olli Savia <ops@iki.fi>
2009-07-09 12:09:21 +0000

    ChangeLogTag: Thu Jul  9 12:08:03 UTC 2009  Olli Savia  <ops@iki.fi>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6772 59e07337-6b18-0410-a141-c5efe34a0da7

--- common/parse_compiler_output.pm
+++ common/parse_compiler_output.pm
@@ -137,6 +137,12 @@ sub handle_compiler_output_line($) {
     return;
   }
 
+  if ($s =~ m/is often misused/) {
+    # Ignore some annoying warnings on OpenBSD
+    $self->Output_Normal ($s);
+    return;
+  }
+
   if ($s =~ m/\-W:c:,\-Xmismatch\-warning\=2/) {
     # catch VxWorks DIAB warning option before it is caught as Warning
     $self->Output_Normal ($s);
