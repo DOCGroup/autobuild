6cc9df02c07ee235e4fa6abf47bf972945fd74f1
Johnny Willemsen <jwillemsen@remedy.nl>
2009-08-08 09:52:57 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6859 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/suse111_cegcc052_cegcc.sh
+++ configs/autobuild/remedynl/suse111_cegcc052_cegcc.sh
@@ -16,3 +16,4 @@ mv libcoredll6.a libcoredll.a -f
 #copy all dll to lib-dir
 cd /home/build/opt/x86mingw32ce/
 find . -name *.dll -type f -print0 |xargs -0 -i cp {} /home/build/ACE/cegcc/ACE_wrappers/lib
+scp /home/build/cegccbuild.txt johnnyw@naboo.dre.vanderbilt.edu:/web/users/remedynl/cegccbuild.txt
