6be1de901f50de7e79d4f885c3b44a734dc9eb98
Johnny Willemsen <jwillemsen@remedy.nl>
2009-08-12 13:50:16 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6893 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/sparc/sparc.sh
+++ configs/autobuild/sparc/sparc.sh
@@ -9,6 +9,6 @@ export PATH
 # OpenSSL requires an explicit source of entropy on Solaris.  The
 # Entropy Gathering Daemon perl script fulfills that requirement.
 
-sh configs/autobuild/sparc/driver_gcc402.sh
-sh configs/autobuild/sparc/driver_studio.sh
+sh $HOME/autobuild/configs/autobuild/sparc/driver_gcc402.sh
+sh $HOME/autobuild/configs/autobuild/sparc/driver_studio.sh
 
