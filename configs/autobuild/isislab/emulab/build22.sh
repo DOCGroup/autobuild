ecad3b6782bd3b80a636c203a44e8aca4bf898d5
Johnny Willemsen <jwillemsen@remedy.nl>
2009-08-12 09:26:41 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@6886 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/emulab/build22.sh
+++ configs/autobuild/isislab/emulab/build22.sh
@@ -22,7 +22,7 @@
 /users/bczar/runbuild.sh FC8NoIIOP
 /users/bczar/runbuild.sh FC8NoInt
 /users/bczar/runbuild.sh FC8SingleThreaded
-/users/bczar/runbuild.sh FC8Static
+/users/bczar/runbuild.sh FC8Static2
 /users/bczar/runbuild.sh FC8versioned
 /users/bczar/runbuild.sh FC8wchar
 /users/bczar/runbuild.sh FC6fpCORBAecompact
