4f4c4eea4788940cb6c6daa627c45453bafde7c2
Marcel Smit <msmit@remedy.nl>
2009-10-12 07:15:25 +0000

    New ACE only build for PS3.
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7005 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/ps3_gcc_ace.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $
+#
+
+cd $HOME/ACE/autobuild
+
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3_gcc_ace.xml
+
