08b76f55390843163866934138276070bb360287
William R. Otte <wotte@dre.vanderbilt.edu>
2009-11-10 15:37:42 +0000

    Tue Nov 10 15:37:27 UTC 2009  William R. Otte  <wotte@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7059 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/zifnab.sh
+++ configs/autobuild/zifnab/zifnab.sh
@@ -1,3 +1,4 @@
 svn up /builds/autobuild
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/gcc32.xml
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/gcc64.xml
+perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/icc64.xml
