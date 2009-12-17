80ebc500159eb2c7c5888201e867957b52213607
Abdullah Sowayan <sowayan@me.com>
2009-12-17 02:19:01 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7125 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/zifnab.sh
+++ configs/autobuild/zifnab/zifnab.sh
@@ -1,6 +1,7 @@
 svn up /builds/autobuild
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/Snow_Leopard_Static_Host.xml
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Hardware.xml
+perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Simulator.xml
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/gcc32.xml
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/gcc64.xml
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/icc64.xml
