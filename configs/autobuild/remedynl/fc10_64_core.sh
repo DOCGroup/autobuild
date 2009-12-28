066901c5b15de61aff66d7ec2781fd0e729f9286
Johnny Willemsen <jwillemsen@remedy.nl>
2009-12-28 12:55:30 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7135 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc10_64_core.sh
+++ configs/autobuild/remedynl/fc10_64_core.sh
@@ -7,4 +7,4 @@ cd $HOME/ACE/autobuild
 svn up
 
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fangorn_ccm.xml
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc10_64_core.xml
