3e96a21a91be0569b30c83d70fcc7343cb1d7fce
Johnny Willemsen <jwillemsen@remedy.nl>
2010-01-22 10:42:20 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7186 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/ps3cellbuild_xampler_run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $Id$
+#
+cd $HOME/ACE/autobuild
+
+svn up
+
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3cellbuild_xampler_host.sh
+# sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3cellbuild_xampler.sh
