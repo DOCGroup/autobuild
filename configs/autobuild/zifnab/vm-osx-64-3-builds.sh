226315d0e4bc6694f4cd73d6305e55895fedeeec
William R. Otte <wotte@dre.vanderbilt.edu>
2010-01-23 14:29:50 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7200 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-3-builds.sh
+++ configs/autobuild/zifnab/vm-osx-64-3-builds.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "`date`" > ~/last_run
+perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/gcc32.xml
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/icc64.xml
 sudo shutdown -h now
 
