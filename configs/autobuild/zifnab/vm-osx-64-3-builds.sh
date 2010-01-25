53bfe37772b97fb77ccecdafeda51bf4c603a32e
Abdullah Sowayan <sowayan@me.com>
2010-01-25 21:33:30 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7214 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-3-builds.sh
+++ configs/autobuild/zifnab/vm-osx-64-3-builds.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "`date`" > ~/last_run
 perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/Snow_Leopard_Static_Host.xml
-perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Simulator.xml 
-perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Hardware.xml 
+perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Simulator.xml & 
+perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Hardware.xml &
+wait
 sudo shutdown -h now
 
