4bd35129ce4d8b0239a7ba4edd2fe3fde47bc4cc
William R. Otte <wotte@dre.vanderbilt.edu>
2010-02-09 16:45:31 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7305 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-3-builds.sh
+++ configs/autobuild/zifnab/vm-osx-64-3-builds.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 echo "`date`" > ~/last_run
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/Snow_Leopard_Static_Host.xml
+
+ssh -o ConnectTimeout=10 root@192.168.69.102 reboot
+sleep 30
+ssh wotte@192.168.69.108 /builds/autobuild/configs/autobuild/zifnab/jailbreak.sh
+sleep 30
+
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Hardware.xml 
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Simulator.xml 
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPad_3.2_Simulator.xml 
