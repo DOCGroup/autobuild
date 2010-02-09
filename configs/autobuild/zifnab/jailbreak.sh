aca53fd3ab8fdc7e75eb84465e14a905367ac620
Abdullah Sowayan <sowayan@me.com>
2010-02-09 03:46:11 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7296 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/jailbreak.sh
+++ configs/autobuild/zifnab/jailbreak.sh
@@ -5,8 +5,11 @@
 #
 # You need to do the following to make this work
 #
-# 1) Go to "System Preferences", select "Universal Access"
-#    then click on "Enable access for assistive devices"
+# 1) Copy blackra1n (the tool used to jailbreak iPhone/iPod Touch) to the
+#    Mac's Application folder. 
+#
+# 2) Go to "System Preferences", select "Universal Access"
+#    then click on "Enable access for assistive devices."
 
 kill -HUP `ps -axwwwww | grep blackra1n | grep -v grep | awk '{print $1}'`
 osascript jailbreak.scpt
