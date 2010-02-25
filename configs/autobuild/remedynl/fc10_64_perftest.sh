d572c8bcfc9bc3ab5cd967542d31c3bb828c9859
Johnny Willemsen <jwillemsen@remedy.nl>
2010-02-25 11:27:06 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7375 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc10_64_perftest.sh
+++ configs/autobuild/remedynl/fc10_64_perftest.sh
@@ -5,6 +5,8 @@
 
 cd $HOME/ACE/autobuild
 
+svn up
+
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
                              $HOME/ACE/autobuild/configs/autobuild/remedynl/fc10_64_perftest.xml
 
