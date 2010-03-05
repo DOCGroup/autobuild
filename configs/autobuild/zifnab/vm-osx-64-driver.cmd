0eb1cc11b5f6e9db7a3b3decad9bded3cf0f9429
William R. Otte <wotte@dre.vanderbilt.edu>
2010-03-05 16:57:49 +0000

    rolling back icecream changes
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7407 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-driver.cmd
+++ configs/autobuild/zifnab/vm-osx-64-driver.cmd
@@ -1,6 +1,3 @@
 #!/bin/sh
 svn up  /builds/autobuild
-export PATH=/opt/icecream/bin:$PATH
-sudo /opt/icecream/sbin/iceccd &
-export ICECC_VERSION=/Users/bczar/9729ecda0c3e2a5ab080917526226623.tar.gz
 exec /builds/autobuild/configs/autobuild/zifnab/vm-osx-64-builds.sh
