885b8e18623b62c2591f9f507638df66997f3315
William R. Otte <wotte@dre.vanderbilt.edu>
2010-03-07 21:26:32 +0000

    adding icecream back to gcc32/64
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7408 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-builds.sh
+++ configs/autobuild/zifnab/vm-osx-64-builds.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "`date`" > ~/last_run
+sudo /opt/icecream/sbin/iceccd &
+export ICECC_VERSION=/Users/bczar/9729ecda0c3e2a5ab080917526226623.tar.gz
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/gcc32.xml
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/gcc64.xml
 sudo shutdown -h now
