115e20728d7822b116eb0b0ef36fa850c39ac252
Marcel Smit <msmit@remedy.nl>
2010-03-09 09:33:21 +0000

    Renamed
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7415 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc10_64_ts_qc.sh
+++ configs/autobuild/remedynl/fc10_64_ts_qc.sh
@@ -7,4 +7,4 @@ cd $HOME/ACE/autobuild
 svn up
 
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc10_64_thr_switch.xml
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc10_64_ts_qc.xml
