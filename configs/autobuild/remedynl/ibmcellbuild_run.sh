820b33c8d6dc1303c081df7feaf8b7a815db1665
Johnny Willemsen <jwillemsen@remedy.nl>
2010-03-30 14:06:00 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7451 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/ibmcellbuild_run.sh
+++ configs/autobuild/remedynl/ibmcellbuild_run.sh
@@ -6,4 +6,4 @@ cd $HOME/ACE/autobuild
 
 svn up
 
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/fc10_64_release.sh
