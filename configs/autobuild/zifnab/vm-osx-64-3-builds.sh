2a022095d320418562c5d0cf8aa82a75d5003649
Abdullah Sowayan <sowayan@me.com>
2010-04-08 20:24:19 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7467 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-3-builds.sh
+++ configs/autobuild/zifnab/vm-osx-64-3-builds.sh
@@ -10,5 +10,6 @@ sleep 60
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Hardware.xml 
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Simulator.xml 
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPad_3.2_Simulator.xml 
+perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPhone_4.0_Simulator.xml
 sudo shutdown -h now
 
