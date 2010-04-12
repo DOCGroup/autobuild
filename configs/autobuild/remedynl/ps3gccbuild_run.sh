cbfb558b947544c036ce2ce0538b722c3dd56887
Martin Corino <mcorino@remedy.nl>
2010-04-12 10:27:20 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7469 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/ps3gccbuild_run.sh
+++ configs/autobuild/remedynl/ps3gccbuild_run.sh
@@ -6,4 +6,4 @@ cd $HOME/ACE/autobuild
 
 svn up
 
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3gccbuild_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3gccbuild.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3gccbuild_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3gccbuild.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3gccbuild_crosstest.sh
