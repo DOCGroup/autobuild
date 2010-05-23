0f4f14b699f44a75f327f93d04348d10cfe8e38b
William R. Otte <wotte@dre.vanderbilt.edu>
2010-05-23 12:01:10 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7502 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-3-builds.sh
+++ configs/autobuild/zifnab/vm-osx-64-3-builds.sh
@@ -4,7 +4,8 @@ perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifna
 
 ssh -o ConnectTimeout=15 root@192.168.69.102 reboot
 sleep 60
-ssh wotte@192.168.69.108 /builds/autobuild/configs/autobuild/zifnab/jailbreak.sh
+# Jailbreak is now unteathered, simple reboot should do.
+# ssh wotte@192.168.69.108 /builds/autobuild/configs/autobuild/zifnab/jailbreak.sh
 sleep 60
 
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Hardware.xml 
