d5146cbc371196d16bfc66a364127c6aa8ad8902
Marcel Smit <msmit@remedy.nl>
2010-07-27 10:44:52 +0000

    update
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7591 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/suse113_gcc.sh
+++ configs/autobuild/remedynl/suse113_gcc.sh
@@ -8,5 +8,5 @@ cd $HOME/ACE/autobuild
 svn up
 
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/suse112_gcc.xml
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/suse113_gcc.xml
 
