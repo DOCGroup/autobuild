ad6cbc3a20be24ddd4293c8eee38784363971985
William R. Otte <wotte@dre.vanderbilt.edu>
2010-09-01 14:12:57 +0000

    remove icc build
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7619 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/vm-osx-64-2-builds.sh
+++ configs/autobuild/zifnab/vm-osx-64-2-builds.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "`date`" > ~/last_run
-perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/icc64.xml
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/gcc64.xml
 sudo shutdown -h now
 
