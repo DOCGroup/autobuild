5776f73cdadc350555147b6335fd8427a687b500
Brian Johnson <johnsonb@ociweb.com>
2011-01-19 14:27:36 +0000

    Missed adding jboss_build to autobuild.pl.
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7742 59e07337-6b18-0410-a141-c5efe34a0da7

--- autobuild.pl
+++ autobuild.pl
@@ -50,6 +50,7 @@ require command::quickfast_auto_run_tests;
 require command::check_compiler;
 require command::check_linker;
 require command::doxygen;
+require command::jboss_build;
 require command::print_os_version;
 require command::print_remote_os_version;
 require command::print_env_vars;
