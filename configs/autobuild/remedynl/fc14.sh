cc6a2124731734806c28dbf81fcc5b5433cb10ce
Johnny Willemsen <jwillemsen@remedy.nl>
2011-02-11 12:12:28 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7781 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc14.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-cd $HOME/ACE/autobuild
-
-svn up
-
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/fc14_valgrind.sh && sh $HOME/ACE/autobuild/configs/autobuild/remedynl/fc14_gcc.sh
