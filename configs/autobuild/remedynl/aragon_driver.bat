2c0ac992e4a641b3fa8f6c26cb447d0d7ff39f9b
Johnny Willemsen <jwillemsen@remedy.nl>
2011-02-11 12:42:13 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7783 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/aragon_driver.bat
+++
@@ -1,6 +0,0 @@
-cd \ace\autobuild
-c:\cygwin\bin\svn up
-cd configs\autobuild\remedynl
-perl c:\ACE\autobuild\autobuild.pl aragon_win_ppc603_host.xml
-perl C:\ACE\autobuild\autobuild.pl aragon_win_ppc603_build.xml
-rem perl C:\ACE\autobuild\autobuild.pl aragon_win_ppc603_test.xml
--- configs/autobuild/remedynl/aragon_vxworks-pcPentiumXSharedLib-gnu-run.bat
+++
@@ -1,7 +0,0 @@
-@setlocal
-
-set PATH=%PATH%;c:\tools\pstools;c:\tornado2.2\host\x86-win32\bin
-rem pskill -t tgtsvr.exe
-perl c:\ACE\autobuild\autobuild.pl aragon_vxworks-pcPentiumXSharedLib-gnu.xml
-
-@endlocal
