f633d5cead768ecb252db2a23b7e7d8228661cef
Johnny Willemsen <jwillemsen@remedy.nl>
2011-02-27 19:08:50 +0000

    added svn up
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7827 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/mpc440build.sh
+++ configs/autobuild/remedynl/mpc440build.sh
@@ -5,6 +5,8 @@
 
 cd $HOME/mpc440_build/ACE/autobuild
 
+svn up
+
 exec /usr/bin/perl $HOME/mpc440_build/ACE/autobuild/autobuild.pl \
                              $HOME/mpc440_build/ACE/autobuild/configs/autobuild/remedynl/mpc440build.xml
 
