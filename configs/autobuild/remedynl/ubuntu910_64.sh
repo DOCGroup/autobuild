acd36ffb97988264c75aa007e223983e87e38e0a
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-04 10:16:56 +0000

    removed autoconf build
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7840 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/ubuntu910_64.sh
+++ configs/autobuild/remedynl/ubuntu910_64.sh
@@ -6,4 +6,4 @@ cd $HOME/ACE/autobuild
 
 svn up
 
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ubuntu910_64_autoconf.sh && sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ubuntu910_64_gcc.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ubuntu910_64_gcc.sh
