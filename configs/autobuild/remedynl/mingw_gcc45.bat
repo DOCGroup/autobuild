c08a8fbe23a99c13257568368343c9928470b9cd
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-06 10:14:26 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7850 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/mingw_gcc45.bat
+++ configs/autobuild/remedynl/mingw_gcc45.bat
@@ -1,4 +1,4 @@
 cd C:\ACE\autobuild
 C:\cygwin\bin\svn up
 cd C:\ACE\autobuild\configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl mingw_new.xml
+perl C:\ACE\autobuild\autobuild.pl mingw_gcc45.xml
