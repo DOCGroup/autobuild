9021710c64074c81a03fe9ed7d51519b811c6e7d
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-06 09:51:23 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7848 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/mpc440build_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-cd $HOME/mpc440_build/ACE/autobuild
-
-svn up
-
-sh $HOME/mpc440_build/ACE/autobuild/configs/autobuild/remedynl/mpc440build_host.sh && $HOME/mpc440_build/ACE/autobuild/configs/autobuild/remedynl/mpc440build.sh
