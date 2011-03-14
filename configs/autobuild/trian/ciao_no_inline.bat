e7a452c0305cfc13d7f436cdb2e30ece30925462
William R. Otte <wotte@dre.vanderbilt.edu>
2011-03-14 14:52:16 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7866 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/trian/ciao_no_inline.bat
+++ configs/autobuild/trian/ciao_no_inline.bat
@@ -2,7 +2,6 @@ REM $Id$
 
 cd ..\..\..\..\
 svn up
-cd configs\autobuild\isislab\blade10
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 perl C:\bczar\autobuild\autobuild.pl ciao_no_inline.xml
