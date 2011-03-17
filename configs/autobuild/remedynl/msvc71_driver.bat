9f035a0b94cc1de70a37cdc7a77a5fc42f7c8587
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-17 15:02:18 +0000

    cleanupp
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7880 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc71_driver.bat
+++
@@ -1,7 +0,0 @@
-cd ..\..\..\
-svn up
-cd configs\autobuild\remedynl
-
-perl C:\ACE\autobuild\autobuild.pl msvc71_d.xml
-perl C:\ACE\autobuild\autobuild.pl msvc71_ipv6_debug.xml
-perl C:\ACE\autobuild\autobuild.pl msvc71_vc71dr.xml
