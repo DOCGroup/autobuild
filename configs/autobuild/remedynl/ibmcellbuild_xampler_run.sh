6aac96b5c299682aa0e8541208ee20d5e6d51f75
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-18 12:23:54 +0000

    cleanup build
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7882 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/ibmcellbuild_xampler_run.sh
+++ configs/autobuild/remedynl/ibmcellbuild_xampler_run.sh
@@ -6,5 +6,5 @@ cd $HOME/ACE/autobuild
 
 svn up
 
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3cellbuild_xampler_host.sh
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ps3cellbuild_xampler.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_xampler_host.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_xampler.sh
