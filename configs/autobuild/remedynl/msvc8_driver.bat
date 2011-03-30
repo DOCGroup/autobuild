beffcee8392551f3b2d98c509e611e8f57fa1363
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-30 09:49:37 +0000

    removed some msvc8 builds
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7896 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc8_driver.bat
+++ configs/autobuild/remedynl/msvc8_driver.bat
@@ -2,6 +2,4 @@ cd ..\..\..\
 c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
 
-perl C:\ACE\autobuild\autobuild.pl msvc8_version.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_d.xml
-perl C:\ACE\autobuild\autobuild.pl msvc8_r.xml
