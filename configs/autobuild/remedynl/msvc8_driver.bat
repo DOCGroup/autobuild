20834257f3b1cdf14bf5fec0f5609231606b478e
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-30 09:38:07 +0000

    removed obsolete files
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7893 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/msvc8_driver.bat
+++ configs/autobuild/remedynl/msvc8_driver.bat
@@ -5,4 +5,3 @@ cd configs\autobuild\remedynl
 perl C:\ACE\autobuild\autobuild.pl msvc8_version.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_d.xml
 perl C:\ACE\autobuild\autobuild.pl msvc8_r.xml
-perl C:\ACE\autobuild\autobuild.pl msvc8_pro_r.xml
--- configs/autobuild/remedynl/msvc90_minimum_corba.bat
+++
@@ -1,4 +0,0 @@
-cd ..\..\..\
-c:\cygwin\bin\svn up
-cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl msvc90_minimum_corba.xml
