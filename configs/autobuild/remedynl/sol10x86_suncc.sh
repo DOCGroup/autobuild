92a00bd80ff5708e1ec3faa60afd5c3b1aae6c88
Johnny Willemsen <jwillemsen@remedy.nl>
2011-03-30 13:37:33 +0000

    toolchain upgrade
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7904 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/sol10x86_suncc.sh
+++ configs/autobuild/remedynl/sol10x86_suncc.sh
@@ -5,6 +5,13 @@
 
 cd /export/home/build/ACE/autobuild
 
+PATH=/usr/local/bin:/etc:/opt/sunstudio12.1/bin:/opt/SUNWspro/bin:/usr/ccs/bin:/usr/ucb:/opt/csw/bin:$PATH
+LD_LIBRRAY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export PATH
+export LD_LIBRRAY_PATH
+
+svn up
+
 exec /usr/bin/perl /export/home/build/ACE/autobuild/autobuild.pl \
                              /export/home/build/ACE/autobuild/configs/autobuild/remedynl/sol10x86_suncc.xml
 
