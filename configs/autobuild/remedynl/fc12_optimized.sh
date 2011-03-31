9e96b07d749e957a647a40a4d8f9e7e202df1dd6
Marijke Hengstmengel <mhengstmengel@remedy.nl>
2011-03-31 12:02:51 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7930 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/fc12_optimized.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd $HOME/ACE/autobuild
+
+svn up
+
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc12_optimized.xml
+
