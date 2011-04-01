00ba29d3028b26059bdbf4f4aa2c22c327dbd588
Johnny Willemsen <jwillemsen@remedy.nl>
2011-04-01 08:53:22 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7944 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/ibmcellbuild_run.sh
+++ configs/autobuild/remedynl/ibmcellbuild_run.sh
@@ -6,6 +6,6 @@ cd $HOME/ACE/autobuild
 
 svn up
 
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_host.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild.sh
 
 
--- configs/autobuild/remedynl/ibmcellbuild_xampler_run.sh
+++ configs/autobuild/remedynl/ibmcellbuild_xampler_run.sh
@@ -6,4 +6,4 @@ cd $HOME/ACE/autobuild
 
 svn up
 
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_xampler_host.sh && sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_xampler.sh
+sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_xampler_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ibmcellbuild_xampler.sh
