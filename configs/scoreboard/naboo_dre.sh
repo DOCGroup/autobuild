0098a83d3e2b4da72ef3d9f7a86c4535e971153e
Johnny Willemsen <jwillemsen@remedy.nl>
2011-04-01 11:07:17 +0000

    scoreboard is run on naboo
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7946 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/scoreboard/naboo_dre.sh
+++ configs/scoreboard/naboo_dre.sh
@@ -6,6 +6,7 @@ PERLLIB=autobuild
 export PERLLIB
 
 cd $HOME/autobuild
+svn cleanup
 svn up
 
 # /usr/bin/perl $HOME/autobuild/scoreboard.pl -o /export/project/workarea01/Scoreboard/html -c $HOME/autobuild/configs/scoreboard/acetao.xml 2>&1 &
