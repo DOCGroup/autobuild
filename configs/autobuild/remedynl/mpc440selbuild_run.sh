5774c95eddc62c57c299e63cf0d83961649b426b
Johnny Willemsen <jwillemsen@remedy.nl>
2011-04-09 08:25:24 +0000

    removed obsole builds
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@7986 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/mpc440selbuild_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-cd $HOME/ACE/autobuild
-
-svn up
-
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/mpc440selbuild_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/mpc440selbuild.sh
