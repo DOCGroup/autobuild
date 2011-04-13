6f7d844bd55fd58b579b8d6bf51b86305deb0547
Marcel Smit <msmit@remedy.nl>
2011-04-13 07:33:57 +0000

    Autobuild files for Android 8 and Android 9
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8023 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/android9_fc14_host.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+exec /usr/bin/perl /home/build/ACE/autobuild/autobuild.pl \
+                             /home/build/ACE/autobuild/configs/autobuild/remedynl/android9_fc14_host.xml
