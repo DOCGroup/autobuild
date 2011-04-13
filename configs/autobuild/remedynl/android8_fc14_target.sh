c9e751cad424e6e22ec48cd5dd72b2c68f7854f6
Marcel Smit <msmit@remedy.nl>
2011-04-13 09:46:11 +0000

    Always update the autobuild directory.
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8029 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/android8_fc14_target.sh
+++ configs/autobuild/remedynl/android8_fc14_target.sh
@@ -2,6 +2,9 @@
 #
 # $Id$
 #
+cd $HOME/ACE/autobuild
+
+svn up
 
 exec /usr/bin/perl /home/build/ACE/autobuild/autobuild.pl \
                              /home/build/ACE/autobuild/configs/autobuild/remedynl/android8_fc14_target.xml
