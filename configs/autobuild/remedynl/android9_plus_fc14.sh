f63f5eb6e064987caaf61373a7ddc92768608a90
Marcel Smit <msmit@remedy.nl>
2011-04-14 11:26:19 +0000

    Different files for the new build machine
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8054 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/android9_plus_fc14.sh
+++ configs/autobuild/remedynl/android9_plus_fc14.sh
@@ -7,6 +7,6 @@ cd $HOME/ACE/autobuild
 
 svn up
 
-$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_fc14_host.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_fc14_target.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_fc14_host.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_fc14_target.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_static_fc14_target.sh
