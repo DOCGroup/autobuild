445e226d22bb92b60d32ed2048a378dde15a4eb0
Marcel Smit <msmit@remedy.nl>
2011-04-14 10:36:40 +0000

    Added another build for Android9. A new VM will be build shared libraries and static once a day
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8051 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/android9_plus_fc14.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd $HOME/ACE/autobuild
+
+svn up
+
+$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_fc14_host.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_fc14_target.sh
+$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_static_fc14_target.sh
