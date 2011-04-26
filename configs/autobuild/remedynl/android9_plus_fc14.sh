41a10c8b9ab83d4f14ca99effb1b6c29eba99593
Marcel Smit <msmit@remedy.nl>
2011-04-26 10:30:07 +0000

    Due to compiler issues regarding dynamic_cast on Android, only build static
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8129 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/android9_plus_fc14.sh
+++ configs/autobuild/remedynl/android9_plus_fc14.sh
@@ -8,5 +8,6 @@ cd $HOME/ACE/autobuild
 svn up
 
 $HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_fc14_host.sh
-$HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_fc14_target.sh
+# No shared object builds due to problems with the Android compiler regarding dynamic_cast
+# $HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_fc14_target.sh
 $HOME/ACE/autobuild/configs/autobuild/remedynl/android9_plus_static_fc14_target.sh
