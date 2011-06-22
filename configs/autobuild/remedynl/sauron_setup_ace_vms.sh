cff6765ffb2d52a45caefd86895b2159832b60d3
Johnny Willemsen <jwillemsen@remedy.nl>
2011-06-22 17:05:33 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8188 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/sauron_setup_ace_vms.sh
+++ configs/autobuild/remedynl/sauron_setup_ace_vms.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-svn up $HOME/ACE/autobuild
-mkdir $HOME/ACE/openvms
-cd $HOME/ACE/openvms
+svn up $HOME/ace/autobuild
+mkdir $HOME/ace/openvms
+cd $HOME/ace/openvms
 rm -rf ace_vms_update.tar
 svn co svn://svn.dre.vanderbilt.edu/DOC/Middleware/sets-anon/ACE+TAO .
 tar -cf ace_vms_update.tar -g ace_vms_update.tar_inc ACE_wrappers --exclude-vcs
