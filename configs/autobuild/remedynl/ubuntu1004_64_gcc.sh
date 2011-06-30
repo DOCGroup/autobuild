ef076b4560a0e4b8a9fad674440f854feaaf675d
Johnny Willemsen <jwillemsen@remedy.nl>
2011-06-30 11:56:49 +0000

    updated
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8191 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/ubuntu1004_64_gcc.sh
+++ configs/autobuild/remedynl/ubuntu1004_64_gcc.sh
@@ -8,5 +8,5 @@ cd $HOME/ACE/autobuild
 svn up
 
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/ubuntu910_64_gcc.xml
+                              $HOME/ACE/autobuild/configs/autobuild/remedynl/ubuntu1004_64_gcc.xml
 
