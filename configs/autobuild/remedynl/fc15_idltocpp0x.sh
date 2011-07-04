8ed51d05a7ceeb32a4fba167512bd864b9346308
Johnny Willemsen <jwillemsen@remedy.nl>
2011-07-04 18:12:22 +0000

    new files
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8198 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc15_idltocpp0x.sh
+++ configs/autobuild/remedynl/fc15_idltocpp0x.sh
@@ -8,4 +8,4 @@ svn up
 
 # Run the build.
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc15_versioned.xml
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc15_idltocpp0x.xml
