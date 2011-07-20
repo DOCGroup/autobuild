5c8e3e011ff321e55cc5d7ff40622302e766b6fc
William R. Otte <wotte@dre.vanderbilt.edu>
2011-07-20 14:51:30 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8216 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/zifnab/zifnab.sh
+++ configs/autobuild/zifnab/zifnab.sh
@@ -8,8 +8,10 @@ sh /builds/autobuild/configs/autobuild/zifnab/clang-builds.sh &
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/cosmic.xml &
 perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/Snow_Leopard_Static_Host_Fast.xml
 
+security unlock -p ""  /Users/wotte/Library/Keychains/iPhone.keychain
 #perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPad_3.2_Hardware.xml &
 #Sleep an hour to give this build a head start.
 #sleep 300
 # Slower build needs to come second
+security unlock -p ""  /Users/wotte/Library/Keychains/iPhone.keychain
 #perl /builds/autobuild/autobuild.pl -V /builds/autobuild/configs/autobuild/zifnab/iPhone_3.1.2_Hardware.xml 
