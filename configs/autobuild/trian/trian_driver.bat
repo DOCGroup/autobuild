315385123ac6172c13c7f879a4c2332712566f3f
Johnny Willemsen <jwillemsen@remedy.nl>
2011-08-11 14:21:47 +0000

    removed another build that just doesn't run
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8223 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/trian/trian_driver.bat
+++ configs/autobuild/trian/trian_driver.bat
@@ -2,5 +2,4 @@ cd \ace\autobuild
 svn up
 cd configs\autobuild\trian
 perl c:\ACE\autobuild\autobuild.pl ciao_no_inline.xml
-perl c:\ACE\autobuild\autobuild.pl ace_for_tao.xml
 perl C:\ACE\autobuild\autobuild.pl versioned_namespace.xml
