41645d67fcbeff574005057cebc588bc7d7cda81
Abdullah Sowayan <sowayan@me.com>
2011-10-20 23:46:02 +0000

    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8350 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/abdul/build_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+cd /builds/autobuild
+
+svn up
+
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/lion_full_llvm.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/lion_full_static_llvm.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_5_Simulator.xml
+
