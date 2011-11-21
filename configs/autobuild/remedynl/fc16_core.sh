b2b4f920526afd84648f8283f40ca761bab63d2a
Johnny Willemsen <jwillemsen@remedy.nl>
2011-11-21 18:34:49 +0000

    new fc16_core
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8391 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/fc16_core.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $Id$
+#
+cd $HOME/ACE/autobuild
+
+svn up
+
+exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc16_core.xml
