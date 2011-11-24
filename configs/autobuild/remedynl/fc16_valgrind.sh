720d0e4fa3b59bccbad722ad430b7959b43adadc
Johnny Willemsen <jwillemsen@remedy.nl>
2011-11-24 11:13:18 +0000

    Moved builds from fc15 to fc16
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8395 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc16_valgrind.sh
+++ configs/autobuild/remedynl/fc16_valgrind.sh
@@ -8,5 +8,5 @@ cd $HOME/ACE/autobuild
 svn up
 
 exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc15_valgrind.xml
+                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc16_valgrind.xml
 
