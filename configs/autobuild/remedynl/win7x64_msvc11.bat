e2841da3cb21a0f6ab28a8c0956690a211fd0939
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-14 08:09:09 +0000

    moved build
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8440 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/win7x64_msvc11.bat
+++ configs/autobuild/remedynl/win7x64_msvc11.bat
@@ -1,4 +1,4 @@
 cd C:\ACE\autobuild
 c:\cygwin\bin\svn up
 cd configs\autobuild\remedynl
-perl C:\ACE\autobuild\autobuild.pl win8_64b_msvc11.xml
+perl C:\ACE\autobuild\autobuild.pl win7x64_msvc11.xml
