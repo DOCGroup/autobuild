9a1741058a8c80cd5e05b5474b714da87e57d655
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-16 12:04:21 +0000

    change
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8534 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc16_slave1.sh
+++ configs/autobuild/remedynl/fc16_slave1.sh
@@ -3,9 +3,8 @@
 # $Id$
 
 # Make sure autobuild directory is up to date.
-cd $HOME/ACE/autobuild
+cd $HOME/autobuild
 svn up
 
 # Run the build.
-exec /usr/bin/perl $HOME/ACE/autobuild/autobuild.pl \
-                             $HOME/ACE/autobuild/configs/autobuild/remedynl/fc16_coiop.xml
+sh $HOME/autobuild/configs/autobuild/remedynl/fc16_coiop.sh && \
