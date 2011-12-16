1dab1c5c138a0e726b2c4a68ddf4435ac5aad4a5
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-16 13:19:43 +0000

    use slave2
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8545 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc16_slave2.sh
+++ configs/autobuild/remedynl/fc16_slave2.sh
@@ -7,4 +7,5 @@ cd $HOME/autobuild
 svn up
 
 # Run the build.
-sh $HOME/autobuild/configs/autobuild/remedynl/fc16_valgrind.sh
+sh $HOME/autobuild/configs/autobuild/remedynl/fc16_valgrind.sh && \
+   $HOME/autobuild/configs/autobuild/remedynl/fc16_perf.sh
