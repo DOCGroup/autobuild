b4a534cd9525c1104ff51961f05dc7e442193bb6
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-16 15:19:04 +0000

    nicer layout
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8553 59e07337-6b18-0410-a141-c5efe34a0da7

---
+++ configs/autobuild/remedynl/fc16_04.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $Id$
+
+# Make sure autobuild directory is up to date.
+cd $HOME/autobuild
+svn up
+
+sh $HOME/autobuild/configs/autobuild/remedynl/fc16_04_perf_ts.sh && \
+   $HOME/autobuild/configs/autobuild/remedynl/fc16_04_lwccm_ne.sh
--- configs/autobuild/remedynl/fc16_slave4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-
-# Make sure autobuild directory is up to date.
-cd $HOME/autobuild
-svn up
-
-sh $HOME/autobuild/configs/autobuild/remedynl/fc16_03_perf_ts.sh
