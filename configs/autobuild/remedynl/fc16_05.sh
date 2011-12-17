fc954409c176c1d037907c6c88fcf906553ade85
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-17 19:20:34 +0000

    new build
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8576 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc16_05.sh
+++ configs/autobuild/remedynl/fc16_05.sh
@@ -7,4 +7,6 @@ cd $HOME/autobuild
 svn up
 
 sh $HOME/autobuild/configs/autobuild/remedynl/fc16_05_tao_valgrind.sh && \
-   $HOME/autobuild/configs/autobuild/remedynl/fc16_05_noiiop.sh
+   $HOME/autobuild/configs/autobuild/remedynl/fc16_05_noiiop.sh && \
+   $HOME/autobuild/configs/autobuild/remedynl/fc16_05_lwccm_ne_lf.sh
+
