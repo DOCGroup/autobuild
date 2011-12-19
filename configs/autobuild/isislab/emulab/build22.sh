c557229c016bef006a5f8da09915de340a696904
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-19 19:09:45 +0000

    cleanup
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8612 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/isislab/emulab/build22.sh
+++ configs/autobuild/isislab/emulab/build22.sh
@@ -3,18 +3,11 @@
 /users/bczar/runbuild.sh FC6cmpmetrics2
 /users/bczar/runbuild.sh FC8xmlconfig
 /users/bczar/runbuild.sh FC8ostream
-/users/bczar/runbuild.sh FC8ciaocore
 /users/bczar/runbuild.sh FC6CORBAecompact2
-/users/bczar/runbuild.sh FC8Core
 /users/bczar/runbuild.sh FC8DynamicHash2
-/users/bczar/runbuild.sh FC6Full
 /users/bczar/runbuild.sh FC6FullReactors2
-/users/bczar/runbuild.sh FC8gcc4
-/users/bczar/runbuild.sh FC8gcc4ssl
 /users/bczar/runbuild.sh FC8ImplicitTemplat2
 /users/bczar/runbuild.sh FC8IPv6
-/users/bczar/runbuild.sh FC8versioned
-/users/bczar/runbuild.sh FC8NoIIOP
 /users/bczar/runbuild.sh FC8NoInt
 /users/bczar/runbuild.sh FC8SingleThreaded2
 /users/bczar/runbuild.sh FC8Static3
@@ -23,6 +16,3 @@
 /users/bczar/runbuild.sh FC6fpCORBAemicros
 /users/bczar/runbuild.sh FC6fpCORBAregular
 /users/bczar/runbuild.sh FC6fpCORBAminimum2
-/users/bczar/runbuild.sh FC6CodeCoverage
-/users/bczar/runbuild.sh FC8xampler2
-/users/bczar/runbuild.sh FC6valgrind
