40403f2fe9e882873d54b0156758cc1fdcd122c6
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-19 18:27:32 +0000

    moved build
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8608 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc16_06.sh
+++ configs/autobuild/remedynl/fc16_06.sh
@@ -9,5 +9,6 @@ svn up
 sh $HOME/autobuild/configs/autobuild/remedynl/fc16_06_fp_corbaec.sh && \
    $HOME/autobuild/configs/autobuild/remedynl/fc16_06_fp_corbaem.sh && \
    $HOME/autobuild/configs/autobuild/remedynl/fc16_06_nologging.sh && \
-   $HOME/autobuild/configs/autobuild/remedynl/fc16_06_fp_corbaems.sh
+   $HOME/autobuild/configs/autobuild/remedynl/fc16_06_fp_corbaems.sh && \
+   $HOME/autobuild/configs/autobuild/remedynl/fc16_06_versioned.sh
 
