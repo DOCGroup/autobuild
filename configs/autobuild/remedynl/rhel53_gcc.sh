1540652f7e470900c6f57408e6fd0bd7f7f354d4
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-21 11:07:16 +0000

    new structure
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8625 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/rhel53_gcc.sh
+++ configs/autobuild/remedynl/rhel53_gcc.sh
@@ -3,8 +3,8 @@
 # $Id$
 
 # Make sure autobuild directory is up to date.
-cd $HOME/ACE/autobuild
+cd $HOME/autobuild
 svn up
 
 # Run the build.
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/rhel53_gcc_ace.sh
+sh $HOME/autobuild/configs/autobuild/remedynl/rhel53_gcc_ace.sh
