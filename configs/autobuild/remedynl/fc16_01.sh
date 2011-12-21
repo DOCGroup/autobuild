c4828c4e8fedd5bf837e8c97fb17b06432e3a501
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-21 15:05:19 +0000

    move build one level up
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8627 59e07337-6b18-0410-a141-c5efe34a0da7

--- configs/autobuild/remedynl/fc16_01.sh
+++ configs/autobuild/remedynl/fc16_01.sh
@@ -9,7 +9,6 @@ svn up
 # Run the build.
 sh $HOME/autobuild/configs/autobuild/remedynl/fc16_01_coiop.sh && \
    $HOME/autobuild/configs/autobuild/remedynl/fc16_01_corbae_compact.sh && \
-   $HOME/autobuild/configs/autobuild/remedynl/fc16_01_acefortao.sh && \
    $HOME/autobuild/configs/autobuild/remedynl/fc16_01_wchar.sh && \
    $HOME/autobuild/configs/autobuild/remedynl/fc16_01_ipv6.sh && \
    $HOME/autobuild/configs/autobuild/remedynl/fc16_01_xmlconfig.sh
