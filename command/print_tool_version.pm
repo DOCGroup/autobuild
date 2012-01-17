9d3e0919fa089b677745e5cdd6426b8f4f1b8b60
Johnny Willemsen <jwillemsen@remedy.nl>
2012-01-17 11:41:46 +0000

    added print_ruby_version
    git-svn-id: file:///project/Git-Conversion/autobuild/trunk@8692 59e07337-6b18-0410-a141-c5efe34a0da7

--- command/print_tool_version.pm
+++ command/print_tool_version.pm
@@ -43,19 +43,19 @@ sub Run ($)
     my $remote = 0;
 
     my $remote_shell = main::GetVariable ('remote_shell');
-    
+
     if ($options =~ m/^remote([ \t]+|$)/) {
         $remote = 1;
         $options =~ s/^remote[ \t]*//;
-        
+
         if (!defined $remote_shell) {
           print "WARNING: no remote_shell variable defined!\n";
           return 1;
         }
     }
 
-    main::PrintStatus ('Config', $remote ? 
-      "print Remote " . $self->{tool} . " version - remote shell=$remote_shell" : 
+    main::PrintStatus ('Config', $remote ?
+      "print Remote " . $self->{tool} . " version - remote shell=$remote_shell" :
       "print " . $self->{tool} . " version" );
 
     print "<h3>", $self->{tool}, " version (", $self->{tool}, " ", $self->{args}, ")</h3>\n";
@@ -64,7 +64,7 @@ sub Run ($)
     } else {
         system($self->{tool} . " " . $self->{args});
     }
-    
+
     return 1;
 }
 
@@ -74,3 +74,4 @@ main::RegisterCommand ("print_purify_version", new print_tool_version("purify", 
 main::RegisterCommand ("print_quantify_version", new print_tool_version("quantify", "-version"));
 main::RegisterCommand ("print_valgrind_version", new print_tool_version("valgrind", "--version"));
 main::RegisterCommand ("print_perl_version", new print_tool_version("perl", "-V"));
+main::RegisterCommand ("print_ruby_version", new print_tool_version("ruby", "-v"));
