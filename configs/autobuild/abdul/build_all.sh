987275a27979f075257e96666e87f4307319823f
sowayan <sowayan@gmail.com>
2012-10-23 15:26:38 -0400

    update this file

--- configs/autobuild/abdul/build_all.sh
+++ configs/autobuild/abdul/build_all.sh
@@ -5,10 +5,10 @@
 
 cd /builds/autobuild
 
-svn up
+git pull
 
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/lion_full_llvm.xml
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/lion_full_static_llvm.xml
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_5_Simulator.xml
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_5_Hardware.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mountain_lion_full_llvm.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mountain_lion_full_static_llvm.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_6_Simulator.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_6_Hardware.xml
 
