76243f7afde06594fcda4363a1c759e0c4f207fd
Johnny Willemsen <jwillemsen@remedy.nl>
2012-11-15 15:46:46 +0100

    * configs/autobuild/remedynl/ngc/fc12_full.xml:     * configs/autobuild/remedynl/ngc/fc12_full_host.xml:     * configs/autobuild/remedynl/ngc/fc12_optimized.xml:     * configs/autobuild/remedynl/ngc/fc12_optimized_host.xml:     * configs/autobuild/remedynl/ngc/mpc440ctubuild.xml:     * configs/autobuild/remedynl/ngc/mpc440ctubuild_host.xml:     * configs/autobuild/remedynl/sauron_copy_buildresult_vms.sh:       Updated rsync to new storage

--- configs/autobuild/remedynl/sauron_copy_buildresult_vms.sh
+++ configs/autobuild/remedynl/sauron_copy_buildresult_vms.sh
@@ -2,5 +2,5 @@
 
 export RSYNC_RSH=ssh
 chmod ugo+r /nfs/openvms/v82ia64/*.html /nfs/openvms/v82ia64/*.txt
-rsync -r --progress --delete -z --exclude *.tar* --exclude /*.tar*/ --exclude /ACE_wrappers*/ --include *.txt --include *.html --exclude ** /nfs/openvms/v82ia64/* qnap.remedy.nl::Qweb/Buildlogs/openvmsia64
+rsync -r --progress --delete -z --exclude *.tar* --exclude /*.tar*/ --exclude /ACE_wrappers*/ --include *.txt --include *.html --exclude ** /nfs/openvms/v82ia64/* treebeard.remedy.nl::Buildlogs/openvmsia64
 
