375cf6cb6f0efe8a7aa556ff789ed2bdb7db9edf
Johnny Willemsen <jwillemsen@remedy.nl>
2012-12-13 11:53:24 +0100

    * configs/autobuild/remedynl/ngc/fc12_full.sh:     * configs/autobuild/remedynl/ngc/fc12_full.xml:     * configs/autobuild/remedynl/ngc/fc12_full_host.sh:     * configs/autobuild/remedynl/ngc/fc12_full_host.xml:     * configs/autobuild/remedynl/ngc/fc12_full_run.sh:     * configs/autobuild/remedynl/ngc/fc12_optimized.sh:     * configs/autobuild/remedynl/ngc/fc12_optimized.xml:     * configs/autobuild/remedynl/ngc/fc12_optimized_host.sh:     * configs/autobuild/remedynl/ngc/fc12_optimized_host.xml:     * configs/autobuild/remedynl/ngc/fc12_optimized_run.sh:     * configs/autobuild/remedynl/ngc/mpc440ctubuild.sh:     * configs/autobuild/remedynl/ngc/mpc440ctubuild.xml:     * configs/autobuild/remedynl/ngc/mpc440ctubuild_host.sh:     * configs/autobuild/remedynl/ngc/mpc440ctubuild_host.xml:     * configs/autobuild/remedynl/ngc/mpc440ctubuild_run.sh:       Removed from here, these files are maintained in the old svn       archive

--- configs/autobuild/remedynl/ngc/fc12_optimized_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-cd $HOME/ACE/autobuild
-
-svn up
-
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ngc/fc12_optimized_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ngc/fc12_optimized.sh
--- configs/autobuild/remedynl/ngc/mpc440ctubuild_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-cd $HOME/ACE/autobuild
-
-svn up
-
-sh $HOME/ACE/autobuild/configs/autobuild/remedynl/ngc/mpc440ctubuild_host.sh && $HOME/ACE/autobuild/configs/autobuild/remedynl/ngc/mpc440ctubuild.sh
