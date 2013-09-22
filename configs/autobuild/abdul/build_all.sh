2f013b6173bf1ec2eb5c681cb6eb19013da9b8f7
sowayan <sowayan@gmail.com>
2013-09-22 17:58:21 -0400

    Adding iOS7 support

--- configs/autobuild/abdul/build_all.sh
+++ configs/autobuild/abdul/build_all.sh
@@ -9,6 +9,6 @@ git pull
 
 /usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mountain_lion_full_llvm.xml
 /usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mountain_lion_full_static_llvm.xml
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_6_Simulator.xml
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_6_Hardware.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_7_Simulator.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_7_Hardware.xml
 
