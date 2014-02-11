b6f9943bd5972f476e9421903dac821e12bb7ef2
Abdul Sowayan <asowayan@vectorworks.net>
2014-02-11 12:01:36 -0500

    Build on Mavericks

--- configs/autobuild/abdul/build_all.sh
+++ configs/autobuild/abdul/build_all.sh
@@ -7,8 +7,8 @@ cd /builds/autobuild
 
 git pull
 
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mountain_lion_full_llvm.xml
-/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mountain_lion_full_static_llvm.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mavericks_full_llvm.xml
+/usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/mavericks_full_static_llvm.xml
 /usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_7_Simulator.xml
 /usr/bin/perl /builds/autobuild/autobuild.pl /builds/autobuild/configs/autobuild/abdul/iOS_7_Hardware.xml
 
