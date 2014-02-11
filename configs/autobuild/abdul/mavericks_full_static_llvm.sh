d340bc2b59206df9d03abfdc24f9b7069bc8a1ea
Abdul Sowayan <asowayan@vectorworks.net>
2014-02-11 11:52:35 -0500

    Minor fixes to configuration files

--- configs/autobuild/abdul/mavericks_full_static_llvm.sh
+++ configs/autobuild/abdul/mavericks_full_static_llvm.sh
@@ -8,5 +8,5 @@ cd /builds/autobuild
 git pull
 
 exec /usr/bin/perl /builds/autobuild/autobuild.pl \
-                             /builds/autobuild/configs/autobuild/abdul/lion_full_static_llvm.xml
+                             /builds/autobuild/configs/autobuild/abdul/mavericks_full_static_llvm.xml
 
