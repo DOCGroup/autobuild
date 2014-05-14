626eaf8c7ce196a1e207bce250c2f065d8f3057c
Johnny Willemsen <jwillemsen@remedy.nl>
2014-05-14 18:08:42 +0200

    Handle ERROR as an error     * common/parse_compiler_output.pm:

--- common/parse_compiler_output.pm
+++ common/parse_compiler_output.pm
@@ -283,6 +283,12 @@ sub handle_compiler_output_line($) {
     return;
   }
 
+  if ($s =~ m/ERROR/)
+  {
+    $self->Output_Error ($s);
+    return;
+  }
+
   if ($s =~ m/cannot execute binary file/) {
     # Means we can't execute the binary, probably using target executable on host
     $self->Output_Error ($s);
