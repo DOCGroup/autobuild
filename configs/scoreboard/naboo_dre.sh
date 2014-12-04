bb265d12a49eec40f2857f8a431eff4cfa721f09
Johnny Willemsen <jwillemsen@remedy.nl>
2014-12-04 19:39:39 +0100

    * ChangeLog:       Deleted.
        * configs/scoreboard/naboo_dre.sh:
          naboo doesn't support git with https, so use a regular
          svn up from github for the moment

--- configs/scoreboard/naboo_dre.sh
+++ configs/scoreboard/naboo_dre.sh
@@ -5,7 +5,9 @@ PERLLIB=autobuild
 export PERLLIB
 
 cd $HOME/autobuild
-/usr/local/bin/git pull
+#/usr/local/bin/git pull
+# naboo isn't able to do git with https, so use github svn support
+svn up
 
 # Generate the index page!
 /usr/bin/perl ./scoreboard.pl -v -d /export/web/www/scoreboard -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
