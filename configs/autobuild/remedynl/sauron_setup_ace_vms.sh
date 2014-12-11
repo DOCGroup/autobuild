f4a0c9e94a4d49c3d64a4026af8dc44544606ba0
Johnny Willemsen <jwillemsen@remedy.nl>
2014-12-11 15:43:01 +0100

    Updated OpenVMS host to use our git mirror     * configs/autobuild/remedynl/sauron.xml:     * configs/autobuild/remedynl/sauron_copy_buildresult_vms.sh:     * configs/autobuild/remedynl/sauron_setup_ace_vms.sh:

--- configs/autobuild/remedynl/sauron_setup_ace_vms.sh
+++ configs/autobuild/remedynl/sauron_setup_ace_vms.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+# when run on a frsh system run the following commands in the $HOME/ace/openvms directory
+# git clone --depth 1 git://git.remedy.nl/mpc.git MPC
+# git clone --depth 1 git://git.remedy.nl/atcd.git ATCD
 
 cd /nfs/openvms/autobuild
 git pull
 cd $HOME/ace/autobuild
 git pull
-mkdir -p $HOME/ace/openvms
+cd $HOME/ace/openvms/ATCD
+git pull
+cd $HOME/ace/openvms/MPC
+git pull
 cd $HOME/ace/openvms
 rm -rf ace_vms_update.tar
-svn co svn://svn.dre.vanderbilt.edu/DOC/Middleware/sets-anon/ACE+TAO .
-tar -cf ace_vms_update.tar -g ace_vms_update.tar_inc ACE_wrappers --exclude-vcs
+tar -cf ace_vms_update.tar -g ace_vms_update.tar_inc ATCD MPC --exclude-vcs
 cp ace_vms_update.tar /nfs/openvms/v82ia64
