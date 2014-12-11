f79665b580eee7e374ce5857eee71301912dade7
Johnny Willemsen <jwillemsen@remedy.nl>
2014-12-11 16:22:25 +0100

    Simplified this script for OpenVMS     * configs/autobuild/remedynl/sauron_setup_ace_vms.sh:

--- configs/autobuild/remedynl/sauron_setup_ace_vms.sh
+++ configs/autobuild/remedynl/sauron_setup_ace_vms.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 # when run on a frsh system run the following commands in the $HOME/ace/openvms directory
-# git clone --depth 1 git://git.remedy.nl/mpc.git MPC
-# git clone --depth 1 git://git.remedy.nl/atcd.git ATCD
+# git clone git://git.remedy.nl/mpc.git MPC
+# git clone git://git.remedy.nl/atcd.git ATCD
 
 cd /nfs/openvms/autobuild
 git pull
 cd $HOME/ace/autobuild
 git pull
-cd $HOME/ace/openvms/ATCD
-git pull
-cd $HOME/ace/openvms/MPC
-git pull
 cd $HOME/ace/openvms
+pushd ATCD && git pull && popd
+pushd MPC && git pull && popd
 rm -rf ace_vms_update.tar
 tar -cf ace_vms_update.tar -g ace_vms_update.tar_inc ATCD MPC --exclude-vcs
 cp ace_vms_update.tar /nfs/openvms/v82ia64
