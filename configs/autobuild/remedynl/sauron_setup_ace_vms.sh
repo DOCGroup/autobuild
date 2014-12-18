99a6590a583c877292b15516da6847761f765e98
Johnny Willemsen <jwillemsen@remedy.nl>
2014-12-18 19:53:23 +0100

    Back to hierarchical directory layout     * configs/autobuild/remedynl/sauron.xml:     * configs/autobuild/remedynl/sauron_copy_buildresult_vms.sh:     * configs/autobuild/remedynl/sauron_setup_ace_vms.sh:

--- configs/autobuild/remedynl/sauron_setup_ace_vms.sh
+++ configs/autobuild/remedynl/sauron_setup_ace_vms.sh
@@ -2,6 +2,10 @@
 # when run on a frsh system run the following commands in the $HOME/ace/openvms directory
 # git clone git://git.remedy.nl/mpc.git MPC
 # git clone git://git.remedy.nl/atcd.git ATCD
+# ln -s $HOME/ace/openvms/ATCD/ACE ACE_wrappers
+# ln -s $HOME/ace/openvms/ATCD/TAO ACE_wrappers/TAO
+# ln -s $HOME/ace/openvms/MPC ACE_wrappers/MPC
+
 
 cd /nfs/openvms/autobuild
 git pull
@@ -11,5 +15,5 @@ cd $HOME/ace/openvms
 pushd ATCD && git pull && popd
 pushd MPC && git pull && popd
 rm -rf ace_vms_update.tar
-tar -cf ace_vms_update.tar -g ace_vms_update.tar_inc ATCD MPC --exclude-vcs
+tar -chf ace_vms_update.tar -g ace_vms_update.tar_inc ACE_wrappers --exclude-vcs
 cp ace_vms_update.tar /nfs/openvms/v82ia64
