350a6e7ceaa4c29d6e9c542b1d37936461d8d1e8
Olli Savia <ops@iki.fi>
2015-01-21 16:15:02 +0200

    Ignore linker warnings on LynxOS 5.0.

--- common/parse_compiler_output.pm
+++ common/parse_compiler_output.pm
@@ -200,6 +200,12 @@ sub handle_compiler_output_line($) {
     return;
   }
 
+  if ($s =~ m/gnu.linkonce.t/) {
+    # Ignore "xxx.gnu.linkonce.t.xxx warnings on LynxOS 5.0
+    $self->Output_Normal ($s);
+    return;
+  }
+
   if ($s =~ m/seed choices are invariably poor/) {
     # Ignore "srand() seed choices are invariably poor" warning on OpenBSD
     $self->Output_Normal ($s);
