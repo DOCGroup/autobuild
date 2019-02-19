ebc1976b0d42e70d9256cba661a6877fe661f39c
Johnny Willemsen <jwillemsen@remedy.nl>
2019-02-19 15:56:33 +0100

    Remove ace/tao/ciao future, cosmic, and xsc scoreboard pages, all empty

--- configs/scoreboard/naboo_dre.sh
+++ configs/scoreboard/naboo_dre.sh
@@ -11,11 +11,8 @@ git pull
 /usr/bin/perl ./scoreboard.pl -v -d /export/web/www/scoreboard -i $HOME/autobuild/configs/scoreboard/index.xml  2>&1 &
 # Generate other pages!
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ace.xml -o ace.html -r ace.rss 2>&1 &
-/usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ace_future.xml -o ace_future.html 2>&1 &
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/tao.xml -o tao.html -r tao.rss 2>&1  &
-/usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/tao_future.xml -o tao_future.html 2>&1 &
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ciao.xml -o ciao.html -r ciao.rss 2>&1  &
-/usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ciao_future.xml -o ciao_future.html  2>&1  &
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/dds.xml -o dds.html -r dds.rss 2>&1  &
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/misc.xml -o misc.html 2>&1 &
 
