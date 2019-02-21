90d423cd883a0025361cb4600638a7fd17375bf0
Johnny Willemsen <jwillemsen@remedy.nl>
2019-02-21 10:02:10 +0100

    Removed misc page, no contents for a long time, removed releases.xml, not used anymore

--- configs/scoreboard/naboo_dre.sh
+++ configs/scoreboard/naboo_dre.sh
@@ -14,7 +14,6 @@ git pull
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/tao.xml -o tao.html -r tao.rss 2>&1  &
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/ciao.xml -o ciao.html -r ciao.rss 2>&1  &
 /usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/dds.xml -o dds.html -r dds.rss 2>&1  &
-/usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -f $HOME/autobuild/configs/scoreboard/misc.xml -o misc.html 2>&1 &
 
 # Generate the test matrices!
 ##testmatrix/update_scoreboard.sh 2>&1 &
