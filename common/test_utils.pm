eab6c6b90b627be535a4418bd0e3cb16717654fd
lij-oci <55156341+lij-oci@users.noreply.github.com>
2021-10-07 14:52:00 -0500

    Test scripts and test cases for scoreboard.pl (#50)
    * fix_empty_table3
    
    * sort by test
    
    * pull
    
    * pull2
    
    * total_html
    
    * same total across runs
    
    * scoreboard MultiRun test
    
    * checks Totals.html
    
    * cut down test data, replaced diff
    
    * replaced diff
    
    * mode 100755
    
    * compare_files line by line

--- common/test_utils.pm
+++ common/test_utils.pm
@@ -25,4 +25,44 @@ sub expect_file_contents
     }
 }
 
+###############################################################################
+# compare_files
+# compare 2 files line by line
+# Arguments: $$ - paths to the 2 files
+# Returns: 0 - the 2 files exist (not empty) and have the same contents
+#          1 - otherwise, and prints the first differing lines, or error
+###############################################################################
+sub compare_files ($$)
+{
+    my $file1 = shift;
+    my $file2 = shift;
+    print "compare \"$file1\" with \"$file2\"\n";
+    if (!(-e $file1) || (-z _)) {
+        print "\"$file1\" does not exist, or is empty.\n";
+        return 1;
+    }
+    if (!(-e $file2) || (-z _)) {
+        print "\"$file2\" does not exist, or is empty.\n";
+        return 1;
+    }
+    my $r1 = open my $fh1, '<', $file1;
+    my $r2 = open my $fh2, '<', $file2;
+    if (!$r1 || !$r2) {
+        print STDERR "ERROR: Couldn't open \"" . ($r1 ? $file2 : $file1) . "\"\n";
+        return 1;
+    }
+    while (1) {
+        my $f1_line = <$fh1>;
+        my $f2_line = <$fh2>;
+        if (!$f1_line && !$f2_line) {
+            return 0;
+        }
+        elsif (!$f1_line || !$f2_line || ($f1_line ne $f2_line)) {
+            print "\"$file1\":\n$f1_line\n";
+            print "\"$file2\":\n$f2_line\n";
+            return 1;
+        }
+    }
+}
+
 1;
