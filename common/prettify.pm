dc7fd105de9443fdff7e8703c3b61e9462cb6256
Timothy Simpson <simpsont@objectcomputing.com>
2021-10-22 08:39:40 +0100

    resolve peer review comments

--- common/prettify.pm
+++ common/prettify.pm
@@ -657,11 +657,11 @@ sub Footer ()
               $indent x 3, "<![CDATA[$test->{OUT}]]></system-out></testcase>\n";
 
           my $github_actions = main::GetVariable ('GITHUB_ACTIONS');
-          if (defined $github_actions) {
+          if (defined $ENV{"GITHUB_ACTIONS"} && (!defined $github_actions || $github_actions ne "0")) {
             my $test_file = (split / /, $test->{NAME})[0];
             my $error_str = $error;
             $error_str =~ s/([%\r\n])/ sprintf "%%%02X", ord $1 /eg;
-            print "::error title=$test->{NAME},file=$test_file,line=0,col=0::$error_str";
+            print "::error title=$test->{NAME},file=$test_file,line=0,col=0::$error_str\n";
           }
         }
     }
